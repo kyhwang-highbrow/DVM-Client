f83fe13425ae52b348276c0100d05fcb32aac686
sgkim <sgkim@highbrow.com>
2016-11-13 01:37:33 +0000

    [성구] #스테이지 맵 1챕터 - 왼쪽 이동 화살표 visible : false 처리
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@67828 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_AdventureSceneNew.lua
+++ dragonvillage/src/UI_AdventureSceneNew.lua
@@ -228,8 +228,10 @@ function UI_AdventureSceneNew:refreshChapter(chapter, difficulty, force)
     self:SpeechBalloon(chapter)
 
     -- 이전, 다음 챕터 버튼
-    vars['prevBtn']:setEnabled(1 < chapter)
-    vars['nextBtn']:setEnabled(chapter < MAX_ADVENTURE_CHAPTER)
+    --vars['prevBtn']:setEnabled(1 < chapter)
+    vars['prevBtn']:setVisible(1 < chapter)
+    --vars['nextBtn']:setEnabled(chapter < MAX_ADVENTURE_CHAPTER)
+    vars['nextBtn']:setVisible(chapter < MAX_ADVENTURE_CHAPTER)
 
 
     -- 마지막에 진입한 챕터 저장
