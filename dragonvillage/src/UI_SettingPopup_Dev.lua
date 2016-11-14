671f4bbe4ea4022db766ad6db8843492d7da5b4d
sgkim <sgkim@highbrow.com>
2016-11-14 03:06:28 +0000

    [성구] #[설정]->[개발]화면에서 "모든 열매 추가", "모든 진화재료 추가" 기능 구현 (100개씩 한 번에 추가됨)
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@67863 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_SettingPopup_Dev.lua
+++ dragonvillage/src/UI_SettingPopup_Dev.lua
@@ -6,6 +6,8 @@ function UI_SettingPopup:init_Dev()
     vars['fpsBtn']:registerScriptTapHandler(function() self:click_fpsBtn() end)
     vars['allClearBtn']:registerScriptTapHandler(function() self:click_allClearBtn() end)
     vars['allDragonBtn']:registerScriptTapHandler(function() self:click_allDragonBtn() end)
+    vars['allFruitBtn']:registerScriptTapHandler(function() self:click_allFruitBtn() end)
+    vars['allMaterialBtn']:registerScriptTapHandler(function() self:click_allMaterialBtn() end)
 end
 
 -------------------------------------
@@ -26,7 +28,8 @@ end
 function UI_SettingPopup:click_allClearBtn()
     g_adventureData:allStageClear()
     UIManager:toastNotificationGreen('모든 스테이지 오픈!')
-    self.m_bRestart = true
+    UIManager:toastNotificationGreen('정상적인 적용을 위해 재시작을 권장합니다.')
+    --self.m_bRestart = true
 end
 
 -------------------------------------
@@ -61,7 +64,104 @@ function UI_SettingPopup:click_allDragonBtn()
         else
             ui_network:close()
             UIManager:toastNotificationGreen('모든 드래곤 추가!')
-            self.m_bRestart = true
+            UIManager:toastNotificationGreen('정상적인 적용을 위해 재시작을 권장합니다.')
+            --self.m_bRestart = true
+        end
+
+        if (ret and ret['dragons']) then
+            for _,t_dragon in pairs(ret['dragons']) do
+                g_dragonsData:applyDragonData(t_dragon)
+            end
+        end
+    end
+    ui_network:setSuccessCB(do_work)
+    do_work()
+end
+
+-------------------------------------
+-- function click_allFruitBtn
+-- @brief 모든 열매 추가
+-------------------------------------
+function UI_SettingPopup:click_allFruitBtn()
+    local uid = g_userData:get('uid')
+    local table_fruit = TABLE:get('fruit')
+    local t_list = {}
+    for id,_ in pairs(table_fruit) do
+        table.insert(t_list, id)
+    end
+    local do_work
+
+    local ui_network = UI_Network()
+    ui_network:setReuse(true)
+    ui_network:setUrl('/users/manage')
+    ui_network:setParam('uid', uid)
+    ui_network:setParam('act', 'increase')
+    ui_network:setParam('key', 'fruits')
+
+    do_work = function(ret)
+        local id = t_list[1]
+        
+        if id then
+            table.remove(t_list, 1)
+            local msg = '"' .. table_fruit[id]['t_name'] .. '" 추가 중...'
+            ui_network:setLoadingMsg(msg)
+            ui_network:setParam('value', tostring(id) .. ',' .. tostring(100))
+            ui_network:request()
+        else
+            ui_network:close()
+            UIManager:toastNotificationGreen('모든 열매 추가!')
+            UIManager:toastNotificationGreen('정상적인 적용을 위해 재시작을 권장합니다.')
+            --self.m_bRestart = true
+
+            -- 한 번에 저장
+            if (ret and ret['user']) then
+                g_serverData:applyServerData(ret['user'], 'user')
+            end
+        end
+    end
+    ui_network:setSuccessCB(do_work)
+    do_work()
+end
+
+-------------------------------------
+-- function click_allMaterialBtn
+-- @brief 모든 진화 재료 추가
+-------------------------------------
+function UI_SettingPopup:click_allMaterialBtn()
+    local uid = g_userData:get('uid')
+    local table_evolution_stone = TABLE:get('evolution_item')
+    local t_list = {}
+    for id,_ in pairs(table_evolution_stone) do
+        table.insert(t_list, id)
+    end
+    local do_work
+
+    local ui_network = UI_Network()
+    ui_network:setReuse(true)
+    ui_network:setUrl('/users/manage')
+    ui_network:setParam('uid', uid)
+    ui_network:setParam('act', 'increase')
+    ui_network:setParam('key', 'evolution_stones')
+
+    do_work = function(ret)
+        local id = t_list[1]
+        
+        if id then
+            table.remove(t_list, 1)
+            local msg = '"' .. table_evolution_stone[id]['t_name'] .. '" 추가 중...'
+            ui_network:setLoadingMsg(msg)
+            ui_network:setParam('value', tostring(id) .. ',' .. tostring(100))
+            ui_network:request()
+        else
+            ui_network:close()
+            UIManager:toastNotificationGreen('모든 진화재료 추가!')
+            UIManager:toastNotificationGreen('정상적인 적용을 위해 재시작을 권장합니다.')
+            --self.m_bRestart = true
+
+            -- 한 번에 저장
+            if (ret and ret['user']) then
+                g_serverData:applyServerData(ret['user'], 'user')
+            end
         end
     end
     ui_network:setSuccessCB(do_work)
