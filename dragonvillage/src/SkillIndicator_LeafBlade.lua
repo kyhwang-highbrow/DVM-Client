987f944e1a1211d02c7e4dcd3d6da91b3e186288
mskim <mskim@highbrow.com>
2016-11-16 03:23:48 +0000

    [문성] 리프블레이드 무관통 인디케이터 구현 완료
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@68230 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/SkillIndicator_LeafBlade.lua
+++ dragonvillage/src/SkillIndicator_LeafBlade.lua
@@ -6,15 +6,19 @@ local PARENT = SkillIndicator
 SkillIndicator_LeafBlade = class(SkillIndicator, {
         m_indicatorEffect01 = '',
         
+		-- 베지어 곡선 이펙트
         m_indicatorLinkEffect1 = '',
         m_indicatorLinkEffect2 = '',
 
+		-- 직선 이펙트
         m_indicatorLinkEffect3 = '',
         m_indicatorLinkEffect4 = '',
 
 		-- 리프블레이드 무관통 관련 변수
 		m_isPass = 'bool',
 		m_isCollision = 'bool', 
+		m_target_1 = '',
+		m_target_2 = '',
     })
 
 -------------------------------------
@@ -25,6 +29,8 @@ function SkillIndicator_LeafBlade:init(hero, t_skill)
 
 	self.m_isPass = (t_skill['val_1'] == 1)
 	self.m_isCollision = false
+	self.m_target_1 = nil
+	self.m_target_2 = nil
 end
 
 -------------------------------------
@@ -74,7 +80,7 @@ function SkillIndicator_LeafBlade:onTouchMoved(x, y)
 	-- 4-2. 직선 이펙트 위치 갱신
     EffectLinearDot_refresh(self.m_indicatorLinkEffect3, tar_x, tar_y, pos_x, pos_y, 1)
     EffectLinearDot_refresh(self.m_indicatorLinkEffect4, tar_x, tar_y, pos_x, pos_y, -1)
-    
+
 	-- 4-3. 타겟에 찍히는 이펙트 위치 갱신
     self.m_indicator2:setPosition(tar_x - pos_x, tar_y - pos_y)
 
@@ -118,9 +124,13 @@ function SkillIndicator_LeafBlade:onTouchMoved(x, y)
     end
 
     self.m_highlightList = t_collision_obj
-
-    local cur_target_count = #self.m_highlightList
-    self:onChangeTargetCount(old_target_count, cur_target_count)
+	    
+	if (not self.m_isPass) then
+		self:changeEffectNonePass()
+	else
+		local cur_target_count = #self.m_highlightList
+		self:onChangeTargetCount(old_target_count, cur_target_count)
+	end
 end
 
 
@@ -201,6 +211,9 @@ end
 function SkillIndicator_LeafBlade:findTargetList(x, y)
     local world = self:getWorld()
     local target_formation_mgr = nil
+	
+	self.m_target_1 = nil 
+	self.m_target_2 = nil
 
     if self.m_hero.m_bLeftFormation then
         target_formation_mgr = world.m_rightFormationMgr
@@ -210,12 +223,28 @@ function SkillIndicator_LeafBlade:findTargetList(x, y)
     local pos_x = self.m_hero.pos.x
     local pos_y = self.m_hero.pos.y
 
-    -- 베지어 곡선에 의한 충돌 리스트와 좌표 리스트
+	local l_target = {}
+
+    -- 베지어 곡선에 의한 충돌 리스트
     local l_target1 = target_formation_mgr:findBezierTarget(x, y, pos_x, pos_y, 1)
     local l_target2 = target_formation_mgr:findBezierTarget(x, y, pos_x, pos_y, -1)
 
-    local std_dist = 1000
+	-- 00. 무관통 탄일 경우 베지어 곡선에서 충돌 리스트가 있다면 탈출
+	if (not self.m_isPass) then 
+		if (#l_target1 > 0) then 
+			table.insert(l_target, l_target1[1])
+			self.m_target_1 = l_target1[1]
+		end
+		if (#l_target2 > 0) then 
+			table.insert(l_target, l_target2[1])
+			self.m_target_2 = l_target2[1]
+		end
+		if (#l_target > 0) then
+			return l_target
+		end
+	end
 
+    local std_dist = 1000
 	local degree = getDegree(pos_x, pos_y, x, y)
 
     -- 직선에 의한 충돌 리스트 (상)
@@ -231,17 +260,77 @@ function SkillIndicator_LeafBlade:findTargetList(x, y)
         x + std_dist, y + std_dist * factor_y, LEAF_COLLISION_SIZE, 'missile_h')
     
     -- getLaserCollision는 반환값이 특정 테이블이기때문에 Character 클래스만 꺼내와서 정리한다.
+	-- 00. 무관통 탄의 경우 첫번째 오브젝트만 가져간다.
     local l_target_line = {}
-    for _, col in pairs(t_target_line_1) do
+    for i, col in pairs(t_target_line_1) do
         table.insert(l_target_line, col['obj'])
+		if (not self.m_isPass) then 
+			self.m_target_1 = col['obj']
+			break
+		end
     end
-    for _, col in pairs(t_target_line_2) do
+    for i, col in pairs(t_target_line_2) do
         table.insert(l_target_line, col['obj'])
+		if (not self.m_isPass) then 
+			self.m_target_2 = col['obj']
+			break
+		end
     end
 	
 	-- 하나의 테이블로 합침
-    local l_target = table.merge(l_target1, l_target2)
-    l_target = table.merge(l_target, l_target_line)
-    
-    return l_target
+	if self.m_isPass then 
+		l_target = table.merge(l_target1, l_target2)
+		l_target = table.merge(l_target, l_target_line)
+		return l_target
+	else
+		return l_target_line
+	end 
+end
+
+-------------------------------------
+-- function checkNonePass
+-------------------------------------
+function SkillIndicator_LeafBlade:changeEffectNonePass()
+	
+	-- 교차점 표시 부분
+	if (not self.m_target_1) and (not self.m_target_2) then
+		self.m_indicator2:changeAni('normal_start_idle', true)
+	else
+		self.m_indicator2:changeAni('enemy_start_idle', true)
+	end
+	
+	-- 탄1
+	if self.m_target_1 then
+		self:checkPosX(self.m_indicatorLinkEffect2.m_lEffectNode, self.m_target_1)
+		self:checkPosX(self.m_indicatorLinkEffect3.m_lEffectNode, self.m_target_1)
+	else
+		self.m_indicatorLinkEffect2:changeAni('circle_normal', true)
+		self.m_indicatorLinkEffect3:changeAni('circle_normal', true)
+	end
+	
+	-- 탄2
+	if self.m_target_2 then
+		self:checkPosX(self.m_indicatorLinkEffect1.m_lEffectNode, self.m_target_2)
+		self:checkPosX(self.m_indicatorLinkEffect4.m_lEffectNode, self.m_target_2)
+	else
+		self.m_indicatorLinkEffect1:changeAni('circle_normal', true)
+		self.m_indicatorLinkEffect4:changeAni('circle_normal', true)
+	end
+end
+
+-------------------------------------
+-- function checkPosX
+-------------------------------------
+function SkillIndicator_LeafBlade:checkPosX(l_effect, target)
+	local pos_x = nil
+	
+	-- 위아래 나누어 타겟을 저장하기 때문에x 좌표만 검사 한다 
+	for _, effect in pairs(l_effect) do
+		pos_x = effect.m_node:getPositionX()
+		if (pos_x + 150> target.pos.x) then
+			effect:changeAni('circle_normal', true)
+		else
+			effect:changeAni('circle', true)
+		end
+	end
 end
