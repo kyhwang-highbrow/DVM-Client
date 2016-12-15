afd1d3bce021fb3ac3f39fd4aa6be9081e422305
skim <skim@highbrow.com>
2016-12-15 08:00:18 +0000

    [성진] 인게임 보스 패턴 타임 트리거 버그 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@69917 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/TriggerTime.lua
+++ dragonvillage/src/TriggerTime.lua
@@ -4,8 +4,7 @@
 TriggerTime = class({
         m_owner = 'Character',
         m_bActive = 'boolean',
-        m_bUsed = 'boolean',    -- 사용되었는지 여부
-
+        
         m_priority = 'number',
         m_lPatternList = 'table',
         m_currIdx = 'number',
@@ -18,7 +17,6 @@ function TriggerTime:init(owner, t_time_trriger)
     self.m_owner = owner
 
     self.m_bActive = false
-    self.m_bUsed = false
     self.m_priority = (t_time_trriger['priority'] or 0)
 
     self.m_lPatternList = {}
@@ -39,10 +37,6 @@ end
 function TriggerTime:checkTrigger(time)
     local t_data = self.m_lPatternList[self.m_currIdx]
 
-    if (self.m_bUsed) then
-        return
-    end
-
     if (not t_data) then
         return
     end
@@ -51,8 +45,6 @@ function TriggerTime:checkTrigger(time)
         return
     end
 
-    self.m_bActive = true
-    self.m_bUsed = true
     self.m_currIdx = self.m_currIdx + 1
 
     do -- 변경된 패턴을 패턴 리스트에 추가
