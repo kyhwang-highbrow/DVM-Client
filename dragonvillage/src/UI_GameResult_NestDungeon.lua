15b2c5c8eb4cc93fe52fa0cbaeaaa1f87b292fba
skim <skim@highbrow.com>
2016-12-20 11:33:36 +0000

    [성진] 네스트모드 테스트용 버튼으로 진입된 경우 게임 결과에서 버튼 클릭시 멈춤현상이 나오는 문제 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@70289 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_GameResult_NestDungeon.lua
+++ dragonvillage/src/UI_GameResult_NestDungeon.lua
@@ -18,16 +18,26 @@ end
 -- function click_retryButton
 -------------------------------------
 function UI_GameResult_NestDungeon:click_retryButton()
-    local scene = SceneNestDungeon(self.m_stageID)
-    scene:runScene()
+    if g_nestDungeonData:isExistData() then
+        local scene = SceneNestDungeon(self.m_stageID)
+        scene:runScene()
+    else
+        local scene = SceneGame(nil, self.m_stageID, 'stage_' .. self.m_stageID, false)
+        scene:runScene()
+    end
 end
 
 -------------------------------------
 -- function click_backBtn
 -------------------------------------
 function UI_GameResult_NestDungeon:click_backBtn()
-    local scene = SceneNestDungeon(self.m_stageID)
-    scene:runScene()
+    if g_nestDungeonData:isExistData() then
+        local scene = SceneNestDungeon(self.m_stageID)
+        scene:runScene()
+    else
+        local scene = SceneLobby()
+        scene:runScene()
+    end
 end
 
 -------------------------------------
@@ -37,6 +47,11 @@ function UI_GameResult_NestDungeon:click_nextBtn()
     -- @TODO sgkim 네스트 던전도 다음 스테이지로 이동 가능 하도록 변경할 것
 
     -- 다음 스테이지 ID 지정
-    local scene = SceneNestDungeon(self.m_stageID)
-    scene:runScene()
+    if g_nestDungeonData:isExistData() then
+        local scene = SceneNestDungeon(self.m_stageID)
+        scene:runScene()
+    else
+        local scene = SceneGame(nil, self.m_stageID, 'stage_' .. self.m_stageID, false)
+        scene:runScene()
+    end
 end
