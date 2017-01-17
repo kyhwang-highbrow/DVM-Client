5cc89d623d4319c607fe9f772198606e7ca4dea1
sgkim <sgkim@highbrow.com>
2017-01-17 08:29:04 +0000

    [성구] #"전투 준비 화면"에서 사용하지 않는 lua코드와 ui node 삭제
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@72667 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_ReadyScene.lua
+++ dragonvillage/src/UI_ReadyScene.lua
@@ -125,22 +125,6 @@ function UI_ReadyScene:refresh()
         end
     end
 
-    do -- 상성에 좋은 속성 아이콘 출력
-        local table_drop = TABLE:get('drop')
-        local t_drop = table_drop[stage_id]
-        local stage_attr = t_drop['attr']
-        local l_attr = getAttrDisadvantageList(stage_attr)        
-
-        for i,v in ipairs(l_attr) do
-            local node = vars['advNode' .. i]
-
-            if node then
-                local icon = IconHelper:getAttributeIcon(v)
-                node:addChild(icon)
-            end
-        end
-    end
-
     self:refresh_tamer()
 end
 
@@ -156,7 +140,6 @@ function UI_ReadyScene:refresh_tamer()
     icon:setDockPoint(cc.p(0.5, 0.5))
     icon:setAnchorPoint(cc.p(0.5, 0.5))
     vars['tamerNode']:addChild(icon)
-    
 end
 
 -------------------------------------
