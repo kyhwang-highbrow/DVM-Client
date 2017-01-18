fe3f54572e2bfaa8d0487fe7b644a1913467404a
sgkim <sgkim@highbrow.com>
2017-01-18 10:55:59 +0000

    [성구] #FGT 0.0.8버전 assets 커밋
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@73022 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/proj.android_fgt/assets/fixed_constant.lua
+++ dragonvillage/proj.android_fgt/assets/fixed_constant.lua
@@ -1,15 +1,13 @@
-IS_EIM_TEST_VERSION = false
+-- 드빌팀 개발자가 svn상의 src경로에서 작업할때만 적용
+DEVELOPMENT_SRC_VER = true
 
--- @LIVE_QA_VERSION 상용서버일때 QA서버로 접속할지 여부
-LIVE_QA_VERSION = false
+-- 김성구의 개인 개발 버전
+DEVELOPMENT_SEONG_GOO_KIM = false
 
--- @APP_TARGET
--- 국내 버전  nil, 'KOREA'
--- GSP 버전   'GSP'
--- 중국 버전  'CHINA'
-APP_TARGET = 'GSP'
+-- 김문성 버전
+DEVELOPMENT_KMS = false
 
--- 카카오 버전일 경우
-if useKakao and (useKakao() == 1) then
-    APP_TARGET = 'KAKAO'
-end
+-- 김성진 버전
+DEVELOPMENT_KSJ = false
+
+TARGET_SERVER = 'FGT'
