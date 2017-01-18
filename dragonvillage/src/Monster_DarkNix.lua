827ab5505a08f42ef8961b8ccf6e350b079a58dd
skim <skim@highbrow.com>
2017-01-18 08:05:46 +0000

    [성진] 챕터6 보스 다크니스 - 파멸의 고동 패턴시 효과음 추가
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@72982 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/Monster_DarkNix.lua
+++ dragonvillage/src/Monster_DarkNix.lua
@@ -19,4 +19,21 @@ end
 -------------------------------------
 function Monster_DarkNix:initState()
     PARENT.initState(self)
+
+    self:addState('attack', Monster_DarkNix.st_attack, 'attack', false)
+end
+
+-------------------------------------
+-- function st_attack
+-------------------------------------
+function Monster_DarkNix.st_attack(owner, dt)
+    if (owner.m_stateTimer == 0) then
+        -- 특정 패턴 전용 효과음
+        local t_skill = owner:getSkillTable(owner.m_reservedSkillId)
+        if t_skill and t_skill['type'] == 'skill_heart_of_ruin' then
+            SoundMgr:playEffect('EFFECT', 'darknix_shout')
+        end
+    end
+
+    PARENT.st_attack(owner, dt)
 end
