43e27030417e10ff4ed34815bd9995f5838c3038
sgkim <sgkim@highbrow.com>
2017-02-01 07:23:02 +0000

    [성구] #추천 친구 리스트 출력, 친구 요청 완료
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@73714 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/ServerData_Friend.lua
+++ dragonvillage/src/ServerData_Friend.lua
@@ -54,3 +54,44 @@ end
 function ServerData_Friend:getRecommendUserList()
     return self.m_lRecommendUserList
 end
+
+-------------------------------------
+-- function request_invite
+-- @brief 친구 초대
+-------------------------------------
+function ServerData_Friend:request_invite(friend_uid, finish_cb)
+    -- 파라미터
+    local uid = g_userData:get('uid')
+
+    -- 콜백 함수
+    local function success_cb(ret)
+        self:applyInvitedRecommendUser(friend_uid)
+
+        if finish_cb then
+            finish_cb(ret)
+        end
+    end
+
+    -- 네트워크 통신 UI 생성
+    local ui_network = UI_Network()
+    ui_network:setUrl('/socials/invite')
+    ui_network:setParam('uid', uid)
+    ui_network:setParam('friend', friend_uid)
+    ui_network:setSuccessCB(success_cb)
+    ui_network:setRevocable(true)
+    ui_network:setReuse(false)
+    ui_network:request()
+end
+
+-------------------------------------
+-- function applyInvitedRecommendUser
+-- @brief 친구 초대가 완료된 항목 처리
+-------------------------------------
+function ServerData_Friend:applyInvitedRecommendUser(friend_uid)
+    local l_user_list = self:getRecommendUserList()
+    for i,v in ipairs(l_user_list) do
+        if (v['uid'] == friend_uid ) then
+            v['invited'] = true
+        end
+    end
+end
