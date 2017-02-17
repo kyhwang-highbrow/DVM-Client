7abc5e7dd78d74e08ace3f7d4dcba04dd9728858
mskim <mskim@highbrow.com>
2017-02-17 08:36:57 +0000

    [문성] ## Skill 1. 새로운 상태효과 '면역' 추가 - 구현 중
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@75010 5003bd00-c432-460b-831f-0659da9acde2

---
+++ dragonvillage/src/StatusEffect_Immune.lua
@@ -0,0 +1,46 @@
+local PARENT = StatusEffect
+
+-------------------------------------
+-- class StatusEffect_Immune
+-------------------------------------
+StatusEffect_Immune = class(PARENT, {
+    })
+
+-------------------------------------
+-- function init
+-- @param file_name
+-- @param body
+-------------------------------------
+function StatusEffect_Immune:init(file_name, body)
+end
+
+-------------------------------------
+-- function init_skill
+-------------------------------------
+function StatusEffect_Immune:init_status(char)
+	self.m_owner = char
+	char:setSilence(true)
+end
+
+-------------------------------------
+-- function initState
+-------------------------------------
+function StatusEffect_Immune:initState()
+    self:addState('start', StatusEffect.st_start, 'center_start', false)
+    self:addState('idle', StatusEffect.st_idle, 'center_idle', true)
+    self:addState('end', StatusEffect_Immune.st_end, 'center_end', false)
+	self:addState('dying', function(owner, dt) return true end, nil, nil, 10)
+end
+
+-------------------------------------
+-- function st_end
+-------------------------------------
+function StatusEffect_Immune.st_end(owner, dt)
+	if (owner.m_stateTimer == 0) then
+        owner:statusEffectReset()
+		owner:addAniHandler(function()
+			owner.m_owner:setSilence(false)
+			owner:changeState('dying')
+		end)
+    end
+end
