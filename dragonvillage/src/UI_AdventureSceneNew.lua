098a6d663d6fc4eaf880c42827e19773a2904771
sgkim <sgkim@highbrow.com>
2017-02-22 05:15:36 +0000

    [성구] #6챕터 스테이지 선택 배경 적용(map_dark_castle)
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@75377 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_AdventureSceneNew.lua
+++ dragonvillage/src/UI_AdventureSceneNew.lua
@@ -4,8 +4,7 @@ T_CHAPTER_MAP_RES[2] = 'res/bg/map_ocean/map_ocean.vrp'
 T_CHAPTER_MAP_RES[3] = 'res/bg/map_canyon/map_canyon.vrp'
 T_CHAPTER_MAP_RES[4] = 'res/bg/map_volcano/map_volcano.vrp'
 T_CHAPTER_MAP_RES[5] = 'res/bg/map_sky_temple/map_sky_temple.vrp'
-
-T_CHAPTER_MAP_RES[6] = 'res/bg/world_map/chapter_03.png'
+T_CHAPTER_MAP_RES[6] = 'res/bg/map_dark_castle/map_dark_castle.vrp'
 
 -------------------------------------
 -- class UI_AdventureSceneNew
