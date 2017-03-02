f56c04ce016fb5c1c160e6fd262275d722d44bbd
mskim <mskim@highbrow.com>
2017-03-02 03:05:35 +0000

    [문성] ## 1. 테이머 스킬 테이블 새로 작성된것으로 교체 및 이전 테이블 정리
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@75915 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/TableTamerSkill.lua
+++ dragonvillage/src/TableTamerSkill.lua
@@ -10,7 +10,7 @@ TableTamerSkill = class(PARENT, {
 -- function init
 -------------------------------------
 function TableTamerSkill:init()
-    self.m_tableName = 'tamer_skill_new'
+    self.m_tableName = 'tamer_skill'
     self.m_orgTable = TABLE:get(self.m_tableName)
 end
 
