c6f4cad462026011e4ca3377df021fbc8ebd6fa4
mskim <mskim@highbrow.com>
2017-03-08 05:15:16 +0000

    [문성] ## UI 1. 루아 에러 띄워주는 팝업 중복 등장안하도록 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@76246 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_ErrorPopup.lua
+++ dragonvillage/src/UI_ErrorPopup.lua
@@ -21,38 +21,110 @@ function UI_ErrorPopup:init(str)
     UIManager:open(self, UIManager.POPUP, true)
 
     -- backkey 지정
-    g_currScene:pushBackKeyListener(self, function() self:close() end, 'UI_ErrorPopup')
+    g_currScene:pushBackKeyListener(self, function() self:click_exitBtn() end, 'UI_ErrorPopup')
 
-	self:initDebugLayer(str)
+	self:initUI()
+	self:initButton()
+	self:setErrorStr(str)
 end
 
 -------------------------------------
 -- function initDebugLayer
 -------------------------------------
-function UI_ErrorPopup:initDebugLayer(str)
+function UI_ErrorPopup:initUI(str)
 	local scr_size = cc.Director:getInstance():getWinSize()
 	
-	local rect = cc.rect(0, 0, 0, 0)
-	self.m_backLayer = cc.Scale9Sprite:create(rect, 'res/ui/toast_notification.png')
-	self.m_backLayer:setDockPoint(cc.p(0.5, 0.5))
-    self.m_backLayer:setAnchorPoint(cc.p(0.5, 0.5))
-	self.m_backLayer:setNormalSize(scr_size)
-	self.m_backLayer:setOpacity(200)
-	self.root:addChild(self.m_backLayer, 1)
-	
-	-- error label
-	local error_str = str or '???'
-	
-	self.m_errorLabel = cc.Label:createWithTTF(
-		error_str,
-		'res/font/common_font_01.ttf', 
-		20, 
-		1, -- stroke
-		cc.size(scr_size['width'] - 200, scr_size['height']), 
-		cc.TEXT_ALIGNMENT_LEFT, 
-		cc.VERTICAL_TEXT_ALIGNMENT_CENTER)
+	-- 검은색 배경
+	do
+		local rect = cc.rect(0, 0, 0, 0)
+		self.m_backLayer = cc.Scale9Sprite:create(rect, 'res/ui/toast_notification.png')
+		self.m_backLayer:setDockPoint(cc.p(0.5, 0.5))
+		self.m_backLayer:setAnchorPoint(cc.p(0.5, 0.5))
+		self.m_backLayer:setNormalSize(scr_size)
+		self.m_backLayer:setOpacity(200)
+		self.root:addChild(self.m_backLayer, 0)
+	end
 
-	self.m_errorLabel:setDockPoint(cc.p(0.5, 0.5))
-    self.m_errorLabel:setAnchorPoint(cc.p(0.5, 0.5))
-	self.m_backLayer:addChild(self.m_errorLabel)
+	-- error label
+	do
+		self.m_errorLabel = cc.Label:createWithTTF(
+			'',
+			'res/font/common_font_01.ttf', 
+			20, 
+			1, -- stroke
+			cc.size(scr_size['width'] - 200, scr_size['height']), 
+			cc.TEXT_ALIGNMENT_LEFT, 
+			cc.VERTICAL_TEXT_ALIGNMENT_CENTER)
+
+		self.m_errorLabel:setDockPoint(cc.p(0.5, 0.5))
+		self.m_errorLabel:setAnchorPoint(cc.p(0.5, 0.5))
+		self.m_backLayer:addChild(self.m_errorLabel)
+	end
+
+	-- 복사 버튼
+	do
+	    local node = cc.MenuItemImage:create('res/ui/btn/base_0206.png', 'res/ui/btn/base_0207.png', 1)
+        node:setDockPoint(cc.p(0.5, 0))
+        node:setAnchorPoint(cc.p(0.5, 0.5))
+        node:setPosition(-100, 50)
+        UIC_Button(node)
+        self.root:addChild(node, 1)
+        self.vars['copyBtn'] = node
+
+		local label = cc.Label:createWithTTF('복사',	'res/font/common_font_01.ttf', 20, 1, cc.size(100, 50),	cc.TEXT_ALIGNMENT_CENTER, cc.VERTICAL_TEXT_ALIGNMENT_CENTER)
+		label:setDockPoint(CENTER_POINT)
+        label:setAnchorPoint(CENTER_POINT)
+		self.vars['copyBtn']:addChild(label)
+	end
+
+	-- 닫기 버튼
+	do
+	    local node = cc.MenuItemImage:create('res/ui/btn/base_0206.png', 'res/ui/btn/base_0207.png', 1)
+        node:setDockPoint(cc.p(0.5, 0))
+        node:setAnchorPoint(cc.p(0.5, 0.5))
+        node:setPosition(100, 50)
+        UIC_Button(node)
+        self.root:addChild(node, 1)
+        self.vars['exitBtn'] = node
+
+		local label = cc.Label:createWithTTF('닫기',	'res/font/common_font_01.ttf', 20, 1, cc.size(100, 50),	cc.TEXT_ALIGNMENT_CENTER, cc.VERTICAL_TEXT_ALIGNMENT_CENTER) 
+		label:setDockPoint(CENTER_POINT)
+        label:setAnchorPoint(CENTER_POINT)
+		self.vars['exitBtn']:addChild(label)
+	end
 end
+
+-------------------------------------
+-- function initButton
+-------------------------------------
+function UI_ErrorPopup:initButton()
+	self.vars['copyBtn']:registerScriptTapHandler(function() self:click_copyBtn() end)
+	self.vars['exitBtn']:registerScriptTapHandler(function() self:click_exitBtn() end)
+end
+
+-------------------------------------
+-- function setErrorStr
+-------------------------------------
+function UI_ErrorPopup:setErrorStr(str)
+	local error_str = string.gsub(str, '\t', '    ') or '???'
+	self.m_errorLabel:setString(error_str)
+end
+
+-------------------------------------
+-- function click_copyBtn
+-------------------------------------
+function UI_ErrorPopup:click_copyBtn()
+	UIManager:toastNotificationGreen('클립보드 복사 기능 준비중')
+end
+
+
+-------------------------------------
+-- function click_exitBtn
+-------------------------------------
+function UI_ErrorPopup:click_exitBtn()
+    self:close()
+	IS_OPEN_ERROR_POPUP = false
+end
+
+
+__G__ERROR_POPUP = UI_ErrorPopup
--- dragonvillage/src/main.lua
+++ dragonvillage/src/main.lua
@@ -7,16 +7,37 @@ print('################################################################')
 print('[Lua Start!!!]')
 print('################################################################')
 
-require 'require'
-loadModule()
-
-
 -------------------------------------
 -- function cclog
 -------------------------------------
 cclog = function(...)
     print(...)
 end
+
+__G__ERROR_POPUP = nil
+IS_OPEN_ERROR_POPUP = false
+-------------------------------------
+-- function __G__TRACKBACK__
+-- @brief for CCLuaEngine traceback
+-------------------------------------
+function __G__TRACKBACK__(msg)
+	local error_msg = "LUA ERROR: " .. tostring(msg) .. "\n\n" .. debug.traceback()
+	
+	-- 에러를 팝업으로 띄워서 출력
+	-- @TODO 디버깅 모드 처리해야함
+	if (not IS_OPEN_ERROR_POPUP) and __G__ERROR_POPUP then
+		__G__ERROR_POPUP(error_msg)
+		IS_OPEN_ERROR_POPUP = true
+	end
+
+    cclog("----------------------------------------")
+    cclog(error_msg)
+    cclog("----------------------------------------")
+    return msg
+end
+
+require 'require'
+loadModule()
 
 -------------------------------------
 -- function applicationDidEnterBackground
@@ -54,24 +75,7 @@ function closeApplication()
 	cc.Director:getInstance():endToLua()
 end
 
--------------------------------------
--- function __G__TRACKBACK__
--- @brief for CCLuaEngine traceback
--------------------------------------
-function __G__TRACKBACK__(msg)
-	local error_msg = "LUA ERROR: " .. tostring(msg) .. "\n\n" .. debug.traceback()
-	
-	-- 에러를 팝업으로 띄워서 출력 \
-	-- @TODO 디버깅 모드 처리해야함
-	do
-		UI_ErrorPopup(error_msg)
-	end
 
-    cclog("----------------------------------------")
-    cclog(error_msg)
-    cclog("----------------------------------------")
-    return msg
-end
 
 -------------------------------------
 -- function main
