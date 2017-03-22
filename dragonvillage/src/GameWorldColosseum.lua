e04b36294c2a5262585803df0a0320e31f95d1e1
mskim <mskim@highbrow.com>
2017-03-22 11:43:59 +0000

    [문성] ##LOG 1. 콜로세움 월드 쪽에서 logRecorder init 순서 변경
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@77482 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/GameWorldColosseum.lua
+++ dragonvillage/src/GameWorldColosseum.lua
@@ -46,6 +46,9 @@ function GameWorldColosseum:initGame(stage_name)
     -- 위치 표시 이펙트 생성
     self:init_formation()
 
+	-- Game Log Recorder 생성
+	self.m_logRecorder = LogRecorderWorld(self)
+
     -- 테이머 생성
     self:initTamer()
 
@@ -54,9 +57,6 @@ function GameWorldColosseum:initGame(stage_name)
 
     -- 적군 덱에 세팅된 드래곤 생성
     self:makeEnemyDeck()
-
-	-- Game Log Recorder 생성
-	self.m_logRecorder = LogRecorderWorld(self)
 
     -- 진형 시스템 초기화
     self:setBattleZone(self.m_deckFormation, true)
