fdb4abeb0c1dabac8a3d68f79cb939cc5981ab20
sgkim <sgkim@highbrow.com>
2017-04-03 06:22:54 +0000

    [성구] #드래곤 연구 구현 중
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@78409 5003bd00-c432-460b-831f-0659da9acde2

---
+++ dragonvillage/src/TableDragonResearch.lua
@@ -0,0 +1,56 @@
+local PARENT = TableClass
+
+-------------------------------------
+-- class TableDragonResearch
+-------------------------------------
+TableDragonResearch = class(PARENT, {
+    })
+
+local THIS = TableDragonResearch
+
+-------------------------------------
+-- function init
+-------------------------------------
+function TableDragonResearch:init()
+    self.m_tableName = 'table_dragon_research'
+    self.m_orgTable = TABLE:get(self.m_tableName)
+end
+
+-------------------------------------
+-- function getDragonResearchPrice
+-------------------------------------
+function TableDragonResearch:getDragonResearchPrice(research_lv)
+    if (self == THIS) then
+        self = THIS()
+    end
+
+    local price = self:getValue(research_lv, 'price')
+    return price or 0
+end
+
+-------------------------------------
+-- function getDragonResearchStatus
+-------------------------------------
+function TableDragonResearch:getDragonResearchStatus(dragon_type, research_lv)
+    if (self == THIS) then
+        self = THIS()
+    end
+
+    local base_did = TableDragonType:getBaseDid(dragon_type)
+
+    local table_dragon  = TableDragon()
+
+    local atk_max = table_dragon:getMaxStatus(base_did, 'atk')
+    local def_max = table_dragon:getMaxStatus(base_did, 'def')
+    local hp_max = table_dragon:getMaxStatus(base_did, 'hp')
+
+    local atk_rate = self:getValue(research_lv, 'atk') / 100
+    local def_rate = self:getValue(research_lv, 'def') / 100
+    local hp_rate = self:getValue(research_lv, 'hp') / 100
+
+    local atk = (atk_max * atk_rate)
+    local def = (def_max * def_rate)
+    local hp = (hp_max * hp_rate)
+
+    return atk, def, hp
+end
