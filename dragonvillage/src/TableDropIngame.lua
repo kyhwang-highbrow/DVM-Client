97291719d9e470a47adb8fc73978adf5779e760e
sgkim <sgkim@highbrow.com>
2017-04-11 07:12:19 +0000

    [성구] #인게임 재화 드랍에 자수정 타입 추가
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@79287 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/TableDropIngame.lua
+++ dragonvillage/src/TableDropIngame.lua
@@ -67,6 +67,7 @@ function TableDropIngame:decideDropItem(chapter_id)
     sum_random:addItem(t_table['cash_weight'], 'cash')
     sum_random:addItem(t_table['gold_weight'], 'gold')
     sum_random:addItem(t_table['lactea_weight'], 'lactea')
+    sum_random:addItem(t_table['amethyst_weight'] or 0, 'amethyst')
 
     local item_type = sum_random:getRandomValue()
 
