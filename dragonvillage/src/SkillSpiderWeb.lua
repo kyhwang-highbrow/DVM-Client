053c27007187ce06aa051063c6889f62ce25c75c
mskim <mskim@highbrow.com>
2017-04-13 05:35:13 +0000

    [문성] ## Skill 1. 4챕터 보스 스킬 동작 로직 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@79736 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/SkillSpiderWeb.lua
+++ dragonvillage/src/SkillSpiderWeb.lua
@@ -21,12 +21,12 @@ end
 -------------------------------------
 function SkillSpiderWeb:init_skill()
 	PARENT.init_skill(self)
+
 	local struct_status_effect = self.m_lStatusEffect[1]
 	self.m_statusDuration = struct_status_effect.m_duration
 	self.m_statusName = struct_status_effect.m_type
 
 	self:setPosition(self.m_targetChar.pos.x, self.m_targetChar.pos.y)
-	self.m_animator:setVisible(false) 
 end
 
 -------------------------------------
@@ -45,8 +45,10 @@ end
 function SkillSpiderWeb.st_appear(owner, dt)
 	if (owner.m_stateTimer == 0) then
 		owner.m_animator:addAniHandler(function()
-			owner.m_targetChar.m_animator:setVisible(false)
+			--owner.m_targetChar.m_animator:setVisible(false)
 			owner:changeState('idle')
+			-- 상태효과
+			owner:dispatch(CON_SKILL_HIT, {l_target = {owner.m_targetChar}})
 		end)
 	end
 end
@@ -56,11 +58,23 @@ end
 -------------------------------------
 function SkillSpiderWeb.st_idle(owner, dt)
 	if (owner.m_stateTimer == 0) then
-		-- 상태효과
-        owner:dispatch(CON_SKILL_HIT, {l_target = {owner.m_targetChar}})
+		owner.m_targetChar.m_animator:setVisible(false)
 
 	elseif (owner.m_stateTimer > owner.m_statusDuration) then
 		owner:changeState('end')
+
+	else
+		-- 해당 상태효과가 제거되었는지 체크
+		local isExist = false
+		for i, v  in pairs(owner.m_targetChar:getStatusEffectList()) do
+			if (owner.m_statusName == v:getTypeName()) then 
+				isExist = true
+			end 
+		end
+
+		if (not isExist) then
+			owner:changeState('end')
+		end
 	end
 end
 
@@ -83,19 +97,7 @@ function SkillSpiderWeb:update(dt)
 	if (self.m_state ~= 'dying') then
 		-- 타겟 사망 체크
 		if (self.m_targetChar.m_bDead) then
-			self:changeState('end')
-		end
-	end
-	if (self.m_state == 'idle') then 
-		-- 해당 상태효과가 제거되었는지 체크
-		local isExist = false
-		for i, v  in pairs(self.m_targetChar:getStatusEffectList()) do 
-			if (self.m_statusName == v:getTypeName()) then 
-				isExist = true
-			end 
-		end
-		if (not isExist) then
-			self:changeState('end')
+			self:changeState('dying')
 		end
 	end
 
@@ -113,7 +115,7 @@ function SkillSpiderWeb:makeSkillInstance(owner, t_skill, t_data)
 	-- 변수 선언부
 	------------------------------------------------------
 	local missile_res = SkillHelper:getAttributeRes(t_skill['res_1'], owner)	  -- 광역 스킬 리소스
-	cclog('123123123')
+
 	-- 인스턴스 생성부
 	------------------------------------------------------
 	-- 1. 스킬 생성
