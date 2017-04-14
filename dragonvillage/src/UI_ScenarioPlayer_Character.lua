5c3dd39e21c119e46d7d6702dfbf8469176434fd
sgkim <sgkim@highbrow.com>
2017-04-14 06:12:05 +0000

    [성구] #캐릭터 시나리오 리소스 테이블로 분리
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@79976 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_ScenarioPlayer_Character.lua
+++ dragonvillage/src/UI_ScenarioPlayer_Character.lua
@@ -1,13 +1,3 @@
-local T_CHAR_RES = {}
-T_CHAR_RES['queenssnake'] = 134011
-T_CHAR_RES['owl'] = 131031
-T_CHAR_RES['drak_robe'] = 132062
-T_CHAR_RES['drak_robe_executive'] = 132062
-
-local T_TAMER_I_RES = {}
-T_TAMER_I_RES['dale_i'] = 'res/character/tamer/dale_i/dale_i.spine'
-
-
 -------------------------------------
 -- class UI_ScenarioPlayer_Character
 -------------------------------------
@@ -78,67 +68,34 @@ end
 -- function setCharacter
 -------------------------------------
 function UI_ScenarioPlayer_Character:setCharacter(key)
-   if (self.m_charKey == key) then
+    if (self.m_charKey == key) then
         return
-   end
+    end
 
-   self.m_charKey = key
+    self.m_charKey = key
 
-   if (self.m_charAnimator) then
-    self.m_charAnimator:release()
-   end
+    if (self.m_charAnimator) then
+        self.m_charAnimator:release()
+    end
 
-    key = tostring(key)
-    
-    if T_CHAR_RES[key] then
+
+    local res = TableScenarioResource:getScenarioRes(key)
+    local type = TableScenarioResource:getScenarioResType(key)
+
+
+    if type and (type == 'monster') then
         self.m_charAnimator = MakeAnimator(res)
-        self.m_charAnimator:setAlpha(0)
-        self.m_charAnimator:runAction(cc.FadeIn:create(0.3))
-        self.m_charNode:addChild(self.m_charAnimator.m_node)
         self.m_charAnimator:setPositionY(200)
-
-        self.m_charAnimator:setFlip(self.m_bCharFlip)
-
-        self:show()
-    end
-
-
-
-    local is_number = false
-    do       
-        local temp = string.match(key, '%d+[.]?%d*')
-        if temp then
-            temp = tostring(tonumber(key))
-        end
-        ccdump(temp)
-        if (key == temp) then
-            is_number = true
-        end
-    end
-
-    if is_number then
-        local monster_id = tonumber(key)
-        local res, attr = TableMonster:getMonsterRes(monster_id)
-        self.m_charAnimator = AnimatorHelper:makeMonsterAnimator(res, attr)
-        self.m_charAnimator:setAlpha(0)
-        self.m_charAnimator:runAction(cc.FadeIn:create(0.3))
-        self.m_charNode:addChild(self.m_charAnimator.m_node)
-        self.m_charAnimator:setPositionY(200)
-        self.m_charAnimator:setScale(1.3)
     else
-        local res = string.format('res/character/tamer/%s/%s.spine', key, key)
-        self.m_charAnimator = MakeAnimator(res)
-        self.m_charAnimator:setAlpha(0)
-        self.m_charAnimator:runAction(cc.FadeIn:create(0.3))
-        self.m_charNode:addChild(self.m_charAnimator.m_node)
+        self.m_charAnimator = MakeAnimator(res) 
     end
 
+    self.m_charAnimator:setAlpha(0)
+    self.m_charAnimator:runAction(cc.FadeIn:create(0.3))
+    self.m_charNode:addChild(self.m_charAnimator.m_node)
+    self.m_charAnimator:setFlip(self.m_bCharFlip)
 
-
-
-   self.m_charAnimator:setFlip(self.m_bCharFlip)
-
-   self:show()
+    self:show()
 end
 
 -------------------------------------
