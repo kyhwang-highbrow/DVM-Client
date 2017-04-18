c656e75532a74d6a4ce714b46b0d71ba67d6643e
sgkim <sgkim@highbrow.com>
2017-04-18 02:11:36 +0000

    [성구] #스테이지 선택화면 배경 적용 7~12챕터
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@80204 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_AdventureSceneNew.lua
+++ dragonvillage/src/UI_AdventureSceneNew.lua
@@ -5,12 +5,12 @@ T_CHAPTER_MAP_RES[3] = 'res/bg/map_canyon/map_canyon.vrp'
 T_CHAPTER_MAP_RES[4] = 'res/bg/map_volcano/map_volcano.vrp'
 T_CHAPTER_MAP_RES[5] = 'res/bg/map_sky_temple/map_sky_temple.vrp'
 T_CHAPTER_MAP_RES[6] = 'res/bg/map_dark_castle/map_dark_castle.vrp'
-T_CHAPTER_MAP_RES[7] = 'res/bg/map_volcano/map_volcano.vrp'
-T_CHAPTER_MAP_RES[8] = 'res/bg/map_canyon/map_canyon.vrp'
-T_CHAPTER_MAP_RES[9] = 'res/bg/map_ocean/map_ocean.vrp'
-T_CHAPTER_MAP_RES[10] = 'res/bg/map_forest/map_forest.vrp'
-T_CHAPTER_MAP_RES[11] = 'res/bg/map_sky_temple/map_sky_temple.vrp'
-T_CHAPTER_MAP_RES[12] = 'res/bg/map_dark_castle/map_dark_castle.vrp'
+T_CHAPTER_MAP_RES[7] = 'res/bg/map_volcano2/map_volcano2.vrp'
+T_CHAPTER_MAP_RES[8] = 'res/bg/map_canyon2/map_canyon2.vrp'
+T_CHAPTER_MAP_RES[9] = 'res/bg/map_ocean2/map_ocean2.vrp'
+T_CHAPTER_MAP_RES[10] = 'res/bg/map_forest2/map_forest2.vrp'
+T_CHAPTER_MAP_RES[11] = 'res/bg/map_sky_temple2/map_sky_temple2.vrp'
+T_CHAPTER_MAP_RES[12] = 'res/bg/map_dark_castle2/map_dark_castle2.vrp'
 
 -------------------------------------
 -- class UI_AdventureSceneNew
