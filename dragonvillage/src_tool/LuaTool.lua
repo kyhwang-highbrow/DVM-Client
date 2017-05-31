5157d5182ba88262be54e37dc1ffd81d83ad8cc5
sgkim <sgkim@highbrow.com>
2017-05-31 03:18:56 +0000

    [성구] #윈도우에서 lua.exe stand alone을 활용해 개발할수있는 환경 조성
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@83254 5003bd00-c432-460b-831f-0659da9acde2

---
+++ dragonvillage/src_tool/LuaTool.lua
@@ -0,0 +1,47 @@
+-- penlight 라이브러리 로드
+pl = require 'pl.import_into'()
+
+-- ../srs경로를 package.path에 추가
+pl.app.require_here('../res')
+pl.app.require_here('../src')
+pl.app.require_here('..')
+pl.app.require_here()
+
+csv = require 'perpleLib/lua_csv'
+require 'LuaBridgeWindows'
+require 'lib/class'
+require 'perpleLib/StringUtils'
+require 'Table'
+require 'TableClass'
+require 'TableGradeInfo'
+
+-------------------------------------
+-- function main
+-- @brief
+-------------------------------------
+function main()
+    TABLE:init()
+
+    -- 잘 동작하는지 테스트 코드
+    local table_grade_info = TableGradeInfo()
+    ccdump(table_grade_info)
+end
+
+-------------------------------------
+-- function __G__TRACKBACK__
+-- @brief for CCLuaEngine traceback
+-------------------------------------
+function __G__TRACKBACK__(msg)
+	local error_msg = "LUA ERROR: " .. tostring(msg) .. "\n\n" .. debug.traceback()
+
+    cclog("----------------------------------------")
+    cclog(error_msg)
+    cclog("----------------------------------------")
+
+    return msg
+end
+
+local status, msg = xpcall(main, __G__TRACKBACK__)
+if not status then
+    error(msg)
+end
