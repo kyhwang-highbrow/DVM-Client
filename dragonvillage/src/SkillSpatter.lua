30e64b66b0509e33d7be5657274b371b198a393a
skim <skim@highbrow.com>
2017-06-16 07:54:51 +0000

    [성진] skill_spatter(퐁당퐁당) 스킬의 드래그 스킬화 임시 처리
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@84071 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/SkillSpatter.lua
+++ dragonvillage/src/SkillSpatter.lua
@@ -59,6 +59,9 @@ end
 -- function initState
 -------------------------------------
 function SkillSpatter:initState()
+    if (self.m_bSkillHitEffect) then
+        self:setCommonState(self)
+    end
     self:addState('idle', SkillSpatter.st_idle, 'idle', true)
     self:addState('dying', function(owner, dt) return true end, nil, nil, 10)
 end
