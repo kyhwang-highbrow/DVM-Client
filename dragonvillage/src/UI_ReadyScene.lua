a97032be3c6b81f1470e5cb92c7daf0fe7211fad
mskim <mskim@highbrow.com>
2017-06-20 14:09:42 +0000

    [문성] ## 1. 리더 버프 있는 드래곤 확인 코드 정리
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@84254 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_ReadyScene.lua
+++ dragonvillage/src/UI_ReadyScene.lua
@@ -859,17 +859,11 @@ function UI_ReadyScene:click_leaderBtn()
 
 	-- 리더버프 있는 드래곤 체크
 	do
-		local have_leader_skill = false
 		local cnt = 0
 		for _, doid in pairs(l_doid) do
 			if (g_dragonsData:haveLeaderSkill(doid)) then
-				have_leader_skill = true
 				cnt = cnt + 1
 			end
-		end
-		if (not have_leader_skill) then
-			UIManager:toastNotificationRed('리더 버프를 가지고 있는 드래곤이 없습니다.')
-			return
 		end
 		if (cnt < 2) then
 			UIManager:toastNotificationRed('리더 버프 있는 드래곤이 2마리 이상이어야 활성화됩니다.')
