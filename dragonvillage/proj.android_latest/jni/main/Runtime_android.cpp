0418acb8f5c1592888e2b758fe7ee8d2a3281da3
sgkim <sgkim@highbrow.com>
2017-06-29 09:27:34 +0000

    [성구] #패키지명 변경 com.perplelab.dragonvillage -> com.perplelab.dragonvillagem.kr #안드로이드 빌드 keystore 적용
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@84984 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/proj.android_latest/jni/main/Runtime_android.cpp
+++ dragonvillage/proj.android_latest/jni/main/Runtime_android.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 #include "LoginPlatform.h"
 
-#define PACKAGE_NAME    "com/perplelab/dragonvillage/AppActivity"
+#define PACKAGE_NAME    "com/perplelab/dragonvillagem/kr/AppActivity"
 
 using namespace std;
 using namespace cocos2d;
