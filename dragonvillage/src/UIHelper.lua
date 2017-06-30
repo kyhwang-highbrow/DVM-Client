8b06d8b6915a71f340586c375a2243678d239ae1
mskim <mskim@highbrow.com>
2017-06-30 09:17:49 +0000

    [문성] ## 1. UI 반복 테스트 용 함수와 해제 -> 반복 테스트 실행 : UIHelper:repeatTest() -> 해제 : UIManager -> key event D
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@85094 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UIHelper.lua
+++ dragonvillage/src/UIHelper.lua
@@ -22,3 +22,32 @@ function UIHelper:makePriceNodeVariable(bg_node, icon_node, price_label)
     local _, height = bg_node:getNormalSize()
     bg_node:setNormalSize(total_width, height)
 end
+
+-------------------------------------
+-- function makePriceNodeVariable
+-- @brief 반복 테스트용
+-------------------------------------
+function UIHelper:repeatTest(sequence_action)
+	g_currScene.m_scene:runAction(cc.RepeatForever:create(sequence_action))
+
+	-- ex)
+	--[[
+	local ui
+	local sequence_action = cc.Sequence:create(
+		cc.CallFunc:create(function()
+			ccdisplay('디버깅 디버깅')
+			ui = UI_ReadyScene(COLOSSEUM_STAGE_ID, nil, 'atk')
+			local function close_cb()
+				local player_3d_deck = self.m_player3DDeck
+				local l_dragon_obj = g_colosseumData.m_playerUserInfo:getAtkDeck_dragonList()
+				player_3d_deck:setDragonObjectList(l_dragon_obj)
+			end
+			ui:setCloseCB(close_cb) 
+		end),
+		cc.DelayTime:create(1.0),
+		cc.CallFunc:create(function() ui:close() end),
+		cc.DelayTime:create(1.0)
+	)
+	UIHelper:repeatTest(sequence_action)
+	]]
+end
--- dragonvillage/src/uilib/UIManager.lua
+++ dragonvillage/src/uilib/UIManager.lua
@@ -539,5 +539,10 @@ function UIManager:onKeyReleased(keyCode, event)
 			self.m_debugUI.m_debugLayer:setVisible(g_constant:get('DEBUG', 'DISPLAY_DEBUG_INFO'))
 		end
 		PrintMemory()
+
+	-- currScene에 반복 액션으로 테스트 중인 경우 해제를 위해서 등록
+	elseif (keyCode == KEY_D) then
+		g_currScene.m_scene:stopAllActions()
+
 	end
 end
