4e8d3e46139d9a1bb38062f4c02a463206871768
mskim <mskim@highbrow.com>
2017-07-03 05:34:45 +0000

    [문성] ## 1. 다량의 재화 획득 팝업 -> l_item_list에 따라 단일 또는 리스트로 출력
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@85137 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_ObtainPopup.lua
+++ dragonvillage/src/UI_ObtainPopup.lua
@@ -50,9 +50,7 @@ function UI_ObtainPopup:initUI()
 			end
 		end
 	else
-		-- make table
-
-		-- set item list
+		self:makeObtainTableView()
 	end
 end
 
@@ -73,6 +71,23 @@ function UI_ObtainPopup:refresh()
 end
 
 -------------------------------------
+-- function makeObtainTableView
+-------------------------------------
+function UI_ObtainPopup:makeObtainTableView()
+	local node = self.vars['listNode']
+	local function create_func(t_data)
+		return self:createListUI(t_data)
+	end
+	
+    -- 테이블 뷰 인스턴스 생성
+    local table_view = UIC_TableView(node)
+    table_view.m_defaultCellSize = cc.size(550, 105)
+    table_view:setCellUIClass(create_func, nil)
+    table_view:setDirection(cc.SCROLLVIEW_DIRECTION_VERTICAL)
+    table_view:setItemList(self.m_lItemList)
+end
+
+-------------------------------------
 -- function click_okBtn
 -------------------------------------
 function UI_ObtainPopup:click_okBtn()
@@ -83,6 +98,28 @@ function UI_ObtainPopup:click_okBtn()
     self:closeWithAction()
 end
 
+-------------------------------------
+-- function click_okBtn
+-------------------------------------
+function UI_ObtainPopup:createListUI(t_data)
+	local ui = class(UI, ITableViewCell:getCloneTable())()
+	local vars = ui:load('obtain_item.ui')
+	local item_id = t_data['item_id']
+	local item_cnt = t_data['count']
+		
+	-- item icon
+	local item_card = UI_ItemCard(item_id, item_cnt)
+	if (item_card) then
+		vars['itemNode']:addChild(item_card.root)
+	end
+
+	-- item label
+	local item_name = TableItem:getItemName(item_id)
+	local item_str = Str('{1} {2}개를 획득했습니다.', item_name, item_cnt)
+	vars['countLabel']:setString(item_str)
+
+	return ui
+end
 
 --@CHECK
 UI:checkCompileError(UI_ObtainPopup)
