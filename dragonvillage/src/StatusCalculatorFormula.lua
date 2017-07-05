101b2bcfbd5c26876039e8a273fe5b789c5b1e53
mskim <mskim@highbrow.com>
2017-07-05 11:43:39 +0000

    [문성] ## 1. dragon table에 max 수치가 없을 경우 발생하는 에러 tracing log 추가
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@85380 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/StatusCalculatorFormula.lua
+++ dragonvillage/src/StatusCalculatorFormula.lua
@@ -22,8 +22,13 @@ function StatusCalculator:calcStat(char_type, cid, status_name, lv, grade, evolu
     local eclv_stat = 0
 
 	-- 2-1. 공방체 스탯만 레벨에 따라 증가시키고 나머지는 고정값이다. 
-	if isExistValue(status_name, 'atk', 'def', 'hp') then 
+	if isExistValue(status_name, 'atk', 'def', 'hp') then
         local max_lv_value = t_char[status_name .. '_max']
+        
+        if (not max_lv_value) or (max_lv_value == '') then
+            error(string.format('%s의 %s_max 스탯에 문제가 있어 에러가 발생했습니다.', t_char['t_name'], status_name))
+        end
+
         local value_per_level = (max_lv_value / 208) -- @ commnet 최대 레벨이 바뀔때마다 수정해야하니 외부에서 참조하도록...
 		lv_stat = value_per_level * lv
 
