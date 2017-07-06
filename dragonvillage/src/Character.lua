5d12c99c788db72be105896ef0b9765def73b09b
mkang <mkang@highbrow.com>
2017-07-06 05:42:14 +0000

    [민승] 방어 관통 스탯 추가.
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@85433 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/Character.lua
+++ dragonvillage/src/Character.lua
@@ -505,7 +505,11 @@ function Character:undergoAttack(attacker, defender, i_x, i_y, body_key, no_even
 		-- 공격력, 방어력 스탯
 		atk_dmg = attack_activity_carrier:getAtkDmg(defender)
         def_pwr = self:getStat('def')
-		
+
+        -- 방어 관통 적용
+        if(attacker) then
+            def_pwr = def_pwr - (def_pwr * attacker_char:getStat('pierce') / 100)
+        end
 		-- 스킬 계수 적용
 		atk_dmg = atk_dmg * attack_activity_carrier:getPowerRate()
         
