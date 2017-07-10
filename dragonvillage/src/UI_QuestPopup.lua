53834c8a5467a6e880f189e8c28a89e97c825100
sgkim <sgkim@highbrow.com>
2017-07-10 07:16:38 +0000

    [성구] #퀘스트UI에서 리스트뷰 간격 조정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@85795 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_QuestPopup.lua
+++ dragonvillage/src/UI_QuestPopup.lua
@@ -115,7 +115,7 @@ function UI_QuestPopup:makeQuestTableView(tab, node)
 
         -- 테이블 뷰 인스턴스 생성
         local table_view = UIC_TableView(node)
-        table_view.m_defaultCellSize = cc.size(1160, 108)
+        table_view.m_defaultCellSize = cc.size(1160 + 10, 108)
         table_view:setCellUIClass(UI_QuestListItem, create_cb_func)
         table_view:setDirection(cc.SCROLLVIEW_DIRECTION_VERTICAL)
         table_view:setItemList(t_quest)
