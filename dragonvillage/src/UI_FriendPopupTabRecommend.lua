fccd5817fc3c67dee1d6938b80a1cf9d6436c09a
mskim <mskim@highbrow.com>
2017-07-10 12:46:05 +0000

    [문성] ## 1. 친구 요청 안되는 것 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@85843 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_FriendPopupTabRecommend.lua
+++ dragonvillage/src/UI_FriendPopupTabRecommend.lua
@@ -146,7 +146,7 @@ function UI_FriendPopupTabRecommend:click_requestBtn(t_friend_info)
     local friend_ui = t_friend_info.m_uid
 
     local function finish_cb(ret)
-        local msg = Str('[{1}]에게 친구 요청을 하였습니다.', t_friend_info.m_nick)
+        local msg = Str('[{1}]에게 친구 요청을 하였습니다.', t_friend_info.m_nickname)
         UIManager:toastNotificationGreen(msg)
         self.m_tableView:delItem(friend_ui)
     end
