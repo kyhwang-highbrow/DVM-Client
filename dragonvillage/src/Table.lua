044633fed68e1a2531c1d18a607aa88cbc0bfcb8
klee <klee@highbrow.com>
2017-07-13 08:38:34 +0000

    [규석] 고대의탑 랭킹정보 테이블 변경
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@86228 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/Table.lua
+++ dragonvillage/src/Table.lua
@@ -43,8 +43,7 @@ local TableInfo = {
 
 		quest			    = {'table_quest',			    'qid',			false},
 		master_road			= {'table_master_road',			'rid',			false},
-        anc_floor_reward    = {'table_ancient_floor_reward','stage',		false},
-        anc_rank_reward     = {'table_ancient_rank_reward',	'rank_id',		false},
+        anc_floor_reward    = {'table_ancient_reward',      'stage',		false},
         anc_weak_debuff     = {'table_ancient_debuff',	    'var',		    false},
         stage_mission       = {'table_stage_mission',		'key',			false},
         calendar            = {'table_calendar',		    'month',		false},
