54129d560871d54c4b1b62d72b02c5c96fda5f46
ycho <ycho@highbrow.com>
2017-07-17 03:54:36 +0000

    로그아웃시 구글 로그아웃 제대로 안되는 문제 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@86486 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_SettingAccountTab.lua
+++ dragonvillage/src/UI_SettingAccountTab.lua
@@ -268,7 +268,7 @@ function UI_Setting:click_logoutBtn()
                 PerpleSDK:logout()
                 local platform_id = g_serverData:get('local', 'platform_id')
                 if platform_id == 'google.com' then
-                    PerpleSDK:googleLogout(1)
+                    PerpleSDK:googleLogout(0)
                 end
                 if platform_id == 'facebook.com' then
                     PerpleSDK:facebookLogout()
