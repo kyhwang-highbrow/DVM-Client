3550a24babcd4bfa22f099ad092ec48fc28a7550
ycho <ycho@highbrow.com>
2017-07-20 06:40:02 +0000

    WebView 생성 코드 복원
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@86899 5003bd00-c432-460b-831f-0659da9acde2

--- cocos2d-x/cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxActivity.java
+++ cocos2d-x/cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxActivity.java
@@ -54,42 +54,44 @@ public abstract class Cocos2dxActivity extends FragmentActivity implements Cocos
 	// ===========================================================
 	// Fields
 	// ===========================================================
-	
-	//private Cocos2dxGLSurfaceView mGLSurfaceView;
+
+	// private Cocos2dxGLSurfaceView mGLSurfaceView;
 	public Cocos2dxGLSurfaceView mGLSurfaceView;
 	private Cocos2dxHandler mHandler;
 	private static Cocos2dxActivity sContext = null;
 	private Cocos2dxVideoHelper mVideoHelper = null;
+	private Cocos2dxWebViewHelper mWebViewHelper = null;
 	private PowerManager.WakeLock mWakeLock = null;
 	private View mDecorView = null;
 	private Runnable mHideRunnable = null;
-	
+	private Runnable mWakeRunnable = null;
+
 	public static Context getContext() {
 		return sContext;
 	}
-	
+
 	
 	protected void onLoadNativeLibraries() {
 		try {
 			ApplicationInfo ai = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
 			Bundle bundle = ai.metaData;
 			try {
-        		String libName = bundle.getString("android.app.lib_name");
-        		System.loadLibrary(libName);
+				String libName = bundle.getString("android.app.lib_name");
+				System.loadLibrary(libName);
 			} catch (Exception e) {
-		 		// ERROR
+				// ERROR
 				e.printStackTrace();
 			}
 		} catch (PackageManager.NameNotFoundException e) {
-		 	// ERROR
+			// ERROR
 			e.printStackTrace();
 		}
 	}
-	
+
 	// ===========================================================
 	// Constructors
 	// ===========================================================
-	
+
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -97,28 +99,32 @@ public abstract class Cocos2dxActivity extends FragmentActivity implements Cocos
 		onLoadNativeLibraries();
 
 		sContext = this;
-    	this.mHandler = new Cocos2dxHandler(this);
-    	
-    	Cocos2dxHelper.init(this);
-    	
-    	this.init();
-    	if (mVideoHelper == null) {
-    		mVideoHelper = new Cocos2dxVideoHelper(this, mFrameLayout);
+		this.mHandler = new Cocos2dxHandler(this);
+
+		Cocos2dxHelper.init(this);
+
+		this.init();
+
+		if (mVideoHelper == null) {
+			mVideoHelper = new Cocos2dxVideoHelper(this, mFrameLayout);
 		}
-		
+
+		if (mWebViewHelper == null) {
+            mWebViewHelper = new Cocos2dxWebViewHelper(mFrameLayout);
+        }
+
 		setVolumeControlStream(AudioManager.STREAM_MUSIC);
-		
+
 		mDecorView = getWindow().getDecorView();
 		this.hideNavigation();
-		
-		mHideRunnable = new Runnable()
-		{
-		    public void run() 
-		    {
-		    	hideNavigation();
-		    }
+
+		mHideRunnable = new Runnable() {
+			@Override
+			public void run() {
+				hideNavigation();
+			}
 		};
-		
+
 		
 		// SystemUIVisibility가 변경될 때 mHideRunnable을 0.5초 후 실행
 		mDecorView.setOnSystemUiVisibilityChangeListener(new OnSystemUiVisibilityChangeListener() {
@@ -128,7 +134,7 @@ public abstract class Cocos2dxActivity extends FragmentActivity implements Cocos
 			}
 		});
 	}
-	
+
 	// ===========================================================
 	// Getter & Setter
 	// ===========================================================
@@ -150,12 +156,11 @@ public abstract class Cocos2dxActivity extends FragmentActivity implements Cocos
 	protected void onPause() {
 		super.onPause();
 
-		if(mWakeLock!=null && mWakeLock.isHeld())
-		{
+		if (mWakeLock != null && mWakeLock.isHeld()) {
 			mWakeLock.release();
 			mWakeLock = null;
 		}
-		
+
 		Cocos2dxHelper.onPause();
 		this.mGLSurfaceView.onPause();
 	}
@@ -175,21 +180,21 @@ public abstract class Cocos2dxActivity extends FragmentActivity implements Cocos
 		msg.obj = new Cocos2dxHandler.EditBoxMessage(pTitle, pContent, pInputMode, pInputFlag, pReturnType, pMaxLength);
 		this.mHandler.sendMessage(msg);
 	}
-	
+
 	@Override
 	public void runOnGLThread(final Runnable pRunnable) {
 		this.mGLSurfaceView.queueEvent(pRunnable);
 	}
-	
+
 	@Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
-        for (OnActivityResultListener listener : Cocos2dxHelper.getOnActivityResultListeners()) {
-            listener.onActivityResult(requestCode, resultCode, data);
-        }
+		for (OnActivityResultListener listener : Cocos2dxHelper.getOnActivityResultListeners()) {
+			listener.onActivityResult(requestCode, resultCode, data);
+		}
 
-        super.onActivityResult(requestCode, resultCode, data);
-    }
+		super.onActivityResult(requestCode, resultCode, data);
+	}
 
 
 	protected FrameLayout mFrameLayout = null;
@@ -197,108 +202,128 @@ public abstract class Cocos2dxActivity extends FragmentActivity implements Cocos
 	// Methods
 	// ===========================================================
 	public void init() {
-		
-    	// FrameLayout
+
+		// FrameLayout
         ViewGroup.LayoutParams framelayout_params =
             new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                                       ViewGroup.LayoutParams.MATCH_PARENT);
-        mFrameLayout = new FrameLayout(this);
-        mFrameLayout.setLayoutParams(framelayout_params);
+				ViewGroup.LayoutParams.MATCH_PARENT);
+		mFrameLayout = new FrameLayout(this);
+		mFrameLayout.setLayoutParams(framelayout_params);
 
-        // Cocos2dxEditText layout
+		// Cocos2dxEditText layout
         ViewGroup.LayoutParams edittext_layout_params =
             new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                                       ViewGroup.LayoutParams.WRAP_CONTENT);
-        Cocos2dxEditText edittext = new Cocos2dxEditText(this);
-        edittext.setLayoutParams(edittext_layout_params);
+				ViewGroup.LayoutParams.WRAP_CONTENT);
+		Cocos2dxEditText edittext = new Cocos2dxEditText(this);
+		edittext.setLayoutParams(edittext_layout_params);
 
-        // ...add to FrameLayout
-        mFrameLayout.addView(edittext);
+		// ...add to FrameLayout
+		mFrameLayout.addView(edittext);
 
-        // Cocos2dxGLSurfaceView
-        this.mGLSurfaceView = this.onCreateView();
+		// Cocos2dxGLSurfaceView
+		this.mGLSurfaceView = this.onCreateView();
 
-        // ...add to FrameLayout
-        mFrameLayout.addView(this.mGLSurfaceView);
+		// ...add to FrameLayout
+		mFrameLayout.addView(this.mGLSurfaceView);
 
-        // Switch to supported OpenGL (ARGB888) mode on emulator
-        if (isAndroidEmulator())
-           this.mGLSurfaceView.setEGLConfigChooser(8 , 8, 8, 8, 16, 0);
+		// Switch to supported OpenGL (ARGB888) mode on emulator
+		if (isAndroidEmulator())
+			this.mGLSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
 
-        this.mGLSurfaceView.setCocos2dxRenderer(new Cocos2dxRenderer());
-        this.mGLSurfaceView.setCocos2dxEditText(edittext);
+		this.mGLSurfaceView.setCocos2dxRenderer(new Cocos2dxRenderer());
+		this.mGLSurfaceView.setCocos2dxEditText(edittext);
 
-        // Set framelayout as the content view
+		// Set framelayout as the content view
 		setContentView(mFrameLayout);
 	}
-	
+
 	public Cocos2dxGLSurfaceView onCreateView() {
-        Cocos2dxGLSurfaceView glSurfaceView = new Cocos2dxGLSurfaceView(this);
-        glSurfaceView.setEGLConfigChooser(5, 6, 5, 0, 16, 8);
-        return glSurfaceView;
-    }
+		Cocos2dxGLSurfaceView glSurfaceView = new Cocos2dxGLSurfaceView(this);
+		glSurfaceView.setEGLConfigChooser(5, 6, 5, 0, 16, 8);
+		return glSurfaceView;
+	}
 
 	private final static boolean isAndroidEmulator() {
-      String model = Build.MODEL;
-      Log.d(TAG, "model=" + model);
-      String product = Build.PRODUCT;
-      Log.d(TAG, "product=" + product);
-      boolean isEmulator = false;
-      if (product != null) {
-         isEmulator = product.equals("sdk") || product.contains("_sdk") || product.contains("sdk_");
-      }
-      Log.d(TAG, "isEmulator=" + isEmulator);
-      return isEmulator;
-   }
+		String model = Build.MODEL;
+		Log.d(TAG, "model=" + model);
+		String product = Build.PRODUCT;
+		Log.d(TAG, "product=" + product);
+		boolean isEmulator = false;
+		if (product != null) {
+			isEmulator = product.equals("sdk") || product.contains("_sdk") || product.contains("sdk_");
+		}
+		Log.d(TAG, "isEmulator=" + isEmulator);
+		return isEmulator;
+	}
 
 	@Override
 	@SuppressLint("Wakelock")
 	public void setWakeLock(boolean bLock) {
-		if(mWakeLock == null)
-		{
+		if (mWakeLock == null) {
 			final PowerManager powerMgr = (PowerManager) getContext().getSystemService(Context.POWER_SERVICE);
 			mWakeLock = powerMgr.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, getContext().getClass().getName());
 		}
-		
-		if(bLock)
-		{
-			if(mWakeLock.isHeld() == false)
-			{
+
+		if (bLock) {
+			if(mWakeLock.isHeld() == false) {
 				mWakeLock.acquire();
 			}
-		}
-		else
-		{
-			if(mWakeLock.isHeld())
-			{
+		} else {
+			if(mWakeLock.isHeld()) {
 				mWakeLock.release();
 			}
 		}
+
+		// SCREEN_BRIGHT_WAKE_LOCK
+		// This constant was deprecated in API level 13.
+		// Most applications should use FLAG_KEEP_SCREEN_ON instead of this type
+		// of wake lock,
+		// as it will be correctly managed by the platform as the user moves
+		// between applications
+		// and doesn't require a special permission.
+		// SCREEN_BRIGHT_WAKE_LOCK이 deprecated되어 FLAG_KEEP_SCREEN_ON으로 대체
+		// Runnable을 생성하는 이유는, 쓰레드 때문
+		if (bLock == true) {
+			mWakeRunnable = new Runnable() {
+				@Override
+				public void run() {
+					getWindow().addFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+				}
+			};
+			mHandler.post(mWakeRunnable);
+		} else {
+			mWakeRunnable = new Runnable() {
+				@Override
+				public void run() {
+					getWindow().clearFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+				}
+			};
+			mHandler.post(mWakeRunnable);
+		}
 	}
-	
+
 	@Override
 	public boolean isWakeLock() {
-		if(mWakeLock == null)
+		if (mWakeLock == null)
 			return false;
 		else
 			return mWakeLock.isHeld();
 	}
-	
+
 	private void hideNavigation() {
-		// 시스템UI 네비게이션 숨김기능 사용하지 않음
-		/*
-		if(mDecorView == null) {
+		// 시스템UI 네비게이션 숨김기능
+
+		if (mDecorView == null) {
 			return;
 		}
-		
+
 	    mDecorView.setSystemUiVisibility(
 	            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 	            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 	            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 	            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-	            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-	            | View.SYSTEM_UI_FLAG_IMMERSIVE);
-		*/
+				| View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+				| View.SYSTEM_UI_FLAG_IMMERSIVE);
 	}
 
 	// ===========================================================
