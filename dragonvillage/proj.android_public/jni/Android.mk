baf21e336a80403f2b614bbbf3a063daa7792292
ycho <ycho@highbrow.com>
2017-07-25 03:41:22 +0000

    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@87248 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/proj.android_public/jni/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-subdirs := $(LOCAL_PATH)/main/Android.mk \
-           $(LOCAL_PATH)/../../../libraries/pati/prebuilt/android/Android.mk
-
-include $(subdirs)
