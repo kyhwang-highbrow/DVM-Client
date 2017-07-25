e41b9e7ecc3fb77bb57c66b590831fc00fc75ea0
ycho <ycho@highbrow.com>
2017-07-25 08:10:18 +0000

    Gradle build 스크립트 파일 추가
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@87294 5003bd00-c432-460b-831f-0659da9acde2

---
+++ dragonvillage/proj.android_studio/build.py
@@ -0,0 +1,17 @@
+import os
+
+# apk file location
+# .\DragonVillageM\build\outputs\apk
+
+# debug build
+#command = 'gradlew assembleDebug'
+# debug build and install
+#command = 'gradlew installDebug'
+
+# release build
+command = 'gradlew assembleRelease'
+# release build and install
+#command = 'gradlew installRelease'
+
+os.system(command)
+os.system('pause')
