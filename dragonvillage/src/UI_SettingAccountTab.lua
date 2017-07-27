7b6a464f177346f55bca6cf8a5091682079bcecf
ycho <ycho@highbrow.com>
2017-07-27 07:15:06 +0000

    구글 업처 처리 오류 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@87513 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_SettingAccountTab.lua
+++ dragonvillage/src/UI_SettingAccountTab.lua
@@ -331,6 +331,12 @@ function UI_Setting:loginSuccess(info)
     g_serverData:applyServerData(platform_id, 'local', 'platform_id')
     g_serverData:applyServerData(account_info, 'local', 'account_info')
 
+    if platform_id == 'google.com' then
+        g_serverData:applyServerData('on', 'local', 'googleplay_connected')
+    else
+        g_serverData:applyServerData('off', 'local', 'googleplay_connected')
+    end
+
     self:updateInfo()
 end
 
