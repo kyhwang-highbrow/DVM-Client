2ccf6634a7741d7d029eb629be8e51c581d39c00
ycho <ycho@highbrow.com>
2017-07-28 13:50:19 +0000

    Android 5.0 이하 기기에서 오류나는 문제 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@87714 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/proj.android/jni/Application.mk
+++ dragonvillage/proj.android/jni/Application.mk
@@ -5,6 +5,8 @@ APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-ch
 APP_LDFLAGS := -latomic
 APP_ABI := armeabi armeabi-v7a
 
+APP_PLATFORM := android-19
+
 APP_DEBUG := $(strip $(NDK_DEBUG))
 ifeq ($(APP_DEBUG),1)
   APP_CPPFLAGS += -DCOCOS2D_DEBUG=1
