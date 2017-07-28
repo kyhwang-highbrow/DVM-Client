82928b49ecd38745a7058ef513746579952903c1
ycho <ycho@highbrow.com>
2017-07-28 14:04:05 +0000

    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@87715 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/proj.android_studio/build.py
+++ dragonvillage/proj.android_studio/build.py
@@ -3,15 +3,8 @@ import os
 # apk file location
 # .\DragonVillageM\build\outputs\apk
 
-# debug build
-#command = 'gradlew assembleDebug'
-# debug build and install
-#command = 'gradlew installDebug'
-
-# release build
-command = 'gradlew assembleRelease'
-# release build and install
-#command = 'gradlew installRelease'
-
-os.system(command)
+#os.system('gradlew assembleDebug')
+#os.system('gradlew installDebug')
+os.system('gradlew assembleRelease')
+#os.system('gradlew installRelease')
 os.system('pause')
