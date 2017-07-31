cb6ee02f08384c3620a3d4d1a876966d67573109
sgkim <sgkim@highbrow.com>
2017-07-31 06:18:11 +0000

    [성구] #0.2.5버전 풀빌드 assets 커밋
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@87802 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/proj.android/assets/fixed_constant.lua
+++ dragonvillage/proj.android/assets/fixed_constant.lua
@@ -1,5 +1,5 @@
 -- 드빌팀 개발자가 svn상의 src경로에서 작업할때만 적용
-DEVELOPMENT_SRC_VER = false
+DEVELOPMENT_SRC_VER = true
 
 -- 김성구의 개인 개발 버전
 DEVELOPMENT_SEONG_GOO_KIM = false
@@ -9,3 +9,9 @@ DEVELOPMENT_KMS = false
 
 -- 김성진 버전
 DEVELOPMENT_KSJ = false
+
+TARGET_SERVER = nil
+--TARGET_SERVER = 'FGT'
+
+-- APK 확장 파일 다운로드 스킵 여부
+SKIP_OBB_DOWNLOAD = true
