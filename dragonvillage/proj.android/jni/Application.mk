ca8923434fc96b64a48cee8dbc2226481e8835b4
ycho <ycho@highbrow.com>
2017-07-31 22:56:48 +0000

    빌드 스크립트 정리
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@87852 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/proj.android/jni/Application.mk
+++ dragonvillage/proj.android/jni/Application.mk
@@ -5,7 +5,7 @@ APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-ch
 APP_LDFLAGS := -latomic
 APP_ABI := armeabi armeabi-v7a
 
-APP_PLATFORM := android-19
+APP_PLATFORM := android-16
 
 APP_DEBUG := $(strip $(NDK_DEBUG))
 ifeq ($(APP_DEBUG),1)
