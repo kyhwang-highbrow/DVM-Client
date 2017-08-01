01299153aa2b0edc25f7a0e1c9f87c6e70a37ad3
mskim <mskim@highbrow.com>
2017-08-01 09:38:45 +0000

    [문성] ## 1. 빌드후 apk 복사 경로 수정 -> build로 보냄 .. ignore 되도록
    2. 속성 변경 
    -> apk 만 복사
    -> 중복 파일도 복사 (/IS)
    
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@87953 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/proj.android_studio/build.py
+++ dragonvillage/proj.android_studio/build.py
@@ -9,12 +9,12 @@ import os
 # build
 #os.system('gradlew assembleDebug')
 #os.system('gradlew installDebug')
-os.system('gradlew assembleRelease')
+#os.system('gradlew assembleRelease')
 #os.system('gradlew installRelease')
 
 # apk copy
 src_path = './DragonVillageM/build/outputs/apk'
-tar_path = './'
-os.system('robocopy "%s" "%s" /E' % (src_path, tar_path ))
+tar_path = './build'
+os.system('robocopy "%s" "%s" *.apk /IS' % (src_path, tar_path ))
 
 os.system('pause')
