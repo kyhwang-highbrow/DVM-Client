2691505fb883d980285ee49d3a0139a2480190ca
skim <skim@highbrow.com>
2017-08-02 07:46:19 +0000

    [성진] fixed_constant.lua에 테스트 모드 설정을 위한 TEST_MODE_VER 추가
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@88040 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/fixed_constant.lua
+++ dragonvillage/fixed_constant.lua
@@ -1,6 +1,9 @@
 -- 드빌팀 개발자가 svn상의 src경로에서 작업할때만 적용
 DEVELOPMENT_SRC_VER = true
 
+-- 테스트 모드 버전(아예 설정되지 않았을 경우도 테스트 모드로 처리됨)
+TEST_MODE_VER = true
+
 -- 김성구의 개인 개발 버전
 DEVELOPMENT_SEONG_GOO_KIM = false
 
