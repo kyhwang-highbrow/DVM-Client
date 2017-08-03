ecde4394ea3e56d1aa631b126a8dfa1f4861d5cf
ycho <ycho@highbrow.com>
2017-08-03 18:14:46 +0000

    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@88230 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/proj.android/jni/Application.mk
+++ dragonvillage/proj.android/jni/Application.mk
@@ -1,7 +1,7 @@
 APP_STL := gnustl_shared
 NDK_TOOLCHAIN_VERSION=4.9
 
-APP_CPPFLAGS := -frtti -fexceptions -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char
+APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char
 APP_LDFLAGS := -latomic
 APP_ABI := armeabi armeabi-v7a x86
 
