4c6de3a3693bcdcb56eb4d382eaf4396a732a2a0
mskim <mskim@highbrow.com>
2017-08-10 12:07:16 +0000

    [문성] ## 1. module 용 xor_core 생성 -> xor, xor_data 에서 공통 로직 사용하도락 하기 위함
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@88976 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/python/xor.py
+++ dragonvillage/python/xor.py
@@ -4,97 +4,43 @@ import os
 import sys
 import shutil
 
-count_files = 0
+#모듈을 사용하기 위해 시스템 경로 추가
+file_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(file_path, 'module'))
+import xor_core
 
-# 확장자 변경 *.lua -> *.ps
-def changeExt(org):
-    dest = os.path.splitext(org)[0] + ".ps"
-    return dest
+def main():
+    # 변경될 확장자 지정
+    xor_core.setChangeExtension('.ps')
 
-def copy_files(src, dst):
-	if not os.path.isdir(src):
-		return
-	else:
-		for item in os.listdir(src):
-			path = os.path.join(src, item)
-			# Android can not package the file that ends with ".gz"
-			if os.path.isfile(path) and not item.startswith('.') and not item.endswith('.gz'):
-				shutil.copy(path, dst)
-			if os.path.isdir(path) and not item.startswith('.'):
-				new_dst = os.path.join(dst, item)
-				os.mkdir(new_dst)
-				copy_files(path, new_dst)
-                
-def convert(rootdir, subdir, subdir2):
-    path = os.path.join(rootdir, subdir)
+    # 대상 확장자 지정
+    xor_core.setTargetExtensionList(['.lua'])
 
-    for item in os.listdir(path):
-        fullpath = os.path.join(path, item)
+    #상위 폴더를 root_dir경로로 지정
+    root_dir = os.path.dirname(os.path.realpath(__file__))
+    root_dir = os.path.join(root_dir, '../')
+    src_dir = os.path.join(root_dir, 'src')
+    ps_dir = os.path.join(root_dir, 'ps')
 
-        subdir3 = subdir2 + '/' + item
+    # 1. ps폴더 삭제
+    print('1. rmtree "ps"')
+    if os.path.isdir(ps_dir):
+        shutil.rmtree(ps_dir)
 
-        if item.endswith('.lua'):
-            xor_encrypter(fullpath, subdir3)
+    # 2. ps폴더 생성    
+    print('2. mkdir "ps"')
+    os.mkdir(ps_dir)
 
-        if os.path.isdir(fullpath):
-            convert(path, item, subdir3)
+    # 3. 폴더 복사
+    print('3. copy_files')
+    xor_core.copy_files(src_dir, ps_dir)
+        
+    # 4. xor 암호화
+    sub_dir = 'ps'
+    xor_core.convert(root_dir, sub_dir, '../' + sub_dir)
 
-def xor_encrypter(file_path, file):
-    # lua파일 오픈
-    #print(file_path)
-    f = open(file_path, 'rb')
-    file_arr = f.read()
-    f.close()
-    
-    # xor 암호화
-    data = bytearray(file_arr)
-    key = bytearray([0x01, 0x90, 0x32, 0xcf, 0x96, 0x7b, 0x5a, 0xe5, 0xd2, 0xbf, 0x2d, 0xdc, 0xb6, 0x83, 0x4e, 0x04])
-    xor_data = xor(data, key)
-    
-    global count_files
+    print('------------------------------------------------------------')
+    print('Total ' + str(xor_core.count_files) + ' files changed')
 
-    # 파일에 쓰기
-    write_f = open(changeExt(file_path), 'wb')
-    write_f.write(xor_data)
-    write_f.close()
-    count_files = (count_files + 1)
-    
-    # lua파일 삭제
-    os.remove(file_path)
-    
-
-def xor(data, key):
-    l = len(key)
-    return bytearray((
-        (data[i] ^ key[i % l]) for i in range(0,len(data))
-    ))
-
-print('------------------------------------------------------------')
-
-#상위 폴더를 root_dir경로로 지정
-root_dir = os.path.dirname(os.path.realpath(__file__))
-root_dir = os.path.join(root_dir, '../')
-src_dir = os.path.join(root_dir, 'src')
-ps_dir = os.path.join(root_dir, 'ps')
-
-# 1. ps폴더 삭제
-print('1. rmtree "ps"')
-if os.path.isdir(ps_dir):
-    shutil.rmtree(ps_dir)
-
-# 2. ps폴더 생성    
-print('2. mkdir "ps"')
-os.mkdir(ps_dir)
-
-# 3. 폴더 복사
-print('3. copy_files')
-copy_files(src_dir, ps_dir)
-    
-# 4. xor 암호화
-sub_dir = 'ps'
-convert(root_dir, sub_dir, '../' + sub_dir)
-
-print('------------------------------------------------------------')
-print('Total ' + str(count_files) + ' files changed')
-
-#os.system('pause')
+if __name__ == '__main__':
+    main()
