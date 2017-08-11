7edace05ebfffa39133bd3453579c0ffbd6a8784
sgkim <sgkim@highbrow.com>
2017-08-11 11:26:16 +0000

    [성구] #0.3.2 테스트서버 퍼플랩 사내 테스트 버전 assets
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@89165 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/proj.android/assets/fixed_constant.lua
+++ dragonvillage/proj.android/assets/fixed_constant.lua
@@ -1,5 +1,8 @@
 -- 드빌팀 개발자가 svn상의 src경로에서 작업할때만 적용
-DEVELOPMENT_SRC_VER = true
+DEVELOPMENT_SRC_VER = false
+
+-- 테스트 모드 버전(아예 설정되지 않았을 경우도 테스트 모드로 처리됨)
+TEST_MODE_VER = false
 
 -- 김성구의 개인 개발 버전
 DEVELOPMENT_SEONG_GOO_KIM = false
@@ -10,8 +13,11 @@ DEVELOPMENT_KMS = false
 -- 김성진 버전
 DEVELOPMENT_KSJ = false
 
+-- nil('TEST"), 'QA', 'LIVE', 'FGT', 'PUBLIC'
 TARGET_SERVER = nil
---TARGET_SERVER = 'FGT'
 
 -- APK 확장 파일 다운로드 스킵 여부
 SKIP_OBB_DOWNLOAD = true
+
+-- 퍼플랩 테스트 버전
+PERPLELAB_EVENT_MODE = true
