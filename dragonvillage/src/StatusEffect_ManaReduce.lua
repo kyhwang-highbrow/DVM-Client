ae189ed8b55886998eeb49c89c67bab4d827267b
mkang <mkang@highbrow.com>
2017-08-16 06:42:38 +0000

    [민승] 마나량 감소 버프 버그 임시 처리 (기획팀 논의 후 수정예정)
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@89262 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/StatusEffect_ManaReduce.lua
+++ dragonvillage/src/StatusEffect_ManaReduce.lua
@@ -6,6 +6,7 @@ local PARENT = StatusEffect
 StatusEffect_ManaReduce = class(PARENT, {
     m_reduceValue = 'number',
     m_originValue = 'number',
+    m_isInCondition = 'boolean',
 })
 
 -------------------------------------
@@ -14,6 +15,7 @@ StatusEffect_ManaReduce = class(PARENT, {
 -- @param body
 -------------------------------------
 function StatusEffect_ManaReduce:init(file_name, body)
+    self.m_isInCondition = false
 end
 
 -------------------------------------
@@ -28,22 +30,28 @@ end
 -------------------------------------
 function StatusEffect_ManaReduce:onStart()
     -- 중첩된 유닛이 없는 경우에 해당 드래곤의 마나를 reduceValue(add_option_value_1)만큼 감소시킨다.
-    self.m_originValue = self.m_owner.m_activeSkillManaCost
-    self.m_owner.m_activeSkillManaCost = self.m_owner.m_activeSkillManaCost - self.m_reduceValue 
-    if(self.m_owner.m_activeSkillManaCost < 1) then
-        self.m_owner.m_activeSkillManaCost = 1
+    if (self.m_owner.m_charType == 'dragon' and type(self.m_owner:getSkillIndivisualInfo('active')) == 'table') then
+        self.m_isInCondition = true
+
+        self.m_originValue = self.m_owner.m_activeSkillManaCost
+        self.m_owner.m_activeSkillManaCost = self.m_owner.m_activeSkillManaCost - self.m_reduceValue 
+        if(self.m_owner.m_activeSkillManaCost < 1) then
+            self.m_owner.m_activeSkillManaCost = 1
+        end
+        local t_event = {}
+        t_event['value'] = self.m_owner.m_activeSkillManaCost
+        self.m_owner:dispatch('dragon_mana_reduce', t_event)
     end
-    local t_event = {}
-    t_event['value'] = self.m_owner.m_activeSkillManaCost
-    self.m_owner:dispatch('dragon_mana_reduce', t_event)
 end
 
 -------------------------------------
 -- function onEnd
 -------------------------------------
 function StatusEffect_ManaReduce:onEnd()
-    self.m_owner.m_activeSkillManaCost = self.m_originValue
-    local t_event = {}
-    t_event['value'] = self.m_owner.m_activeSkillManaCost
-    self.m_owner:dispatch('dragon_mana_reduce_finish', t_event)
+    if (self.m_isInCondition) then
+        self.m_owner.m_activeSkillManaCost = self.m_originValue
+        local t_event = {}
+        t_event['value'] = self.m_owner.m_activeSkillManaCost
+        self.m_owner:dispatch('dragon_mana_reduce_finish', t_event)
+    end
 end
