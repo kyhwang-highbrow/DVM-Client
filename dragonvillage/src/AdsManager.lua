f3b14d68445c8364cb577f2978f24431cc40d276
ycho <ycho@highbrow.com>
2017-08-19 15:16:30 +0000

    광고 오류 메시지 처리 추가
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@89609 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/AdsManager.lua
+++ dragonvillage/src/AdsManager.lua
@@ -72,15 +72,58 @@ function AdsManager:showPlacement(placementId, result_cb)
         return
     end
 
-    local function _result_cb(ret, info)
-        if (ret == 'ready') and (info == placementId) then
-            self:show(placementId, function(ret, info)
+    local function __result_cb(ret, info)
+        if (ret == 'error') then
+            self:showErrorPopup(info, function()
                 result_cb(ret, info)
-            end)
+            end
+        else
+            result_cb(ret, info)
+        end
+    end
+
+    local function _result_cb(ret, info)
+        if (ret == 'ready') then
+            if (info == placementId) then
+                self:show(placementId, function(ret, info)
+                    __result_cb(ret, info)
+                end)
+            end
+        else
+            __result_cb(ret, info)
         end
     end
 
     self:start(placementId, _result_cb)
+end
+
+function AdsManager:showErrorPopup(errorCode, result_cb)
+    local msg = ''
+
+    if (errorCode == 'INITIALIZE_FAILED') then
+        msg = Str('광고 모듈 초기화에 실패하였습니다.')
+    elseif (errorCode == 'NOT_INITIALIZED') then
+        msg = Str('광고 모듈이 아직 초기화되지 않았습니다. 잠시 후 다시 시도해 주세요.')
+    elseif (errorCode == 'VIDEO_PLAYER_ERROR') then
+        msg = Str('광고를 재생하는 비디오 플레이어에서 오류가 발생하였습니다.')
+    elseif (errorCode == 'AD_BLOCKER_DETECTED') then
+        msg = Str('광고가 차단되었습니다. 광고를 보시려면 광고 차단기를 해제하고 다시 시도해 주세요.')
+    elseif (errorCode == 'FILE_IO_ERROR') then
+        msg = Str('저장공간이 부족하여 광고를 다운로드받지 못했습니다. 불필요한 앱이나 파일을 삭제 후 다시 시도해 주세요.')
+    elseif (errorCode == 'DEVICE_ID_ERROR') then
+        msg = Str('디바이스 설정에 문제가 있어 광고를 보실 수 없습니다.')
+    elseif (errorCode == 'INIT_SANITY_CHECK_FAIL') then
+        msg = Str('디바이스 환경 설정에 문제가 있어 광고를 보실 수 없습니다.')
+    elseif (errorCode == 'INVALID_ARGUMENT') then
+        msg = Str('광고 API 호출시 잘못된 인자를 전달하였습니다.')
+    elseif (errorCode == 'SHOW_ERROR') then
+        msg = Str('광고 재생 과정에서 오류가 발생하였습니다. 잠시 후 다시 시도해 주세요.')
+    else
+        -- 'INTERNAL_ERROR'
+        msg = Str('광고 모듈에 오류가 발생하였습니다. 잠시 후 다시 시도해 주세요.')
+    end
+
+    MakeSimplePopup(POPUP_TYPE.OK, msg, result_cb)
 end
 
 -- 호출 방법
