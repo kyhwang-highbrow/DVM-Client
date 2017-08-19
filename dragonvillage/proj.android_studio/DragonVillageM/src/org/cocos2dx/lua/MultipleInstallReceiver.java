aea35484224cc8f0d6a2e47692b6dd885b12e777
ycho <ycho@highbrow.com>
2017-08-19 15:57:40 +0000

    Tapjoy InstallReferrerReceiver 추가
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@89616 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/proj.android_studio/DragonVillageM/src/org/cocos2dx/lua/MultipleInstallReceiver.java
+++ dragonvillage/proj.android_studio/DragonVillageM/src/org/cocos2dx/lua/MultipleInstallReceiver.java
@@ -1,12 +1,12 @@
 package org.cocos2dx.lua;
 
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
 import com.google.android.gms.measurement.AppMeasurementInstallReferrerReceiver;
 import com.igaworks.IgawReceiver;
+import com.tapjoy.InstallReferrerReceiver;
 
 public class MultipleInstallReceiver extends BroadcastReceiver {
     @Override
@@ -18,5 +18,9 @@ public class MultipleInstallReceiver extends BroadcastReceiver {
         // @adbrix
         IgawReceiver igawReceiver = new IgawReceiver();
         igawReceiver.onReceive(context, intent);
+
+        // @tapjoy
+        InstallReferrerReceiver tapjoyReceiver = new InstallReferrerReceiver();
+        tapjoyReceiver.onReceive(context, intent);
    }
 }
