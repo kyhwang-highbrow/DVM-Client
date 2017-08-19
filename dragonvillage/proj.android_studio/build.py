36d9416e08aca13b2af7f05576b9a8c60b86bded
mskim <mskim@highbrow.com>
2017-08-19 14:33:40 +0000

    [문성] ## 1. gradle build 변경 -> 아래와 같은 빌드 세팅을 지원 할 수 있도록 함 - BuildType (빌드 유형) : Release & Debug - ProductFlavors (제품 버전) : Market(under 100mb) & Full - preBuild에서 처리 : Live & QA & Dev
    2. ProductFlavors (제품 버전)에 따라 assets_src 타겟을 바꾸도록 함
    -> Market : assets/100mb
    -> Full : assets/full
    
    3. ProductFlavors (제품 버전)에 따라 release_key 변경
    -> Market : upload_release_key
    -> Full : release_key
    
    4. task preBuild 구성 -> 추후 작업
    
    
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@89605 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/proj.android_studio/build.py
+++ dragonvillage/proj.android_studio/build.py
@@ -3,19 +3,17 @@ import os
 # apk file location
 # .\DragonVillageM\build\outputs\apk
 
+# if you want to get more info, do THIS
+#os.system('gradlew tasks')
+
 # clean
 os.system('clean.py')
 #os.system('gradlew clean')
 
 # build
 os.system('gradlew assembleRelease')
-#os.system('gradlew installRelease')
 #os.system('gradlew assembleDebug')
-#os.system('gradlew installDebug')
-
-# apk copy
-src_path = './DragonVillageM/build/outputs/apk'
-tar_path = './build'
-os.system('robocopy "%s" "%s" *.apk /IS' % (src_path, tar_path ))
+#os.system('gradlew assembleMarket')
+#os.system('gradlew assembleFull')
 
 os.system('pause')
