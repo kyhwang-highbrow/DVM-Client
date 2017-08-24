7c9ebc6028dc587cca0d3f9c5f74032a491bb22b
mkang <mkang@highbrow.com>
2017-08-24 01:59:06 +0000

    [민승] 권선징악/퐁당퐁당 스킬 버그 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@90208 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/SkillSpatter.lua
+++ dragonvillage/src/SkillSpatter.lua
@@ -131,7 +131,6 @@ end
 -- function trySpatter
 -------------------------------------
 function SkillSpatter:trySpatter()
-    print (self.m_spatterCount, self.m_spatterMaxCount)
 	if (self.m_spatterCount >= self.m_spatterMaxCount) then
         self:changeState('dying')
         return false
