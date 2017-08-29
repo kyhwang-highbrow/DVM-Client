ec9b0cccd742c2d2d485ed5ee60730e982e2ff27
sgkim <sgkim@highbrow.com>
2017-08-29 10:11:42 +0000

    [성구] #APK 확장파일 받지 않아도 되는 상황에선 파일 받지 않도록 변경
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@90939 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/ApkExpansion.lua
+++ dragonvillage/src/ApkExpansion.lua
@@ -84,8 +84,8 @@ function ApkExpansion:doStep()
             end
             MakeSimplePopup2(POPUP_TYPE.OK, "게임 실행에 필요한 추가 파일를 내려받기 위해 '사진/미디어/파일 액세스' 접근 권한이 필요합니다.", "권한 요청을 거부할 경우 정상적인 게임 실행이 불가능하며\n앱을 삭제한 후 다시 설치하셔야 합니다.", ok_cb)
         else
-            SDKManager:apkExpansionStart(param_str, md5, callback)
-            --self:finish()
+            --SDKManager:apkExpansionStart(param_str, md5, callback)
+            self:finish()
         end
     end)
 end
