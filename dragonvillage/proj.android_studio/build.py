bc95750c912cd06e93ef63e99789055fbca5b285
ycho <ycho@highbrow.com>
2017-09-26 09:33:48 +0000

    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@92369 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/proj.android_studio/build.py
+++ dragonvillage/proj.android_studio/build.py
@@ -7,15 +7,36 @@ import os
 #os.system('gradlew tasks')
 
 # clean
-os.system('clean.py')
-os.system('gradlew clean')
+#os.system('clean.py')
+#os.system('gradlew clean')
 
 # build
 #os.system('gradlew assembleDev100mbTest')
 #os.system('gradlew assembleDevFullTest')
 #os.system('gradlew assembleQa100mbTest')
-os.system('gradlew assembleQaFullTest')
+#os.system('gradlew assembleQaFullTest')
 #os.system('gradlew assembleLive100mb')
 #os.system('gradlew assembleLiveFull')
+
+#os.system('gradlew assembleDevFullTestDebug')
+#os.system('gradlew installDevFullTestDebug')
+
+#os.system('gradlew assembleDevFullTestRelease')
+#os.system('gradlew installDevFullTestRelease')
+
+#os.system('gradlew assembleQaFullTestDebug')
+#os.system('gradlew installQaFullTestDebug')
+
+os.system('gradlew assembleQaFullTestRelease')
+#os.system('gradlew installQaFullTestRelease')
+
+#os.system('gradlew assembleLive100mbRelease')
+#os.system('gradlew installLive100mbRelease')
+
+#os.system('gradlew assembleLiveFullRelease')
+#os.system('gradlew installLiveFullRelease')
+
+# apk copy
+#os.system('sdkcopy.bat')
 
 os.system('pause')
