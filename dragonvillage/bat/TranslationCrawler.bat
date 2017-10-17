e62e8c34d4955d9279560e9b32afe96542d9a4f1
mskim <mskim@highbrow.com>
2017-10-17 08:23:09 +0000

    [문성] ## 1. 번역 텍스트 수집 및 루아테이블 생성 까지는 완료 -> 실제 운용 방법에 따라 저장방식, 저장 양식, version control 방법들을 정하면 됩니다
    실행 시 프로젝트 내 src, res, data 폴더를 순회하며 한국어 텍스트를 수집합니다.
    수집된 텍스트는 프로젝트 폴더 내 translate 폴더에 work_xx, full_xx, total_translation 언어별 3가지 형태로 추출됩니다.
    번역작업자에게 work_xx를 보내고 작업이 완료된 파일은 파일명 그대로 translate 폴더에 넣은 후
    다시 해당 파일을 실행하면 번역이 적용됩니다. (full_xx에 들어감)
    work_xx는 번역 대상 텍스트가 없는 경우 생성되지 않습니다.
    
    
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@92741 5003bd00-c432-460b-831f-0659da9acde2

---
+++ dragonvillage/bat/TranslationCrawler.bat
@@ -0,0 +1,7 @@
+::UTF-8�� ����
+chcp 65001
+
+cd "../src_tool"
+call "../../../../res/tools/lua/bin/lua.exe" "TranslationCrawler.lua" run
+
+PAUSE
--- dragonvillage/src_tool/TranslationCrawler.lua
+++ dragonvillage/src_tool/TranslationCrawler.lua
@@ -1,3 +1,63 @@
+-------------------------------------
+-- summary
+-------------------------------------
+--[[
+실행 시 프로젝트 내 src, res, data 폴더를 순회하며 한국어 텍스트를 수집합니다.
+수집된 텍스트는 프로젝트 폴더 내 translate 폴더에 work_xx, full_xx, total_translation 언어별 3가지 형태로 추출됩니다.
+번역작업자에게 work_xx를 보내고 작업이 완료된 파일은 파일명 그대로 translate 폴더에 넣은 후
+다시 해당 파일을 실행하면 번역이 적용됩니다. (full_xx에 들어감)
+work_xx는 번역 대상 텍스트가 없는 경우 생성되지 않습니다.
+]]
+
+-------------------------------------
+-- Terminology
+-------------------------------------
+--[[
+
+0. tr = translate
+
+1. work : 미번역된 텍스트 리스트로 작업 대상을 의미, 텍스트의 출처를 병기하지 않는다.
+    ex)
+        [work_en.json]
+        {
+            "org":"번역"
+            "tr":"translation"
+        }
+
+2. full translation : 번역 + 미번역 테스트를 포괄하여 언어별로 생성한 리스트. 출처를 병기.
+    ex)
+        [full_en.json]
+        {
+            "org":"번역",
+            "tr":"translation", 
+            "src":{
+                "Translate.lua":1,
+                "translate.ui":1,
+                "table_translation.csv":1
+            }
+        }
+
+3. total translation : 모든 언어의 번역 + 미번역 테스트를 포괄한 리스트. 출처 및 모든 언어 병기
+    ex)
+        [total_translation.json]
+        {
+            "org":"번역",
+            "tr":{
+                "en":"translation",
+                "jp":"飜譯"
+            }
+            "src":{
+                "Translate.lua":1,
+                "translate.ui":1,
+                "table_translation.csv":1
+            }
+        }
+
+]]
+
+-------------------------------------
+-- require
+-------------------------------------
 require 'LuaStandAlone'
 
 -------------------------------------
@@ -8,21 +68,33 @@ TranslationCrawler = class({
 
 -------------------------------------
 -- local vars
--------------------------------------  
+------------------------------------- 
+local L_LANG = {'en', 'jp'}
+
+-- 추출할 결과 저장 영역
+local T_LANG_LIST = {}
+local T_WORK = {}
+
+-- 작업용 임시 저장 영역
 local T_STR = {}
-local T_UI_STR = {}
-local T_LUA_STR = {}
-local T_DATA_STR = {}
+local T_STR_UI = {}
+local T_STR_LUA = {}
+local T_STR_DATA = {}
 
+-- 사용할 디렉토리
 local CURR_DIR = lfs.currentdir()
-local RESULT_DIR = '../translate/'
+local RESULT_DIR = '../translate'
 
+-- 제외할 파일 목록 .. 현재는 csv에만 적용
 local T_EXCEPT_FILE = 
 {
-    ['table_ban_word_chat.csv'] = true,
-    ['table_ban_word_naming.csv'] = true,
+    ['table_ban_word_chat.csv'] = 1,
+    ['table_ban_word_naming.csv'] = 1,
 }
 
+-------------------------------------
+-- local utillity functions
+------------------------------------- 
 -- function isKorean
 local function isKorean(str)
     return string.match(str, '[가-힣]+')
@@ -38,6 +110,29 @@ local function dietTable(t_table)
         end
     end
     return t_table
+end
+
+-- function readTranslation
+local function readTranslation(lang, prefix)
+    local t_temp = {}
+    local path = string.format('%s/%s_%s.json', RESULT_DIR, prefix, lang)
+    local content = LuaBridge:getStringFromFile(path)
+    if (content) then
+        for i, t_info in ipairs(dkjson.decode(content)) do
+            t_temp[t_info['org']] = t_info
+        end
+        if (prefix == 'work') then
+            os.remove(path)
+        end
+    else
+        cclog(string.format('## not exist file : %s_%s', prefix, lang))
+    end
+    return t_temp
+end
+
+-- function makeJsonString
+local function makeJsonString(t)
+    return dkjson.encode(t, {indent = true, keyorder = {'org', 'tr', 'src'}})
 end
 
 -------------------------------------
@@ -60,10 +155,19 @@ function TranslationCrawler:run()
     self:crawler_src()
     self:crawler_ui()
     self:crawler_data()
+    
+    -- 언어별로 풀스크립트 및 번역안된텍스트파일 생성
+    for _, lang in ipairs(L_LANG) do
+        self:compareWithFullTranslation(lang)
+        self:saveUntranslatedStr(lang)
+        self:saveFullTranslation(lang)
+        self:saveLuaTranslation(lang)
+    end
 
-    self:printDebug()
+    self:saveTotalTranslation()
 
-    self:saveTraslation()
+    -- 작업 결과 출력
+    self:printResult()
 
     -- Stopwatch stop
     stopwatch:stop()
@@ -74,24 +178,25 @@ end
 
 -------------------------------------
 -- function insertData
+-- @brief 공통된 form으로 데이터 구성하도록 함
 -------------------------------------
 function TranslationCrawler:insertData(t, kr_str, file)
     if not (t[kr_str]) then
         t[kr_str] = {
             ['src'] = {},
-            ['tr'] = {
-                ['en'] = nil,
-                ['jp'] = nil,
-            }
+            ['tr'] = {},
+            ['org'] = kr_str,
         }
     end
-    t[kr_str]['src'][file] = true
+    t[kr_str]['src'][file] = 1
 end
 
 -------------------------------------
 -- function crawler_src
 -------------------------------------
 function TranslationCrawler:crawler_src()
+    cclog('#### crawler_src')
+
     -- @dir, @iter_func
     local root_dir = CURR_DIR .. '/../src'
     local function iter_func(path, file)
@@ -104,7 +209,7 @@ function TranslationCrawler:crawler_src()
         -- Str(""), Str( ''), Str( "") 등 다양한 오류가 존재할 수 있으므로 최대한 포괄적으로 구성
         for kr_str in string.gmatch(content, 'Str%(%s*[\"\']([^\n]-)[\"\']') do
             if (isKorean(kr_str)) then
-                self:insertData(T_LUA_STR, kr_str, file)
+                self:insertData(T_STR_LUA, kr_str, file)
                 self:insertData(T_STR, kr_str, file)
             end
         end
@@ -118,6 +223,8 @@ end
 -- function crawler_ui
 -------------------------------------
 function TranslationCrawler:crawler_ui()
+    cclog('#### crawler_ui')
+
     -- @dir, @iter_func
     local root_dir = CURR_DIR .. '/../res'
     local function iter_func(path, file)
@@ -133,7 +240,7 @@ function TranslationCrawler:crawler_ui()
         -- ui에서 더미 파일을 걸러야 하는데.. 일차적으로는 {n} 이 포함된 것은 더미로 판단
         for kr_str in string.gmatch(content, '[(text)||(placeholder)] = \'([^\n{]-)\'') do
             if (isKorean(kr_str)) then
-                self:insertData(T_UI_STR, kr_str, file)
+                self:insertData(T_STR_UI, kr_str, file)
                 self:insertData(T_STR, kr_str, file)
             end
         end
@@ -147,6 +254,8 @@ end
 -- function crawler_data
 -------------------------------------
 function TranslationCrawler:crawler_data()
+    cclog('#### crawler_data')
+
     -- @dir, @iter_func
     local root_dir = CURR_DIR .. '/../data'
     local function iter_func(path, file)
@@ -178,7 +287,7 @@ function TranslationCrawler:crawler_data()
                     -- nothing to do
                 else
                     if (isKorean(value)) then
-                        self:insertData(T_DATA_STR, value, file)
+                        self:insertData(T_STR_DATA, value, file)
                         self:insertData(T_STR, value, file)
                     end
                 end
@@ -192,39 +301,169 @@ function TranslationCrawler:crawler_data()
 end
     
 -------------------------------------
--- function saveTraslation
+-- function compareWithFullTranslation
+-- @brief 추출한 리스트를 언어별 이미 번역된 테스트들과 비교하여 작업대상을 구분하고 full translation을 새로이 생성
+-- @param lang 언어키
 -------------------------------------
-function TranslationCrawler:saveTraslation()
+function TranslationCrawler:compareWithFullTranslation(lang)
+    cclog('## compare with full translation ' .. lang)
 
+    -- 언어별 테이블 생성
+    T_LANG_LIST[lang] = {}
+    T_WORK[lang] = {}
+
+    -- 이미 번역된 텍스트 불러옴
+    local t_full = readTranslation(lang, 'full')
+
+    -- 작업결과물 번역도 불러옴
+    local t_work = readTranslation(lang, 'work')
+    
+    -- 이미 번역된 텍스트와 비교하여 해당 언어의 맵 생성
+    local tr_str
+    for str, t_info in pairs(T_STR) do
+        -- full translation에 번역한 텍스트가 있음
+        if (t_full[str]) and (t_full[str]['tr']) and (t_full[str]['tr'] ~= '') then
+            tr_str = t_full[str]['tr']
+            
+            -- total translation 출력을 위해 저장 
+            t_info['tr'][lang] = tr_str
+
+        -- work translation에 번역한 텍스트가 있음
+        elseif (t_work[str]) and (t_work[str]['tr']) and (t_work[str]['tr'] ~= '') then
+            tr_str = t_work[str]['tr']
+            
+            -- total translation 출력을 위해 저장 
+            t_info['tr'][lang] = tr_str
+
+        -- 번역한 텍스트가 없음
+        else
+            tr_str = nil
+            
+            -- 작업 리스트에 추가
+            table.insert(T_WORK[lang], {['org'] = str, ['tr'] = ''})
+        end
+
+        -- 언어별 full translation에 추가
+        table.insert(T_LANG_LIST[lang], {['org'] = str, ['tr'] = tr_str, ['src'] = t_info['src']})
+    end
 end
 
 -------------------------------------
--- function saveTraslation
+-- function saveUntranslatedStr
+-- @brief 번역 대상 리스트를 저장
+-- @param lang 언어키
 -------------------------------------
-function TranslationCrawler:saveTraslation()
+function TranslationCrawler:saveUntranslatedStr(lang)
+    cclog('## extract untranslated string ' .. lang)
 
+    -- 작업해야할 텍스트가 없다면 통과
+    if (table.count(T_WORK[lang]) == 0) then
+        cclog(lang .. ' is translated all')
+        return
+    end
+
+    -- 가나다 순으로 정렬한다.
+    table.sort(T_WORK[lang], function(a, b)
+        return a['org'] < b['org']
+    end)
+
+    -- 해당 언어 테이블 json으로 변환
+    local work_str = makeJsonString(T_WORK[lang])
+
+    -- 저장
+    local path = string.format('%s/work_%s.json', RESULT_DIR, lang)
+    local f = io.open(path, 'w')
+    f:write(work_str)
+    f:close()
 end
 
 -------------------------------------
--- function saveTraslation
+-- function saveFullTranslation
+-- @brief full translation 을 저장한다.
+-- @param lang 언어키
 -------------------------------------
-function TranslationCrawler:saveTraslation()
-    local json_str = json.encode(T_STR)
+function TranslationCrawler:saveFullTranslation(lang)
+    cclog('## save full translation ' .. lang)
 
-    local f = io.open(RESULT_DIR .. 'full_translation.json','w')
+    -- 가나다 순으로 정렬한다.
+    table.sort(T_LANG_LIST[lang], function(a, b)
+        return a['org'] < b['org']
+    end)
+
+    -- 해당 언어의 전체 파일 json으로 변환
+    local json_str = makeJsonString(T_LANG_LIST[lang])
+
+    -- 저장
+    local path = string.format('%s/full_%s.json', RESULT_DIR, lang)
+    local f = io.open(path,'w')
     f:write(json_str)
     f:close()
 end
 
 -------------------------------------
--- function printDebug
+-- function saveLuaTranslation
+-- @brief full translation 을 lua table로 변환하여 저장한다.
+-- @param lang 언어키
 -------------------------------------
-function TranslationCrawler:printDebug()
-    cclog('==================================================')
-    cclog('src string cnt: ' .. table.count(T_LUA_STR))
-    cclog('ui string cnt: ' .. table.count(T_UI_STR))
-    cclog('data string cnt: ' .. table.count(T_DATA_STR))
+function TranslationCrawler:saveLuaTranslation(lang)
+    cclog('## save lua translation ' .. lang)
+
+    -- lua table로 변환하기 위해 폼 변경
+    local t_lua = {}
+    for _, t_info in pairs(T_LANG_LIST[lang]) do
+        t_lua[t_info['org']] = t_info['tr'] -- 번역 텍스트가 없다면 생성되지 않음
+    end
+
+    -- 해당 언어의 전체 파일 json으로 변환
+    local lua_str = 'return ' .. util.makeLuaTableStr(t_lua)
+
+    -- 저장
+    local path = string.format('%s/%s.lua', RESULT_DIR, lang)
+    local f = io.open(path,'w')
+    f:write(lua_str)
+    f:close()
+end
+
+-------------------------------------
+-- function saveTotalTranslation
+-- @brief 모든 언어를 통합한 total translation 을 저장한다.
+-------------------------------------
+function TranslationCrawler:saveTotalTranslation()
+    cclog('## save total translation')
+
+    -- 리스트로 변환
+    local l_sort = {}
+    for str, t_info in pairs(T_STR) do
+        table.insert(l_sort, t_info)
+    end
+
+    -- 가나다 순으로 정렬한다.
+    table.sort(l_sort, function(a, b)
+        return a['org'] < b['org']
+    end)
+
+    -- json으로 변환
+    local json_str = makeJsonString(l_sort)
+
+    -- 저장
+    local path = string.format('%s/total_translation.json', RESULT_DIR)
+    local f = io.open(path,'w')
+    f:write(json_str)
+    f:close()
+end
+
+-------------------------------------
+-- function printResult
+-------------------------------------
+function TranslationCrawler:printResult()
+    cclog('RESULT ==========================================')
+    cclog('src string cnt: ' .. table.count(T_STR_LUA))
+    cclog('ui string cnt: ' .. table.count(T_STR_UI))
+    cclog('data string cnt: ' .. table.count(T_STR_DATA))
     cclog('total : ' .. table.count(T_STR))
+    for lang, t_str in pairs(T_WORK) do
+        cclog(lang .. ' work out :' .. table.count(t_str))
+    end
     cclog('==================================================')
 end
 
