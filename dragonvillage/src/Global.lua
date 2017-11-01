0729617b4d78484fb66243caa4dd10b6b0e1dd96
mskim <mskim@highbrow.com>
2017-11-01 07:52:45 +0000

    [문성] ## 1. 이름 입력시 영대소문자, 숫자, 한글완성형 만 가능하도록 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@93135 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/Global.lua
+++ dragonvillage/src/Global.lua
@@ -90,67 +90,8 @@ function IsValidText(str, is_name)
 	end
 
 	if is_name then
-        local APP_TARGET = 'KOREA' -- 드래곤히어로즈에서 사용하던 코드 그대로 쓰기위해 유지
-		-- 국내 버전만 체크
-		if (APP_TARGET == nil) or (APP_TARGET == 'KOREA') then
-			if str ~= string.match(str, '[0-9|A-z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]*') then
-				return false
-			end
-
-			if string.match(str, '\\') then
-				return false
-			end
-
-			local koreanVanWord = {
-				'ㄱ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅅ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ', 'ㄲ', 'ㄸ', 'ㅆ', 'ㅉ', 'ㅃ',
-				'ㅏ', 'ㅓ', 'ㅑ', 'ㅕ', 'ㅗ', 'ㅛ', 'ㅜ', 'ㅠ', 'ㅡ', 'ㅣ', 'ㅚ', 'ㅟ', 'ㅐ', 'ㅒ', 'ㅖ', 'ㅔ', 'ㅙ', 'ㅞ',
-			}
-			-- 한국어 자음 이름 방지
-			for _, c in pairs(koreanVanWord) do
-				if string.match(str, c) then
-					return false
-				end
-			end
-
-			-- iOS, 특수문자 x
-			if string.match(str, string.char(226, 157, 142)) then
-				return false
-			end
-
-			-- iOS, 특수문자 v
-			if string.match(str, string.char(226, 156, 133)) then
-				return false
-			end
-
-			--[[
-			for _, c in pairs({'~', '!', '@', '#', '%$', '%%', '%^', '&', '%*', '%?', '_', ' '}) do
-				if string.match(str, c) then
-					return false
-				end
-			end
-
-			for _, c in pairs({'ㄱ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅅ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ', 'ㄲ', 'ㄸ', 'ㅆ', 'ㅉ', 'ㅃ'}) do
-				if string.match(str, c) then
-					return false
-				end
-			end
-			for _, c in pairs({'ㅛ', 'ㅕ', 'ㅑ', 'ㅐ', 'ㅔ', 'ㅖ', 'ㅗ', 'ㅓ', 'ㅏ', 'ㅣ', 'ㅠ', 'ㅜ', 'ㅡ'}) do
-				if string.match(str, c) then
-					return false
-				end
-			end
-			for _, c in pairs({'a', 'b', 'c', 'd', 'e', 'f', 'g', 'f', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}) do
-				if string.match(str, c) then
-					return false
-				end
-			end
-			]]--
-
-		-- GSP, CHINA버전은 별도로 닉네임 체크
-		elseif APP_TARGET == 'CHINA' then
-			return ValidStrUtils:checkNickName_forChina(str)
-		else
-			return ValidStrUtils:checkNickName_forGsp(str)
+		if (not ValidStrUtils:checkNickName_forGsp(str)) then
+            return false
 		end
 	end
 
