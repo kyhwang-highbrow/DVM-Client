4c8872b3d950bb1ce02a7af46337e6d2b383de61
skim <skim@highbrow.com>
2017-11-08 11:45:09 +0000

    [성진] 밸런스 수정(분기처리) - dmg_adj_rate의 최소값을 -80으로 제한(피해감소 80%)
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@93510 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/StatusCalculator.lua
+++ dragonvillage/src/StatusCalculator.lua
@@ -151,6 +151,9 @@ function StatusCalculator:getFinalStat(stat_type)
     -- 공속(aspd)값은 최소값을 50으로 고정
     if (stat_type == 'aspd') then
         final_stat = math_max(final_stat, 50)
+    elseif (stat_type == 'dmg_adj_rate') then
+        final_stat = math_max(final_stat, -80)
+
     -- 특정 타입의 스텟들은 제외한 나머지는 최소값을 0으로 처리
     elseif (not M_SPECIAL_STATUS_TYPE[stat_type]) then
         final_stat = math_max(final_stat, 0)
