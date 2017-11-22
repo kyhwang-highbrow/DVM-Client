8fa0c092788a3b3dc677670a0a2d2e9d70577b3d
skim <skim@highbrow.com>
2017-11-22 09:14:10 +0000

    [성진] 드래곤 드래그 스킬 연출 스킬명 위치 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@93836 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage_bp/src/GameDragonSkill.lua
+++ dragonvillage_bp/src/GameDragonSkill.lua
@@ -76,6 +76,7 @@ function GameDragonSkill:initUI()
     local descNode = self.m_skillDescEffect.m_node:getSocketNode('skill_dsc')
     
     self.m_skillNameLabel = cc.Label:createWithTTF('', 'res/font/common_font_01.ttf', 60, 3, cc.size(1000, 200), 1, 1)
+    self.m_skillNameLabel:setPosition(0, -20)
     self.m_skillNameLabel:setAnchorPoint(cc.p(0.5, 0.5))
 	self.m_skillNameLabel:setDockPoint(cc.p(0, 0))
 	self.m_skillNameLabel:setColor(cc.c3b(84,244,87))
