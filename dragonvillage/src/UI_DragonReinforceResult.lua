ffc774107e6b702b8d7901012cf6c6e44c986c28
mskim <mskim@highbrow.com>
2017-12-06 09:26:57 +0000

    [문성] ## 1. 드래곤 강화 결과 화면 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@94526 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_DragonReinforceResult.lua
+++ dragonvillage/src/UI_DragonReinforceResult.lua
@@ -82,7 +82,7 @@ function UI_DragonReinforceResult:initUI(t_dragon_data)
 
 	 -- 이전 경험치와 레벨 미리 표시
     local lv = dragon_object:getRlv()
-	local icon = IconHelper:getDragonReinforceIcon(lv)
+	local icon = IconHelper:getDragonReinforceIcon(lv - 1)
     vars['beforeNode']:addChild(icon)
 	local icon = IconHelper:getDragonReinforceIcon(lv)
     vars['afterNode']:addChild(icon)
