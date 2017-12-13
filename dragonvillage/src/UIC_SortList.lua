d4f0ff2352979b368159c4c269c943634325909e
mskim <mskim@highbrow.com>
2017-12-13 08:40:38 +0000

    [문성] ## 1. 룬 정렬 리스트 순서 변경
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@94740 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UIC_SortList.lua
+++ dragonvillage/src/UIC_SortList.lua
@@ -521,8 +521,8 @@ function MakeUICSortList_runeManage(button, label)
 
     uic:addSortType('set_id', Str('세트'))
     uic:addSortType('grade', Str('등급'))
-    uic:addSortType('rarity', Str('희귀도'))
     uic:addSortType('lv', Str('레벨'))
+    uic:addSortType('rarity', Str('희귀도'))
 	uic:addSortType('created_at', Str('획득순'))
 
     return uic
