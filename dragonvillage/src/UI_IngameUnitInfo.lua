efd5684816d1fd994ee8f4480d799b3d4bbe3f77
mkang <mkang@highbrow.com>
2018-01-12 07:16:07 +0000

    [민승] 상태효과 아이콘 찍는 로직 수정, 추가적으로 클랜던전에서는 한 줄에 3개가 되도록. constant.json에 상태효과 아이콘에 대한 상수 추가
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@96083 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_IngameUnitInfo.lua
+++ dragonvillage/src/UI_IngameUnitInfo.lua
@@ -78,16 +78,32 @@ end
 -- function getPositionForStatusIcon
 -------------------------------------
 function UI_IngameUnitInfo:getPositionForStatusIcon(bLeftFormation, idx)
-    -- 4개를 넘어가면 y 값 조정
-	local factor_y = 0
-	if idx > 4 then 
-		idx = idx - 4
-		factor_y = -20
-	end
+    -- max_idx개를 넘어가면 y 값 조정
+    --  idx가 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 이고, max_idx가 4이면
+    --  x  =  0, 1, 2, 3, 0, 1, 2, 3, 0, 1 
+    --  y  =  0, 0, 0, 0, 1, 1, 1, 1, 2, 2
+    --  로 만들어서 해당 x,y idx에 각각의 gap을 곱해준다.
+
+    local game_mode = g_gameScene.m_gameMode
+    local t_constant = g_constant:get('UI', 'STATUS_EFFECT_ICON_GAP')
+    local max_idx, gap_x, gap_y
+
+    local constants = t_constant[IN_GAME_MODE[game_mode]]
+    if(not constants) then
+        constants = t_constant['COMMON']
+    end
+
+    max_idx, gap_x, gap_y = constants[1], constants[2], constants[3]
+    idx = idx + max_idx - 1
+    local factor_y = 0
+
+    local quotient = math.floor(idx / max_idx) - 1
+    idx = idx % max_idx
+	factor_y = - gap_y * quotient
 
     local x, y
     
-	x = 20 - 18 * (idx - 1)
+	x = 20 - gap_x * idx
     y = -23 + factor_y
 	
     return x, y
@@ -97,5 +113,9 @@ end
 -- function getScaleForStatusIcon
 -------------------------------------
 function UI_IngameUnitInfo:getScaleForStatusIcon()
+    if (g_gameScene.m_gameMode == GAME_MODE_CLAN_RAID) then
+        return 0.5
+    end
+
     return 0.375
 end
