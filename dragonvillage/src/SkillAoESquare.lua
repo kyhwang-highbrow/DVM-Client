418549c12d31fdbb3883724aac6a0835f35e8361
mkang <mkang@highbrow.com>
2018-01-12 03:14:53 +0000

    [민승] AoERound, AoESquare 타입의 드래그 스킬이 딜타임이 끝나고 암전효과가 종료되도록 변경 (기존 메탈마그넷드래곤, 윗치드래곤, 데몽과 가고일 드래그 스킬에서 생기던 문제)
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@96064 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/SkillAoESquare.lua
+++ dragonvillage/src/SkillAoESquare.lua
@@ -1,4 +1,4 @@
-local PARENT = class(Skill, ISkillMultiAttack:getCloneTable())
+local PARENT = class(Skill, ISkillMultiAttack:getCloneTable(), IStateDelegate:getCloneTable())
 
 -------------------------------------
 -- class SkillAoESquare
