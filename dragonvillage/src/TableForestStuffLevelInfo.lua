100a979f271394f52757ebaa836949faa17c8f6f
mskim <mskim@highbrow.com>
2018-01-24 16:08:34 +0000

    [문성] ## 1. 드래곤의 숲 미번역 텍스트 추가
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@96857 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/TableForestStuffLevelInfo.lua
+++ dragonvillage/src/TableForestStuffLevelInfo.lua
@@ -120,8 +120,8 @@ function TableForestStuffLevelInfo:getStuffOptionDesc(stuff_type, lv)
 
     -- 보상이 하나인 경우
     if (reward_2 == '') then
-        local template = '{@item_name}{1}분{@DESC}마다 {@count}{2} {3}개{@DESC} 획득'
-        return Str(template, cool, item_name, comma_value(item_cnt))
+        return Str('{@item_name}{1}분{@DESC}마다 {@count}{2} {3}개{@DESC} 획득', 
+			cool, item_name, comma_value(item_cnt))
     -- 2개인 경우 : 갯수는 빼버린다.
     else
         -- 2번 보상 상세
@@ -130,8 +130,8 @@ function TableForestStuffLevelInfo:getStuffOptionDesc(stuff_type, lv)
         local item_name_2 = TableItem:getItemName(item_id_2)
         local item_cnt_2 = t_reward_2[2]
 
-        local template = '{@item_name}{1}분{@DESC}마다 {@count}{2} {3}개 {@DESC}또는 {@count}{4} {5}개{@DESC} 획득'
-        return Str(template, cool, item_name, comma_value(item_cnt), item_name_2, comma_value(item_cnt_2))
+        return Str('{@item_name}{1}분{@DESC}마다 {@count}{2} {3}개 {@DESC}또는 {@count}{4} {5}개{@DESC} 획득', 
+			cool, item_name, comma_value(item_cnt), item_name_2, comma_value(item_cnt_2))
     end
 end
 
