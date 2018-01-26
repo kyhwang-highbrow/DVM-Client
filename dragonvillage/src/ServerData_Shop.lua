0a60c7dc322113735b6b308e06f12bb5672615a7
klee <klee@highbrow.com>
2018-01-26 01:52:08 +0000

    [규석] 1. 탭조이 매출 지표 struct_proudct nil 체크 추가
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@96946 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/ServerData_Shop.lua
+++ dragonvillage/src/ServerData_Shop.lua
@@ -580,13 +580,16 @@ function ServerData_Shop:request_checkReceiptValidation(struct_product, validati
 
     -- 콜백 함수
     local function success_cb(ret)
+        
         -- @analytics
         -- 탭조이 매출은 모두 krw 가격으로 보냄
-        local krw_price = struct_product['price'] -- getPrice() 함수 나중에 수정하기
+        if (struct_product) then
+            local krw_price = struct_product['price'] -- getPrice() 함수 나중에 수정하기
 
-        Analytics:purchase(product_id, sku, krw_price)
-        Analytics:trackGetGoodsWithRet(ret, string.format('상품 구매 : %d', product_id))
-
+            Analytics:purchase(product_id, sku, krw_price)
+            Analytics:trackGetGoodsWithRet(ret, string.format('상품 구매 : %d', product_id))
+        end
+        
         g_serverData:networkCommonRespone(ret)
         g_serverData:networkCommonRespone_addedItems(ret)
     
