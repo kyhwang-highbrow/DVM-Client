c961b67fe5def953886e4f4ca98f8dfbbd431742
klee <klee@highbrow.com>
2018-01-30 03:10:12 +0000

    [규석] 1.교환 이벤트 swallow touch 처리 2.패키지 탭버튼 뱃지 추가
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@97109 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_PackageTabButton.lua
+++ dragonvillage/src/UI_PackageTabButton.lua
@@ -26,6 +26,26 @@ end
 -------------------------------------
 function UI_PackageTabButton:initUI()
     local vars = self.vars
+
+    -- 버튼 이름
+    local struct_event_popup_tab = self.m_structEventPopupTab
+    local tab_btn_name = struct_event_popup_tab:getTabButtonName()
+    vars['listLabel']:setString(tab_btn_name)
+
+    -- 패키지 뱃지
+    local package_name = struct_event_popup_tab.m_type
+    local t_pids = TablePackageBundle:getPidsWithName(package_name)
+    if (#t_pids > 0) then
+        local l_item_list = g_shopDataNew:getProductList('package')
+        local target_pid = tonumber(t_pids[1])
+        local struct_product = l_item_list[target_pid]
+        if (struct_product) then
+            local badge = struct_product:makeBadgeIcon()
+            if (badge) then
+		        vars['badgeNode']:addChild(badge)
+            end
+        end
+    end
 end
 
 -------------------------------------
@@ -40,7 +60,4 @@ end
 -------------------------------------
 function UI_PackageTabButton:refresh()
     local vars = self.vars
-    local struct_event_popup_tab = self.m_structEventPopupTab
-    local tab_btn_name = struct_event_popup_tab:getTabButtonName()
-    vars['listLabel']:setString(tab_btn_name)
 end
