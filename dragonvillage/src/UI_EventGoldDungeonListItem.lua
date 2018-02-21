2cc805cd7f63321e79e3c71f788e9fdf9d31a61e
klee <klee@highbrow.com>
2018-02-21 05:36:20 +0000

    [규석] 1.황금던전 누적보상 리스트 아이템 문구 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@97817 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_EventGoldDungeonListItem.lua
+++ dragonvillage/src/UI_EventGoldDungeonListItem.lua
@@ -29,7 +29,7 @@ function UI_EventGoldDungeonListItem:initUI()
     local price = data_info['price']
 
     -- 교환 갯수
-    vars['numberLabel']:setString(Str('{1}개', comma_value(price)))
+    vars['numberLabel']:setString(Str('{1}회', comma_value(price)))
 
     -- 보상 정보
     local l_reward = g_itemData:parsePackageItemStr(data_info['reward'])
