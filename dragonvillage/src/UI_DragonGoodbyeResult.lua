48e6e3e42ff806f202c511ed0266aba2e3b41ff7
klee <klee@highbrow.com>
2018-03-16 06:18:58 +0000

    [규석] 1.드래곤 작별 연출 - 스킵 기능 추가
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@98471 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_DragonGoodbyeResult.lua
+++ dragonvillage/src/UI_DragonGoodbyeResult.lua
@@ -41,28 +41,6 @@ end
 function UI_DragonGoodbyeResult:initUI()
     local vars = self.vars
 
-    -- 결과 팝업
-    local function show_popup()
-        local dragon_data = self.m_dragonData
-        local info_data = self.m_infoData
-        local l_item_list = info_data['items_list']
-	    if (l_item_list) then
-            local did = dragon_data['did']
-            local name = TableDragon():getDragonName(did)
-
-		    -- 얻은 인연포인트 텍스트를 만든다.
-		    local t_item = l_item_list[1]
-		    local goodbye_str_3 = UIHelper:makeGoodbyeStr(t_item, name)
-
-		    -- 획득 팝업 출력
-		    local ui = UI_ObtainPopup(l_item_list, goodbye_str_3)
-            ui:setCloseCB(function() self:fadeOutClose() end)
-	    end
-
-        -- 블럭 해제
-        UIManager:blockBackKey(false)
-    end
-
     -- 속도 조절용 (사운드가 1배속에 맞춰져있어 수정하지 못함)
     local time_scale = 1
 
@@ -85,7 +63,7 @@ function UI_DragonGoodbyeResult:initUI()
         visual:setIgnoreLowEndMode(true) -- 저사양 모드 무시
         visual:setTimeScale(time_scale)
         visual:setVisible(true)
-        visual:addAniHandler(function() show_popup() end)
+        visual:addAniHandler(function() self:showResult() end)
 
         local move_action = cc.MoveBy:create(1.5 / time_scale, cc.p(0, -800))
         local ease_action = cc.EaseIn:create(move_action, 2)
@@ -97,12 +75,41 @@ end
 -- function initButton
 -------------------------------------
 function UI_DragonGoodbyeResult:initButton()
+    local vars = self.vars
+    vars['skipBtn']:registerScriptTapHandler(function() self:click_skipBtn() end)
 end
 
 -------------------------------------
 -- function refresh
 -------------------------------------
 function UI_DragonGoodbyeResult:refresh()
+end
+
+-------------------------------------
+-- function showResult
+-------------------------------------
+function UI_DragonGoodbyeResult:showResult()
+    local vars = self.vars
+    vars['skipBtn']:setVisible(false)
+
+    local dragon_data = self.m_dragonData
+    local info_data = self.m_infoData
+    local l_item_list = info_data['items_list']
+	if (l_item_list) then
+        local did = dragon_data['did']
+        local name = TableDragon():getDragonName(did)
+
+		-- 얻은 인연포인트 텍스트를 만든다.
+		local t_item = l_item_list[1]
+		local goodbye_str_3 = UIHelper:makeGoodbyeStr(t_item, name)
+
+		-- 획득 팝업 출력
+		local ui = UI_ObtainPopup(l_item_list, goodbye_str_3)
+        ui:setCloseCB(function() self:fadeOutClose() end)
+	end
+
+    -- 블럭 해제
+    UIManager:blockBackKey(false)
 end
 
 -------------------------------------
@@ -113,6 +120,40 @@ function UI_DragonGoodbyeResult:fadeOutClose()
         SoundMgr:playPrevBGM()
         self:close()
     end)
+end
+
+-------------------------------------
+-- function click_skipBtn
+-------------------------------------
+function UI_DragonGoodbyeResult:click_skipBtn()
+    local vars = self.vars
+    vars['skipBtn']:setEnabled(false)
+
+    -- 페이드인 효과
+    self:sceneFadeInAction(nil, nil, 1.0)
+
+    -- 배경
+    do
+        local scr_size = cc.Director:getInstance():getWinSize()
+        local height_scale = CRITERIA_RESOLUTION_Y / scr_size['height']
+        local pos_y = 800 - (800 * height_scale)
+
+        local visual = vars['bg_visual']
+        visual:stopAllActions()
+        visual.m_node:setPositionY(pos_y)
+    end
+
+    -- 작별 연출
+    do
+        local visual = vars['effect_visual']
+        visual:addAniHandler(function() end)
+        visual:stopAllActions()
+        visual.m_node:setPositionY(-200)
+
+        visual:changeAni('result', false)
+    end
+
+    self:showResult()
 end
 
 --@CHECK
