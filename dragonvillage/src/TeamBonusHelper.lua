a17cfa3d7c236b525a19acdd1fabf05c14d7b507
klee <klee@highbrow.com>
2018-03-20 01:50:31 +0000

    [규석] 1.did 팀보너스 체크 함수 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@98516 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/TeamBonusHelper.lua
+++ dragonvillage/src/TeamBonusHelper.lua
@@ -224,6 +224,7 @@ function TeamBonusHelper:checkConditionFromDid(t_teambonus, did)
             -- DID 체크
             elseif (type == 'did') then
                 is_satisfy = (condition == did)
+                break
             end
         end
     end
