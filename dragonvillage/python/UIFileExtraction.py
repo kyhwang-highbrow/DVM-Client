5ebfd79715c1b7cc475128ee030303deac5c8e67
klee <klee@highbrow.com>
2018-03-23 10:05:14 +0000

    [규석] 1.ui파일 순회하며 lua_name, ui_name, file_name 검사하는 파이썬 파일 추가
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@98679 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/python/UIFileExtraction.py
+++ dragonvillage/python/UIFileExtraction.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 ## 개요
-## ui파일을 순회하며 png, vrp 리소스 추출
+## ui파일을 순회하며 file_name, lua_name, ui_name등 원하는 값 추출
 
 import os
 import sys
@@ -10,6 +10,8 @@ import shutil
 
 ## globals ############################
 ROOT_PATH = '../res'
+TARGET_NAME = ''
+READ_FAIL_LIST = []
 #######################################
 
 ## define loopAllFile
@@ -30,6 +32,7 @@ def readUIFile(filename):
         fr = open(file_path, 'r')
         extractionRes(fr)
     except:
+        READ_FAIL_LIST.append(filename)
         print '\n### '+filename+'\n'+'read fail!!!'
         pass
 
@@ -41,35 +44,58 @@ def extractionRes(fr):
     for line in fr:
         data = data + line.strip()
     data = data.replace(" ", "")
-    vars_target = re.findall(r"file_name='[\w\d\s+-/*]*'", data)
+    vars_target = re.findall(r"{0}='[\w\d\s+-/*]*'".format(TARGET_NAME), data)
 
     for i in range(len(vars_target)):
-        if vars_target[i] == "file_name=''":
-            try:
-                vars_target.remove(i)
-            except:
-                pass
+        if vars_target[i] == "{0}=''".format(TARGET_NAME):
+            try: vars_target.remove(i)
+            except: pass
 
     for i in range(len(vars_target)):
         _res = vars_target[i]
         _res = re.sub("'", "", _res)
-        _res = re.sub("file_name=", "", _res)
+        _res = re.sub("{0}=".format(TARGET_NAME), "", _res)
         if len(_res) > 0:
             res_list.append(_res)
 
     for var in res_list:
-        try:
-            print var
-        except:
-            pass
+        try: print var
+        except: pass
+
+## define retryReadFile
+## read fail한 파일들 있으면 다시한번 시도
+def retryReadFile():
+    if len(READ_FAIL_LIST) > 0:
+        for i in range(len(READ_FAIL_LIST)):
+            try:
+                var = READ_FAIL_LIST[i]
+                READ_FAIL_LIST.remove(i)
+                readUIFile(var)
+            except:
+                pass
+
+## define finishReadFile
+## read fail한 파일들 있으면 출력
+def finishReadFile():
+    if len(READ_FAIL_LIST) > 0:
+        print '\n\n##READ_FAIL_LIST'
+        for var in READ_FAIL_LIST:
+            try: print var
+            except: pass
+    else:
+        print '\n\n##SUCCESS'
 
 ###################################
 # MAIN
 ###################################
 if __name__ == '__main__':
-    print '## find resource in ui file'
+    print 'UIResExtraction'
+    print
+    print 'Type the name you want to extract in ui file!\n(ex:file_name, lua_name, ui_name)'
+    TARGET_NAME = raw_input()
     loopAllFile()
-
+    retryReadFile()
+    finishReadFile()
 else:
     print '## I am being imported from another module'
     
