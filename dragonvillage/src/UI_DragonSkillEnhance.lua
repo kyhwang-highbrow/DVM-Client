345c498e77fa9c3c9d8d087276fa45453306f721
mskim <mskim@highbrow.com>
2018-07-26 01:45:10 +0000

    [문성] ## 1. 스킬슬라임 상점 닫음
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@102158 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_DragonSkillEnhance.lua
+++ dragonvillage/src/UI_DragonSkillEnhance.lua
@@ -186,7 +186,7 @@ function UI_DragonSkillEnhance:refresh()
     self:refresh_dragonSkillMoveTableView()
 
     -- 스킬 슬라임 상점 버튼 갱신
-	self.vars['skillSlimShopBtn']:setVisible(self.m_selectDragonData:getRarity() == 'legend')
+	vars['skillSlimShopBtn']:setVisible(false)--self.m_selectDragonData:getRarity() == 'legend')
 end
 
 -------------------------------------
