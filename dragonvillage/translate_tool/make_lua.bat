e99fccfac798181cab4e7af63b5282db1a4043fa
mskim <mskim@highbrow.com>
2018-08-10 03:04:28 +0000

    [문성] ## 1. 번역 툴 스페인어 추가 2. make_lua.bat에서 루아 검증 삭제 - lua path 차이로 실행 안되는 듯 - 이부분은 따로 만들어야 함 3. legacy 파일 삭제
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@102444 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/translate_tool/make_lua.bat
+++ dragonvillage/translate_tool/make_lua.bat
@@ -4,14 +4,7 @@ set HOD_ROOT=%cd%
 ::test
 ::node --max-old-space-size=4096 tools/make_lua.js test_onlyingame;test_onlyscenario 1s3m5A7rl4JHngXFknMd3MTkbf0vVaAIPoRx3GPHJvoo en;jp;zhtw
 ::live
-node --max-old-space-size=4096 tools/make_lua.js only_ingame;only_scenario 1M_in-ZIMHsXvkSu_EBktrtDxBVpKXSbl0l4wpljU1gw en;jp;zhtw;th
-
-::루아 검증
-cd "../translate"
-call "../../../res/tools/lua/bin/lua.exe" "checkLua.lua" en
-call "../../../res/tools/lua/bin/lua.exe" "checkLua.lua" jp
-call "../../../res/tools/lua/bin/lua.exe" "checkLua.lua" zhtw
-call "../../../res/tools/lua/bin/lua.exe" "checkLua.lua" th
+node --max-old-space-size=4096 tools/make_lua.js only_ingame;only_scenario 1M_in-ZIMHsXvkSu_EBktrtDxBVpKXSbl0l4wpljU1gw en;jp;zhtw;th;es
 
 ::커밋
 TortoiseProc /command:commit /path:%cd% /closeonend:1
