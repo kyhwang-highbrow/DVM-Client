1a1f7072ec4b84d28801a4bdcec359bdfb769728
sgkim <sgkim@highbrow.com>
2018-08-24 06:52:45 +0000

    [성구] #구글 피처드 대응, 18.5:9 해상도 대응
    - 마녀의 상점 배경 사이즈 안맞는 부분 수정
    
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@102719 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_RandomShop.lua
+++ dragonvillage/src/UI_RandomShop.lua
@@ -68,6 +68,10 @@ function UI_RandomShop:initUI()
     do -- 갱신시간
         self:registerUpdate()
     end
+
+    -- 리소스가 1280길이로 제작되어 보정 (더 와이드한 해상도)
+    local scr_size = cc.Director:getInstance():getWinSize()
+    vars['bgVisual']:setScale(scr_size.width / 1280)
 end
 
 -------------------------------------
