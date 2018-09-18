a51d69b12654d97be6c7efb7717336f374c0b64b
mskim <mskim@highbrow.com>
2018-09-18 12:50:31 +0000

    [문성] ## 1. 콜로세움 결과화면 ui 점수판 visible on
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@103197 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_ArenaResult.lua
+++ dragonvillage/src/UI_ArenaResult.lua
@@ -48,6 +48,8 @@ function UI_ArenaResult:initUI()
     vars['resultMenu']:setVisible(false)
     local ori_y = vars['resultMenu']:getPositionY()
     vars['resultMenu']:setPositionY(ori_y - ACTION_MOVE_Y)
+
+	vars['colosseumNode']:setVisible(true)
 end
 
 -------------------------------------
