a5125972fc1c40070fe308210cd69ce44637b50e
jhakim <jhakim@highbrow.com>
2018-12-17 07:50:25 +0000

    [정하]#알파벳 이벤트 - 획득처 ui에 스크롤 추가
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@104784 5003bd00-c432-460b-831f-0659da9acde2

--- dragonvillage/src/UI_EventAlphabetInfoPopup.lua
+++ dragonvillage/src/UI_EventAlphabetInfoPopup.lua
@@ -32,6 +32,36 @@ end
 -------------------------------------
 function UI_EventAlphabetInfoPopup:initUI()
     local vars = self.vars
+    
+    local scroll_node = vars['eventScrollNode']
+    local scroll_menu = vars['eventScrollMenu']
+
+    -- ScrollView 사이즈 설정 (ScrollNode 사이즈)
+    local size = scroll_node:getContentSize()
+    local scroll_view = cc.ScrollView:create()
+    scroll_view:setNormalSize(size)
+    scroll_node:setSwallowTouch(false)
+    scroll_node:addChild(scroll_view)
+
+    -- ScrollView 에 달아놓을 컨텐츠 사이즈(ScrollMenu)
+    local target_size = scroll_menu:getContentSize()
+    scroll_view:setContentSize(target_size)
+    scroll_view:setDockPoint(CENTER_POINT)
+    scroll_view:setAnchorPoint(CENTER_POINT)
+    scroll_view:setPosition(ZERO_POINT)
+    scroll_view:setTouchEnabled(true)
+
+    -- ScrollMenu를 부모에서 분리하여 ScrollView에 연결
+    -- 분리할 부모가 없을 때 에러 없음
+    scroll_menu:removeFromParent()
+    scroll_view:addChild(scroll_menu)
+
+    -- 스크롤 초기 위치를 위한 계산
+    local container_node = scroll_view:getContainer()
+    local size_y = size.height - target_size.height
+    
+    container_node:setPositionY(size_y)
+    scroll_view:setDirection(cc.SCROLLVIEW_DIRECTION_VERTICAL)
 
 end
 
