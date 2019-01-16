58f970560d0b8cc08861fca99d0bbbe20417492d
jhakim <jhakim@highbrow.com>
2019-01-16 04:16:12 +0000

    [정하]#주사위 이벤트 - 스타트 지점에 이미지 겹치지 않게 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@105142 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 9f0b59e692d58b10bdc44ffdce9b6fc613ecc603

--- dragonvillage/src/UI_DiceEvent.lua
+++ dragonvillage/src/UI_DiceEvent.lua
@@ -162,11 +162,9 @@ function UI_DiceEvent:refresh()
     -- 최초 출발 처리
     local is_first = ((curr_cell == 1) and (lap_cnt == 0))
     local first_cell_ui = self.m_cellUIList[1]
-    if (is_first) then
-        first_cell_ui.vars['startSprite']:setVisible(is_first)
-    elseif (first_cell_ui.vars['startSprite']:isVisible()) then
-        first_cell_ui.vars['startSprite']:setVisible(false)
-    end
+
+    first_cell_ui.vars['startSprite']:setVisible(is_first)
+    first_cell_ui.vars['iconNode']:setVisible(not is_first)
 
     -- 완주 보상 UI 처리
     for i, t_ui in ipairs(self.m_lapRewardInfoList) do
