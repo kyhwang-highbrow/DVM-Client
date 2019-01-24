17d8074e3fe9fa968a832046b116d90de35ecfbb
jhakim <jhakim@highbrow.com>
2019-01-24 07:48:45 +0000

    [정하]#고대 유적 던전 - 고대 유적 던전 층 다 뚫린 문제 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@105305 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 187574367d2a206e5e538c290a675e663493d7b7

--- dragonvillage/src/ServerData_Stage.lua
+++ dragonvillage/src/ServerData_Stage.lua
@@ -152,9 +152,7 @@ function ServerData_Stage:isOpenStage(stage_id)
 
     -- 고대 유적 던전 모드
     elseif (game_mode == GAME_MODE_ANCIENT_RUIN) then
-        if g_ancientRuinData then
-            ret = g_ancientRuinData:isOpenAncientRuin()             
-        end
+        ret = g_nestDungeonData:isOpenStage(stage_id)
 
     -- 룬 수호자 던전 (모든 스테이지가 열린 상태로 시작)
     elseif (game_mode == GAME_MODE_RUNE_GUARDIAN) then
