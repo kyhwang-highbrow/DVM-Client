ce296d1a7f17fcfbece78969cbd4181edc202fb3
jhakim <jhakim@highbrow.com>
2019-03-07 05:03:05 +0000

    [정하]#빙고 이벤트 - 숫자 막 바뀌다가 해당 숫자 뽑는 액션 함수 추가 - 빙고 글자 색상 적용 - 버튼 세팅
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@105976 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: acadf2b82759a7e6cabffed9ace498fe3245dbea

--- dragonvillage/src/UI_EventBingo.lua
+++ dragonvillage/src/UI_EventBingo.lua
@@ -44,6 +44,10 @@ end
 function UI_EventBingo:initButton()
     local vars = self.vars
 
+    vars['notRewardBtn']:registerScriptTapHandler(function() self:click_infoBtn() end)
+    vars['playBtn1']:registerScriptTapHandler(function() self:click_infoBtn() end)
+    vars['playBtn2']:registerScriptTapHandler(function() self:click_infoBtn() end)
+    vars['infoBtn']:registerScriptTapHandler(function() self:click_infoBtn() end)
 end
 
 -------------------------------------
@@ -74,7 +78,7 @@ function UI_EventBingo:setBingo(bingo_type, line) -- HORIZONTAL, VERTICAL, CROSS
 end
 
 -------------------------------------
--- function existType
+-- function getLinePos
 -------------------------------------
 function UI_EventBingo:getLinePos(bingo_type, line) -- param 의미 : 가로 3 번 째줄, 세로 1 번 째줄
     local vars = self.vars
@@ -105,35 +109,51 @@ function UI_EventBingo:getLinePos(bingo_type, line) -- param 의미 : 가로 3 번 째
 end
 
 -------------------------------------
--- function existType
+-- function pickNumberAction
+-- @brief 숫자들이 막 바뀌다가 고정되는 액션
 -------------------------------------
-function UI_EventBingo:getLinePos(bingo_type, line) -- param 의미 : 가로 3 번 째줄, 세로 1 번 째줄
+function UI_EventBingo:pickNumberAction(number)
     local vars = self.vars
-
-    local pos_x, pos_y = 0, 0
-    local offset = 40 -- 빙고칸 절반크기 조금 안되는 위치
-
-    if (bingo_type == BINGO_TYPE.HORIZONTAL) then        -- 가로 빙고 : line 첫 칸 - offsetX
-        local _line = 1 + (line-1) * 5
-        pos_x, pos_y = vars['bingoNode'.._line]:getPosition()
-        pos_x = pos_x - offset
-
-    elseif (bingo_type == BINGO_TYPE.VERTICAL) then       -- 세로 빙고 : line 첫 줄 + offsetY
-        pos_x, pos_y = vars['bingoNode'..line]:getPosition()
-        pos_y = pos_y + offset
-
-    elseif (bingo_type == BINGO_TYPE.CROSS_LEFT_TO_RIGHT) then  -- 대각선 빙고(left_to_right) : 1번 칸 - offsetX + offsetY
-        pos_x, pos_y = vars['bingoNode1']:getPosition()
-        pos_x = pos_x - offset
-        pos_y = pos_y + offset
-    elseif (bingo_type == BINGO_TYPE.CROSS_RIGHT_TO_LEFT) then  -- 대각선 빙고(right_to_left) : 1번 칸 + offsetX + offsetY
-        pos_x, pos_y = vars['bingoNode5']:getPosition()
-        pos_x = pos_x + offset
-        pos_y = pos_y + offset
+    local change_speed = 0.05
+    local repeat_cnt = 12
+    
+    if (not vars['pickAniSprite']) then
+        return
     end
+    
+    vars['pickAniSprite']:setVisible(true)
 
-    return pos_x, pos_y
-end 
+    local random_frunc = function()
+        local num = math_random(24) + 1
+        local _num = string.format('%03d', num) --ex) 001, ..023 3자리 형식
+        local num_sprite_name = string.format('res/ui/icons/bingo/%s.png', _num)
+        vars['pickAniSprite']:setTexture(num_sprite_name)
+    end
+    
+    local end_frunc = function()
+        local num = number
+        local _num = string.format('%03d', num) --ex) 001, ..023 3자리 형식
+        local num_sprite_name = string.format('res/ui/icons/bingo/%s.png', _num)
+        vars['pickAniSprite']:setTexture(num_sprite_name)
+    end 
+    
+    -- 랜덤으로 바뀌는 효과
+    local random_action = cc.CallFunc:create(random_frunc)
+    local delay_action = cc.DelayTime:create(change_speed)
+    local repeat_sequence_action = cc.Sequence:create(random_action, delay_action)
+    local repeat_action = cc.Repeat:create(repeat_sequence_action, repeat_cnt)
+    local end_action = cc.CallFunc:create(end_frunc)
+    local sequence_action = cc.Sequence:create(repeat_action, end_action)
+
+    cca.runAction(self.root, sequence_action, nil)
+end
+
+-------------------------------------
+-- function click_infoBtn
+-------------------------------------
+function UI_EventBingo:click_infoBtn()
+
+end
 
 
 
@@ -170,6 +190,7 @@ function UI_EventBingoListItem:initUI()
     local _num = string.format('%03d', self.m_bingoInd) --ex) 001, ..023 3자리 형식
     local num_sprite_name = string.format('res/ui/icons/bingo/%s.png', _num)
     vars['numberSprite']:setTexture(num_sprite_name)
+    vars['numberSprite']:setColor(cc.c3b(240,215,159))
 end
 
 -------------------------------------
@@ -181,15 +202,16 @@ function UI_EventBingoListItem:setActiveNumber()
     local action_speed = 0.1
     
     local func_active = function()
-        vars['clearSprite']:setVisible(true)   
+        vars['clearSprite']:setVisible(true)
+        vars['numberSprite']:setColor(cc.c3b(72,25,0))
     end 
     
     -- 카드 뒤집는 효과
     local flip_action = cc.ScaleTo:create(action_speed, 0, 1)
     local cb_frunc = cc.CallFunc:create(func_active)
     local flip_action_reverse = cc.ScaleTo:create(action_speed, 1, 1)
-	local sequence = cc.Sequence:create(flip_action, cb_frunc, flip_action_reverse)
-    cca.runAction(self.root, sequence, nil)
+	local sequence_action = cc.Sequence:create(flip_action, cb_frunc, flip_action_reverse)
+    cca.runAction(self.root, sequence_action, nil)
 end
 
 -------------------------------------
