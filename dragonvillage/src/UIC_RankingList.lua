2ea8ed77390d7edeb895329b8b9e71c658ea41a2
jhakim <jhakim@highbrow.com>
2019-03-19 01:04:32 +0000

    [정하]#UIC_RankingList.lua 주석 추가&수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@106219 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 4634818840aeb4423e6e818b2f53f10a8f8f5d92

--- dragonvillage/src/UIC_RankingList.lua
+++ dragonvillage/src/UIC_RankingList.lua
@@ -12,8 +12,25 @@ UIC_RankingList = class({
         m_prevCb = 'function',
         m_nextCb = 'function',
         m_makeMyRankCb = 'function',
+        m_cellCount = 'number',
 
     })
+
+-------------------------------------------------------
+
+-- UIC_RankingList 사용 예시(순서 지켜야함)
+
+-- local rank_list = UIC_RankingList()                                  -- step0. (필수)랭킹 UI 컴포넌트 생성
+-- rank_list:setRankUIClass(_UI_ClanRaidRankListItem, nil)              -- step1. (필수)셸 UI 설정 
+-- rank_list:setRankList(l_rank_list)                                   -- step2. (필수)리스트 설정
+-- rank_list:makeRankMoveBtn(click_func, click_func, CLAN_OFFSET_GAP)   -- step3. (선택)이전, 다음 버튼 추가할 것인가 (param : (이전)눌렀을 때 콜백 함수, (다음)눌렀을 때 콜백 함수, 이동할 갯수)
+-- rank_list:setEmptyStr('')                                            -- step4. (선택)랭킹이 없을 때, 메세지 설정
+-- rank_list:setOffset(offset)                                          -- step5. (선택)몇 랭킹부터 보여줄 것인가 (1 이면 최상위 랭킹 부터, -1이면 내 랭킹 부터(내 랭킹 모를 때, 통신용))
+-- rank_list:setOffset:setMyRank(make_my_rank_cb)                       -- step6. (선택)내 랭킹 만드는 콜백 함수
+-- rank_list:setOffset:makeRankList(node)                               -- step7. (필수)실제로 랭킹 생성
+-- rank_list:setFocus('id', focus_value)                                -- step8. (선택)해당 리스트에서 ex) id가  focus_value인 값에 포커싱 and 하이라이트(셀 UI에 vars['mySprite']가 있어야함)
+
+-------------------------------------------------------
 
 -------------------------------------
 -- function init
@@ -62,7 +79,10 @@ end
 function UIC_RankingList:makeRankMoveBtn(prev_cb, next_cb, offset_gap)
     local l_item = self.m_itemList
     self.m_offsetGap = offset_gap
-
+    self.m_cellCount = #l_item
+    
+    -- 첫 통신할 때 내 순위를 받아오기 위해 offset을 -1 로 들고 있음
+    -- 첫 통신 이후에는 offset에 옳은 값에 넣어줌 (offset : offset (랭킹)부터 시작하는 랭킹 리스트를 받음)
     if (self.m_offset == -1) then
         if (l_item_list[1] or l_item_list[1]) then
             self.m_offset = l_item_list[1]['rank']
@@ -72,11 +92,13 @@ function UIC_RankingList:makeRankMoveBtn(prev_cb, next_cb, offset_gap)
     -- 이전 보기 추가
     if (1 < self.m_offset) then
         l_item['prev'] = 'prev'
+        self.m_cellCount = self.m_cellCount + 1
     end
     
     -- 다음 보기 추가
     if (#l_item > 0) then
         l_item['next'] = 'next'
+        self.m_cellCount = self.m_cellCount + 1
     end
 
     self.m_prevCb = prev_cb
@@ -143,6 +165,9 @@ end
 
 -------------------------------------
 -- function setFocus
+-- @brief 포커싱과 하이라이트는 같이 있어야 할 기능이라 묶어버림, 추후에 필요하다면 매개변수 bool값 추가
+-- @brief 인덱스로도 하이라이트 가능(UI_ChallengRankingPopup 참고), 이 경우에는 테이블 셸 UI들이 모두 생성된 상태여야함 -> table_view:setItemList(l_item, true) -- 아이템 UI를 미리 다 만듬
+-- @brief 하지만 랭킹이 많을 경우, 퍼포먼스가 떨어지기 때문에 아이템을 미리 다 만들어 놓지 않는 것을 디폴트로 함
 -------------------------------------
 function UIC_RankingList:setFocus(value_type, value)
     local l_item = self.m_itemList
@@ -205,7 +230,7 @@ function UIC_RankingList:click_next()
         return
     end
 
-    self.m_offset = self.m_offset + #l_item - 2
+    self.m_offset = self.m_offset + self.m_cellCount
 
     if (self.m_nextCb) then
         self.m_nextCb()
