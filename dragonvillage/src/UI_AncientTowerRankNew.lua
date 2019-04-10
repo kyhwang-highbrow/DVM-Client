7697b298a9011330f8fe0f9a1643db0800fd7fdd
sgkim <sgkim@highbrow.com>
2019-04-10 10:27:55 +0000

    [성구] #고대의 탑 개인 랭킹에서 친구 랭킹, 클랜원 랭킹 정렬 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@106658 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 5b9833816d6350e1e1a20657ce25a879c7c136e8

--- dragonvillage/src/UI_AncientTowerRankNew.lua
+++ dragonvillage/src/UI_AncientTowerRankNew.lua
@@ -284,32 +284,6 @@ function UI_AncientTowerRankNew:init_rankTableView()
 
     local l_item_list = g_ancientTowerData.m_lGlobalRank
 
-    local sort_func = function(a, b)
-        local a_rank = a['m_rank']
-        local b_rank = b['m_rank']
-
-        if (not a_rank) then
-            return true
-        end
-
-        if (not b_rank) then
-            return true
-        end
-        
-        if (a_rank < 1) then
-            return true
-        end
-        
-        if (b_rank < 1) then
-            return true
-        end
-
-        return a_rank < b_rank
-    end
-
-    table.sort(l_item_list, sort_func)
-
-
     if (self.m_rankOffset > 1) then
         local prev_data = { m_tag = 'prev' }
         l_item_list['prev'] = prev_data
@@ -369,10 +343,10 @@ function UI_AncientTowerRankNew:init_rankTableView()
             end
 
             -- 랭킹으로 선별
-            local a_rank = a_data.m_rank
-            local b_rank = b_data.m_rank 
+            local a_rank = conditionalOperator((0 < a_data.m_rank), a_data.m_rank, 9999999)
+            local b_rank = conditionalOperator((0 < b_data.m_rank), b_data.m_rank, 9999999)
+
             return a_rank < b_rank
-            
         end
 
         table.sort(table_view.m_itemList, sort_func)
