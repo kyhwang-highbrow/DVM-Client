2cdd1e4bfd50c156d37e2ef8a0e2e49d7379a436
jhakim <jhakim@highbrow.com>
2019-04-23 09:28:13 +0000

    [정하]#고대의 탑 랭킹 - 클랜 랭킹 -> 클랜원 랭킹
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@106867 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: fbfd1a296a8f7931b0a0c5b5ff5db4d4ce69b5ba

--- dragonvillage/src/UI_AncientTowerRankNew.lua
+++ dragonvillage/src/UI_AncientTowerRankNew.lua
@@ -129,7 +129,7 @@ function UI_AncientTowerRankNew:make_UIC_SortList()
     uic:addSortType('my', Str('내 랭킹'))
     uic:addSortType('top', Str('최상위 랭킹'))
     uic:addSortType('friend', Str('친구 랭킹'))
-    uic:addSortType('clan', Str('클랜 랭킹'))
+    uic:addSortType('clan', Str('클랜원 랭킹'))
 
     uic:setSortChangeCB(function(sort_type) self:onChangeRankingType(sort_type) end)
     uic:setSelectSortType('my')
