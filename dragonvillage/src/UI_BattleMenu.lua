5af8267d961f99a855384283c8b7104fcd430cff
jhakim <jhakim@highbrow.com>
2019-05-03 08:05:54 +0000

    [정하]#황금 던전 - 입장권 있을 때 던전 탭에 노티 표시
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@107047 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: c15df07b4b8adecf91189ff1a58d6148aaf1eb21

--- dragonvillage/src/UI_BattleMenu.lua
+++ dragonvillage/src/UI_BattleMenu.lua
@@ -147,6 +147,13 @@ function UI_BattleMenu:update(dt)
         if (g_secretDungeonData:isSecretDungeonExist()) then
             t_noti['dungeon'] = true
         end
+
+        local event_data = g_eventGoldDungeonData
+        -- 현재 입장권 개수
+        local stamina_cnt = event_data:getStaminaCount()
+        if (stamina_cnt > 0) then
+            t_noti['dungeon'] = true
+        end
     end
     UIHelper:autoNoti(t_noti, self.m_tNotiSprite, 'Btn', self.vars)
 end
