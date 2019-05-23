711cf99c8c1879c7698a008a502ef3971f5110da
jhakim <jhakim@highbrow.com>
2019-05-23 06:19:04 +0000

    [정하]#환상 던전 - (임의로) 보스 체력 검사 안하도록 수정 - 체력 2번씩 깍는 중->수정해야함
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@107345 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: d8a64ab760653e48621ad7e8807cc58cae33d621

--- dragonvillage/src/GameState_Illusion.lua
+++ dragonvillage/src/GameState_Illusion.lua
@@ -71,3 +71,66 @@ function GameState_Illusion:makeResultUI(is_success)
     func_network_game_finish()
     --]]
 end
+
+-------------------------------------
+-- function onEvent
+-- @brief
+-------------------------------------
+function GameState_Illusion:onEvent(event_name, t_event, ...)
+    PARENT.onEvent(self, event_name, t_event, ...)
+
+    -- 보스 체력 공유 처리
+    if (event_name == 'character_set_hp') then
+        local prev_hp = t_event['prev_hp']
+        local new_hp = t_event['hp']
+
+        -- 이미 타임 아웃된 경우 점수 처리 하지 않음
+        if (self.m_bTimeOut) then return end
+
+        --[[
+        -- 이전 체력이 동일한지 검사
+        local safed_hp = self.m_bossHp:get()
+        if (math_floor(prev_hp) ~= math_floor(safed_hp)) then
+            -- 값이 동일하지 않을 경우 해킹인 것으로 간주해서 체력을 깍지 않음
+            new_hp = safed_hp
+        end
+        --]]
+        -- 현재 체력 정보 갱신
+        self:setBossHp(new_hp)
+
+        -- 누적 데미지 갱신(정확히는 체력을 깍은 양)
+        local accum_damage = self.m_orgBossHp:get() - new_hp
+        accum_damage = math_floor(math_max(accum_damage, 0))
+        self.m_accumDamage:set(accum_damage)
+        
+        -- UI 갱신
+        local final_damage = self.m_finalDamage
+        local total_damage = accum_damage + final_damage
+
+        self.m_world.m_inGameUI:setTotalDamage(total_damage)
+
+    -- 보스 막타 데미지
+    elseif (event_name == 'clan_boss_final_damage') then
+        local damage = t_event['damage']
+        local skill_id = t_event['skill_id']
+
+        -- 이미 타임 아웃된 경우 점수 처리 하지 않음
+        if (self.m_bTimeOut) then return end
+
+        if (not self.m_finalSkillId) then
+            self.m_finalSkillId = skill_id
+        elseif (self.m_finalSkillId ~= skill_id) then
+            return
+        end
+
+        -- 막타 데미지 갱신
+        self.m_finalDamage = self.m_finalDamage + math_floor(damage)
+
+        -- UI 갱신
+        local accum_damage = self.m_accumDamage:get()
+        local final_damage = self.m_finalDamage
+        local total_damage = accum_damage + final_damage
+
+        self.m_world.m_inGameUI:setTotalDamage(total_damage)
+    end
+end
