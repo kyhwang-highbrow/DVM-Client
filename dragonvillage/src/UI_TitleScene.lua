15f8f8b2f6dd6ff509c0f1911d7b609d4f5d060e
sgkim <sgkim@highbrow.com>
2019-05-29 05:48:11 +0000

    [성구] #카페 바자르 빌드에서 Firebase를 사용하지 않는 로그인 과정 거치도록 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@107444 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 85dbf0adeda76da7798bf41cc7c055f77c1c6def

--- dragonvillage/src/UI_TitleScene.lua
+++ dragonvillage/src/UI_TitleScene.lua
@@ -485,7 +485,22 @@ function UI_TitleScene:workCheckUserID()
 
     SoundMgr.m_bStopPreload = false
 
-    if isWin32() then
+    -- Firebase Authentication으로 로그인 처리가 불가능지 여부
+    local not_available_firebase = false
+
+    -- 윈도우에서는 firebase 사용 불가
+    if (isWin32()) then
+        not_available_firebase = true
+    end
+
+    -- 카페 바자르 빌드에서는 이란 제재로 firebase 서비스 불가
+    if (CppFunctions:isCafeBazaarBuild() == true) then
+        not_available_firebase = true
+    end
+
+
+    -- Firebase Authentication으로 로그인 처리가 불가능할 경우
+    if (not_available_firebase == true) then
         local uid = g_localData:get('local', 'uid')
         if uid then
             self:doNextWork()
