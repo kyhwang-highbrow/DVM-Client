7dc108ba2e6f4db8dbadb6f09cb462bc948ede89
jhakim <jhakim@highbrow.com>
2019-05-29 07:56:41 +0000

    [정하]#환상 던전 - 보스 폭주 데미지 감소시킴 함수 수정 - 누락된 테이블 추가
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@107450 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 283b98073ffbbc4f6287799c318e2672df5e8f23

--- dragonvillage/src/SkillScript_Illusion.lua
+++ dragonvillage/src/SkillScript_Illusion.lua
@@ -1,11 +1,31 @@
-local PARENT = SkillScript_ClanRaidBoss
+local PARENT = SkillScript
 
 local CON_SKILL_IDLE = 'skill_idle'
 
 -------------------------------------
--- class SkillScript_ClanRaidBoss
+-- class SkillScript_Illusion
 -------------------------------------
 SkillScript_Illusion = class(PARENT, {})
+
+-------------------------------------
+-- function initEventListener
+-- @breif 이벤트 처리..
+-------------------------------------
+function SkillScript_Illusion:initEventListener()
+    PARENT.initEventListener(self)
+
+    self:addListener(CON_SKILL_IDLE, self)
+end
+
+-------------------------------------
+-- function setSkillParams
+-- @brief 멤버변수 정의
+-------------------------------------
+function SkillScript_Illusion:setSkillParams(owner, t_skill, t_data)
+    PARENT.setSkillParams(self, owner, t_skill, t_data)
+
+    self.m_lTargetChar = self.m_world:getDragonList()
+end
 
 -------------------------------------
 -- function init_skill
@@ -38,4 +58,109 @@ function SkillScript_Illusion:init_skill(script_name, duration)
         source = '',
     })
     table.insert(self.m_lStatusEffect, struct_status_effect)
+end
+
+-------------------------------------
+-- function initState
+-------------------------------------
+function SkillScript_Illusion:initState()
+    self:setCommonState(self)
+	
+    self:addState('start', SkillScript_Illusion.st_appear, nil, false)
+    self:addState('attack', SkillScript_Illusion.st_attack, nil, false)
+    self:addState('end', SkillScript_Illusion.st_disappear, nil, false)
+end
+
+-------------------------------------
+-- function st_appear
+-------------------------------------
+function SkillScript_Illusion.st_appear(owner, dt)
+	if (owner.m_stateTimer == 0) then
+        local unit = owner.m_owner
+
+        -- 주체 유닛 애니 설정
+        unit.m_animator:changeAni('skill_appear', false)
+        unit.m_animator:addAniHandler(function()
+            owner:changeState('attack')
+        end)
+    end
+end
+
+-------------------------------------
+-- function st_attack
+-------------------------------------
+function SkillScript_Illusion.st_attack(owner, dt)
+	if (owner.m_stateTimer == 0) then
+        local unit = owner.m_owner
+
+        -- 주체 유닛 애니 설정
+        unit.m_animator:changeAni('skill_idle', true)
+
+        -- 미사일 발사
+        owner:runAttack()
+                
+        -- idle 애니메이션 시작시 발동되는 status effect를 적용
+		owner:dispatch(CON_SKILL_IDLE, {l_target = {owner.m_targetChar}})
+	end
+
+    if (owner.m_stateTimer >= owner.m_duration) then
+        owner:changeState('end')
+    end
+end
+
+-------------------------------------
+-- function st_disappear
+-------------------------------------
+function SkillScript_Illusion.st_disappear(owner, dt)
+	if (owner.m_stateTimer == 0) then
+        local unit = owner.m_owner
+
+        -- 주체 유닛 애니 설정
+        unit.m_animator:changeAni('skill_disappear', false)
+        unit.m_animator:addAniHandler(function()
+            owner:changeState('dying')
+        end)
+    end
+end
+
+-------------------------------------
+-- function runAttack
+-------------------------------------
+function SkillScript_Illusion:runAttack()
+    -- attack pos 가져옴
+    local pos = self:getOwnerAttackPos('skill_idle')
+    if (not pos) then
+        pos = { x = 0, y = 0 }
+    end
+
+    self:do_script_shot(pos['x'], pos['y'], PHYS.MISSILE.ENEMY)
+end
+
+-------------------------------------
+-- function makeSkillInstance
+-------------------------------------
+function SkillScript_Illusion:makeSkillInstance(owner, t_skill, t_data)
+	-- 변수 선언부
+	------------------------------------------------------
+	local res = t_skill['res_1']
+    local script_name = t_skill['val_1']
+    local duration = t_skill['val_2']
+	
+	-- 인스턴스 생성부
+	------------------------------------------------------
+	-- 1. 스킬 생성
+    local skill = SkillScript_Illusion(res)
+	-- 2. 초기화 관련 함수
+	skill:setSkillParams(owner, t_skill, t_data)
+    skill:init_skill(script_name, duration)
+	skill:initState()
+
+	-- 3. state 시작 
+    skill:changeState('delay')
+
+    -- 4. Physics, Node, GameMgr에 등록
+    local world = skill.m_owner.m_world
+    local missileNode = world:getMissileNode('bottom')
+    missileNode:addChild(skill.m_rootNode, 0)
+    world:addToSkillList(skill)
 end
