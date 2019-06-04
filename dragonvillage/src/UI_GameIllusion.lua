c5550418fecfa8ce73fdcbdcad1395c81bf28640
jhakim <jhakim@highbrow.com>
2019-06-04 09:44:05 +0000

    [정하]#환상 던전 - 인게임 중, 연속 전투 클릭했을 경우 환상 던전 연속전투 팝업 뜨도록 수정 - 환상 던전 인게임의 self.m_gameMode는 황금던전인 상태
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@107573 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: d4a4eb11021e6effc4adeba48edcc87328e73bc2

--- dragonvillage/src/UI_GameIllusion.lua
+++ dragonvillage/src/UI_GameIllusion.lua
@@ -35,3 +35,51 @@ function UI_GameIllusion:setAutoPlayUI()
     vars['autoStartNumberLabel']:setString(Str('{1}회 반복중', g_autoPlaySetting:getAutoPlayCnt()))
     vars['autoStartVisual']:setVisible(g_autoPlaySetting:isAutoPlay())
 end
+
+-------------------------------------
+-- function click_autoStartButton
+-- @brief 연속 전투 .. 자동 아님
+-------------------------------------
+function UI_GameIllusion:click_autoStartButton()
+	-- 튜토리얼 진행 중 block
+	local stage_id = self.m_gameScene.m_stageID
+
+    local world = self.m_gameScene.m_gameWorld
+    if (not world) then return end
+
+    if (world.m_skillIndicatorMgr and world.m_skillIndicatorMgr:isControlling()) then
+        world.m_skillIndicatorMgr:clear()
+    end
+
+    self.m_gameScene:gamePause()
+
+    local function close_cb()
+        -- 자동모드 여부(연속전투 활성화시 같이 활성화 시킴)
+        local is_auto_mode = g_autoPlaySetting:get('auto_mode')
+
+        -- 설정된 정보로 UI 변경
+        self:setAutoPlayUI()
+        self:setAutoMode(is_auto_mode)
+
+        if (is_auto_mode) then
+			world:dispatch('auto_start')
+        else
+			world:dispatch('auto_end')
+        end
+
+        self.m_gameScene:gameResume()
+    end
+
+    local is_auto = g_autoPlaySetting:isAutoPlay()
+
+    -- 바로 해제
+    if (is_auto) then
+        g_autoPlaySetting:setAutoPlay(false)
+		world:dispatch('farming_changed')
+        close_cb()
+    else
+		local game_mode = GAME_MODE_EVENT_ILLUSION_DUNSEON
+        local ui = UI_AutoPlaySettingPopup(game_mode)
+        ui:setCloseCB(close_cb)
+    end
+end
