011a9a5236277dc248f55913a25202e3969d68f0
jhakim <jhakim@highbrow.com>
2019-08-22 08:38:32 +0000

    [정하]#컨텐츠 오픈 - 퀘스트 리스트 아이템 연결
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@108738 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 2199bca82bd27a7cdb13ca988e5f5d738a3d1797

--- dragonvillage/src/UI_QuestListItem_Contents.lua
+++ dragonvillage/src/UI_QuestListItem_Contents.lua
@@ -25,9 +25,60 @@ function UI_QuestListItem_Contents:init(data)
 	self:load('quest_item_contents_open.ui')
     self.m_data = data
 
+
+    if (data['reward'] == '') then
+        return
+    end
+
+    if (not data['reward']) then
+        return
+    end
+
     self:initUI()
     self:initButton()
     self:refresh()
+end
+
+-------------------------------------
+-- function initUI
+-------------------------------------
+function UI_QuestListItem_Contents:initUI()
+    local vars = self.vars
+    local data = self.m_data
+
+    -- 컨텐츠 이름
+    local content_name = data['t_name']
+    vars['contentsLabel']:setString(Str(content_name))
+
+    -- 컨텐츠 설명
+    local desc = data['t_desc_2']
+    vars['dscLabel']:setString(Str(desc))
+
+    -- 컨텐츠 열리는 조건
+    local req_stage_id = data['req_stage_id']
+    local difficulty, chapter, stage = parseAdventureID(req_stage_id)
+    local stage_name = chapter .. '-' .. stage
+    local condition_str = Str(data['t_desc'], stage_name, '') 
+    vars['conditionLabel']:setString(Str(condition_str))
+
+    -- 컨텐츠 이미지
+    local res = data['res']
+    local contents_icon = cc.Sprite:create(res)
+    if (contents_icon) then
+        vars['contentsNode']:addChild(contents_icon)
+    end
+
+    -- 퀘스트 보상
+    -- @jhakim 190822 컨텐츠 보상은 하나만 있는 상태
+    -- 2개이상 올 경우 대비해서 리스트 형식으로 보상을 출력
+    local l_reward_info = TableQuest.arrangeDataByStr(data['reward'])
+    local reward_idx = 1
+    for i, v in ipairs(l_reward_info) do
+        local reward_card = UI_ItemCard(v['item_id'], v['count'])
+        reward_card.root:setSwallowTouch(false)
+        vars['rewardNode']:addChild(reward_card.root)
+        reward_idx = reward_idx + 1
+    end
 end
 
 -------------------------------------
@@ -36,5 +87,32 @@ end
 function UI_QuestListItem_Contents:initButton()
     local vars = self.vars
     local data = self.m_data
+
+    local req_stage = data['req_stage_id']
+    local is_available = g_adventureData:isClearStage(req_stage)
+    local is_lock = g_contentLockData:isContentLock(data['content_name'])
+
+    -- 컨텐츠 조건 만족 상태, 컨텐츠 잠금 상태 조합해서 버튼 상태 설정
+    local reward_able = is_available and is_lock
+    local after_reward = is_available and not is_lock
+    local before_reward = not is_available
+
     vars['rewardBtn']:registerScriptTapHandler(function() g_contentLockData:request_contentsOpenReward(data['content_name']) end)
+    vars['questLinkBtn']:registerScriptTapHandler(function() g_contentLockData:request_contentsOpenReward(data['content_name']) end)
+    vars['lockBtn']:setEnabled(false)
+
+    vars['rewardBtn']:setVisible(false)
+    vars['questLinkBtn']:setVisible(false)
+    vars['lockBtn']:setVisible(false)
+
+    -- 보상 받기 가능, 바로가기 버튼, 잠금 버튼
+    if (reward_able) then
+        vars['rewardBtn']:setVisible(true)
+    elseif (after_reward) then
+        vars['questLinkBtn']:setVisible(true)
+    elseif (before_reward) then
+        vars['lockBtn']:setVisible(true)
+    else
+        vars['lockBtn']:setVisible(true)
+    end
 end
