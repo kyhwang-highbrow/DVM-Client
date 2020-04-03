caa70e13896ba63ecd1a683c438ab68f7296d9bf
sgkim <sgkim@highbrow.com>
2020-04-03 08:50:33 +0000

    [성구] #첫 충전 선물 구현 중
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@112175 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: d2fd09adb4401fd70bd43c93c910231d8d93e499

--- dragonvillage/src/UI_ButtonFirstPurchaseReward.lua
+++ dragonvillage/src/UI_ButtonFirstPurchaseReward.lua
@@ -7,6 +7,7 @@ local PARENT = UI_ManagedButton
 -------------------------------------
 UI_ButtonFirstPurchaseReward = class(PARENT, {
         m_eventId = 'string', -- '10102'
+        m_bFirstOpen = 'boolean', -- 최초 1번은 노티를 보여주기 위함
     })
 
 -------------------------------------
@@ -14,28 +15,44 @@ UI_ButtonFirstPurchaseReward = class(PARENT, {
 -------------------------------------
 function UI_ButtonFirstPurchaseReward:init(event_id)
     self.m_eventId = event_id
+    self.m_bFirstOpen = true
 
     -- 서버에서 받은 버튼 ui로 생성한다.
     local ui_res = self:getDataByKey('btn_ui') or 'button_first_purchase_reward.ui'
-
     self:load(ui_res)
 
+    -- 업데이트 스케줄러
     self.root:scheduleUpdateWithPriorityLua(function(dt) self:update(dt) end, 0)
+
+    -- 버튼 설정
+    local btn = self.vars['btn']
+    if btn then
+        btn:registerScriptTapHandler(function() self:click_btn() end)
+    end
 end
 
 -------------------------------------
 -- function isActive
 -------------------------------------
 function UI_ButtonFirstPurchaseReward:isActive()
-    return true
-end
+    local status = self:getDataByKey('status')
 
--------------------------------------
--- function updateButtonStatus
--- virtual 순수 가상 함수
--------------------------------------
-function UI_ButtonFirstPurchaseReward:updateButtonStatus()
-    self.root:setVisible(true)
+    -- 초기상태
+    if (status == -1) then
+        return true
+
+    -- 결제 완료 ( 보상 수령 가능 
+    elseif (status == 0) then
+        return true
+
+    -- 보상 수령 완료
+    elseif (status == 1) then
+        return false
+    
+    -- 그 외 모든 상황
+    else
+        return false
+    end
 end
 
 -------------------------------------
@@ -47,15 +64,15 @@ function UI_ButtonFirstPurchaseReward:getDataByKey(key)
     end
 
     local t_info = g_firstPurchaseEventData:getFirstPurchaseEventInfoByEventId(self.m_eventId)
-    --"10102":{
-    --  "is_reward":0,
-    --  "btn_ui":"button_first_purchase_reward.ui",
-    --  "end_date":1586790000000,
-    --  "popup_ui":"event_first_purchase_newbie.ui",
-    --  "t_name":"첫 충전 선물",
-    --  "start_date":1585839600000,
-    --  "reward":"770405;1,700001;2000"
-    --}
+    -- "10102":{
+    --   "popup_ui":"event_first_purchase_newbie.ui",
+    --   "t_name":"첫 충전 선물",
+    --   "end_date":1586790000000,
+    --   "status":-1,
+    --   "btn_ui":"button_first_purchase_reward.ui",
+    --   "start_date":1585839600000,
+    --   "reward":"770405;1,700001;2000"
+    -- }
     if (not t_info) then
         return nil
     end
@@ -65,11 +82,68 @@ end
 
 
 -------------------------------------
+-- function click_btn
+-- @brief 버튼 클릭
+-------------------------------------
+function UI_ButtonFirstPurchaseReward:click_btn()
+    self.m_bFirstOpen = false
+
+    -- 이하 개발을 위한 코드
+    --local t_info = g_firstPurchaseEventData:getFirstPurchaseEventInfoByEventId(self.m_eventId)
+    --t_info['status'] = 1
+    --t_info['end_date'] = (Timer:getServerTime() + 3) * 1000
+end
+
+-------------------------------------
 -- function update
 -- @brief 매 프레인 호출되는 함수
 -------------------------------------
 function UI_ButtonFirstPurchaseReward:update(dt)
-    -- 1. 빨간 느낌표 표시 상태 (실행 후 한번도 보지 않았을 경우 or 보상이 수령 가능한 상태일 경우)
+    local vars = self.vars
+    local status = self:getDataByKey('status') -- -1, 0, 1
+    local end_date = (self:getDataByKey('end_date') or 0) / 1000 -- timestamp 1585839600000
+    local curr_time = Timer:getServerTime()
 
-    -- 2. 삭제될 건지 확인 (시간이 지났거나, 보상을 수령했을 경우)
+    -- 1. 남은 시간 표시 (기간제일 경우에만)
+    local time_label = vars['timeLabel']
+    if time_label then
+        if (0 < end_date) and (curr_time < end_date) then
+            local time_millisec = (end_date - curr_time) * 1000
+            local str = datetime.makeTimeDesc_timer(time_millisec)
+            time_label:setString(str)
+        else
+            time_label:setString('')
+        end
+    end
+
+    -- 2. 빨간 느낌표 표시 상태 (실행 후 한번도 보지 않았을 경우 or 보상이 수령 가능한 상태일 경우)
+    local noti_sprite = vars['notiSprite']
+    if noti_sprite then
+        -- 앱 실행 후 최초 1회는 노출
+        if (self.m_bFirstOpen == true) then
+            noti_sprite:setVisible(true)
+        
+        -- 획득 가능한 보상이 있을 경우 노출
+        elseif (status == 0) then
+            noti_sprite:setVisible(true)
+
+        -- 그 외 경우 미노출
+        else
+            noti_sprite:setVisible(false)
+        end
+    end
+
+    do -- 3. 삭제될 건지 확인
+        -- 시간이 지난 경우 (제한 시간이 없을 경우 end_date가 0)
+        if (0 < end_date) and (end_date < curr_time) then
+            self.m_bMarkDelete = true
+            self:callDirtyStatusCB()
+        end
+
+        -- 보상을 수령한 상태일 경우
+        if (status == 1) then
+            self.m_bMarkDelete = true
+            self:callDirtyStatusCB()
+        end
+    end
 end
--- dragonvillage/src/UI_LobbyLeftTopBtnManager.lua
+++ dragonvillage/src/UI_LobbyLeftTopBtnManager.lua
@@ -10,6 +10,8 @@ UI_LobbyLeftTopBtnManager = class({
 
         m_lManagedButtonUI = 'list',
         m_interval = 'number',
+
+        PRIORITY = 'table',
     })
 
 -------------------------------------
@@ -21,13 +23,16 @@ function UI_LobbyLeftTopBtnManager:init(ui_lobby)
 
     self.m_lManagedButtonUI = {}
     self.m_interval = 10
+    self.PRIORITY = {}
+    self.PRIORITY.FIRST_PURCHASE_REWARD = 900
+    self.PRIORITY.SPECIAL_OFFER_PRODUCT = 800
+    self.PRIORITY.SPOT_SALE_PRODUCT = 700
 
     vars['productBtnMenu']:scheduleUpdateWithPriorityLua(function(dt) self:update(dt) end, 0)
 
     local l_managed_button_info = {}
-    --table.insert(l_managed_button_info, {900, UI_ButtonFirstPurchaseReward}) -- 첫 충전 선물 (첫 결제 보상)
-    table.insert(l_managed_button_info, {800, UI_ButtonSpecialOfferProduct}) -- 특별 할인 상품
-    table.insert(l_managed_button_info, {700, UI_ButtonSpotSale}) -- 깜짝 할인 상품
+    table.insert(l_managed_button_info, {self.PRIORITY.SPECIAL_OFFER_PRODUCT, UI_ButtonSpecialOfferProduct}) -- 특별 할인 상품
+    table.insert(l_managed_button_info, {self.PRIORITY.SPOT_SALE_PRODUCT, UI_ButtonSpotSale}) -- 깜짝 할인 상품
     
     for i,v in ipairs(l_managed_button_info) do
         local priority = v[1]
@@ -74,6 +79,9 @@ end
 
 -------------------------------------
 -- function getManagedButtonByUniqueKey
+-- @brief unique_key로 ManagedButton 리턴
+-- @param unique_key (string)
+-- @return UI_ManagedButton을 상속 받은 클래스 or nil
 -------------------------------------
 function UI_LobbyLeftTopBtnManager:getManagedButtonByUniqueKey(unique_key)
     for i,v in ipairs(self.m_lManagedButtonUI) do
@@ -90,31 +98,36 @@ end
 -------------------------------------
 function UI_LobbyLeftTopBtnManager:updateButtonsStatus()
 
-    do -- 추가 확인
+    do -- 추가될 버튼 확인
         for event_id,v in pairs(g_firstPurchaseEventData.m_tFirstPurchaseEventInfo) do
             local unique_key = ('first_purchase_reward_' .. event_id)
 
             -- 존재하지 않으면 생성 (삭제는 managed button에서 알아서 진행
             if (self:getManagedButtonByUniqueKey(unique_key) == nil) then
-                local class_ = UI_ButtonFirstPurchaseReward
-                local priority = 900
-                local managed_button = self:makeManagedButton(class_, event_id)
-                managed_button:setPriority(priority)
-                managed_button.m_uniqueKey = unique_key
+                -- status가 1이면 보상을 수령한 상태
+                if (v['status'] ~= 1) then
+                    local class_ = UI_ButtonFirstPurchaseReward
+                    local priority = self.PRIORITY.FIRST_PURCHASE_REWARD
+                    local managed_button = self:makeManagedButton(class_, event_id)
+                    managed_button:setPriority(priority)
+                    managed_button.m_uniqueKey = unique_key
+                end
             end
         end
     end
 
+    -- 버튼들의 상태 업데이트
     local l_remove_idx = {}
     for i,v in ipairs(self.m_lManagedButtonUI) do
         v:updateButtonStatus()
 
+        -- 삭제 표시가 된 버튼 수집
         if (v.m_bMarkDelete == true) then
             table.insert(l_remove_idx, 1, i)
         end
     end
 
-    -- 중복된 did들 순차적으로 제거
+    -- 삭제
     for i,v in ipairs(l_remove_idx) do
         local ui = self.m_lManagedButtonUI[i]
         if ui.root then
@@ -139,10 +152,23 @@ function UI_LobbyLeftTopBtnManager:updateButtonsPosition()
 
     -- 2. 위치 지정
     local pos_x = 0
+    local action_tag = 1917 -- 해당 액션만 재생하고 정지하기 위해 고유한 아무 값이나 사용
+    local duration = 0.2
     for i,v in ipairs(self.m_lManagedButtonUI) do
         if (v:isActive() == true) then
             local width = v:getWidth()
-            v.root:setPositionX(pos_x + (width / 2))
+            
+            -- 액션으로 이동
+            local x = pos_x + (width / 2)
+            local y = v.root:getPositionY()
+            local action = cca.makeBasicEaseMove(duration, x, y)
+            cca.stopAction(v.root, action_tag)
+            cca.runAction(v.root, action, action_tag)
+
+            -- 즉시 이동
+            --v.root:setPositionX(pos_x + (width / 2))
+
+            -- 다음 위치 계산
             pos_x = (pos_x + width + self.m_interval)
         end
     end
