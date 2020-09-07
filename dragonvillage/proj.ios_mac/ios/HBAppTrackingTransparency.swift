2a871a1395b96f9a92625d31e19efcf875765f59
mskim <mskim@highbrow.com>
2020-09-07 12:18:46 +0000

    [문성] ## 1. iOS 1.2.5 - 3 - libSwiftCoreMidi 라이브러리 참조 정리 - svn ignore 추가 : xcuserdata xcshareddate - HBAppTrackingTransparency iOS 13에서 에러 안나도록 주석 처리
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@113759 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: e7a6210280488ea54710180c01f795c1903ebd78

--- dragonvillage/proj.ios_mac/ios/HBAppTrackingTransparency.swift
+++ dragonvillage/proj.ios_mac/ios/HBAppTrackingTransparency.swift
@@ -6,14 +6,14 @@
 //
 
 import Foundation
-import AppTrackingTransparency
+//import AppTrackingTransparency
 
 @objc class HBAppTrackingTransparency : NSObject
 {
     @objc static func isAuthorized() -> Bool
     {
         if #available(iOS 14, *) {
-            return ATTrackingManager.trackingAuthorizationStatus == ATTrackingManager.AuthorizationStatus.authorized
+            return true;//ATTrackingManager.trackingAuthorizationStatus == ATTrackingManager.AuthorizationStatus.authorized
         } else {
             // 이전 버전에서는 인증 된 것과 같다.
             return true
@@ -23,7 +23,7 @@ import AppTrackingTransparency
     @objc static func isNotDetermined() -> Bool
     {
         if #available(iOS 14, *) {
-            return ATTrackingManager.trackingAuthorizationStatus == ATTrackingManager.AuthorizationStatus.notDetermined
+            return true;//ATTrackingManager.trackingAuthorizationStatus == ATTrackingManager.AuthorizationStatus.notDetermined
         } else {
             return true
         }
@@ -34,10 +34,10 @@ import AppTrackingTransparency
     {
         // @mskim
         // Objective-C <--> Swift 간 enum 전달이 안되기 때문에 Int값을 받아온 후 익명 클로저를 사용하여 enum으로 변환하여 사용하도록 함
-        ATTrackingManager.requestTrackingAuthorization(
-            completionHandler: {
-                (status:ATTrackingManager.AuthorizationStatus) -> () in
-                handler(status.rawValue)
-            })
+//        ATTrackingManager.requestTrackingAuthorization(
+//            completionHandler: {
+//                (status:ATTrackingManager.AuthorizationStatus) -> () in
+//                handler(status.rawValue)
+//            })
     }
 }
