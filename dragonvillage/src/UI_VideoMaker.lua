85bf69530c12be8a608ac85c295dbeabf2e277ca
mskim <mskim@highbrow.com>
2020-09-08 13:29:58 +0000

    [문성] ## 1. video maker
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@113762 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 37e279c421922e6c351b699ec537736f0c59d922

---
+++ dragonvillage/src/UI_VideoMaker.lua
@@ -0,0 +1,300 @@
+local PARENT = UI
+local THIS
+
+
+
+
+local DRAGON_ID = 120801
+local BG_NAME = 'map_forest'
+local BG_SCALE = 0.5
+
+
+local DRAGON_POS_Y_BY_EVOL = { 0, -30, -50 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+-------------------------------------
+-- LOCAL function onKeyReleaseListener
+-------------------------------------
+local function onKeyReleaseListener(key_code, event)
+    -- 세팅 팝업
+    if (key_code == KEY_Z) then
+        THIS:showSettingPanel()
+    -- 재생
+    elseif (key_code == KEY_A) then
+        THIS:play()
+    -- 중지 (초기화 함)
+    elseif (key_code == KEY_S) then
+        THIS:stop()
+    end
+end
+
+
+-------------------------------------
+-- class UI_VideoMaker
+-------------------------------------
+UI_VideoMaker = class(PARENT,{
+        m_dragonAnimator = '',
+        m_mapManager = 'map ani',
+        m_bgName = 'string',
+
+        m_coroutineHelper = 'CoroutineHelper',
+    })
+
+-------------------------------------
+-- function init
+-------------------------------------
+function UI_VideoMaker:init()
+    self.m_uiName = 'UI_VideoMaker'
+    local vars = self:load('video_maker.ui')
+    UIManager:open(self, UIManager.SCENE)
+
+    -- backkey 지정
+    g_currScene:pushBackKeyListener(self, 
+        function() 
+            self:close()
+            UIManager:removeKeyListener()
+        end, 'UI_VideoMaker')
+
+    -- @UI_ACTION
+    --self:addAction(vars['rootNode'], UI_ACTION_TYPE_LEFT, 0, 0.2)
+
+    -- ReleaseKeyListener 등록
+    UIManager:registerKeyListener(onKeyReleaseListener)
+
+
+--    self:doActionReset()
+--    self:doAction(nil, false)
+
+    self.m_bgName = BG_NAME
+
+    self:initUI()
+    self:initButton()
+    self:refresh()
+
+    THIS = self
+end
+
+-------------------------------------
+-- function initUI
+-------------------------------------
+function UI_VideoMaker:initUI()
+    local vars = self.vars
+
+    -- 설정 패널 숨김
+    vars['settingPanel']:setVisible(false)
+
+    -- 드래곤 초기화
+    self.m_dragonAnimator = UIC_DragonAnimator()
+    self.m_dragonAnimator:setTalkEnable(false)
+    self.m_dragonAnimator:setChangeAniEnable(false)
+    vars['dragonNode']:addChild(self.m_dragonAnimator.m_node)
+
+    -- 배경 노드에 업데이트 붙임
+    vars['bgNode']:scheduleUpdateWithPriorityLua(function(dt) self:update(dt) end, 0)
+    vars['bgNode']:setScale(BG_SCALE)
+end
+
+-------------------------------------
+-- function initButton
+-------------------------------------
+function UI_VideoMaker:initButton()
+    local vars = self.vars
+
+    vars['dragonBtn']:registerScriptTapHandler(function() self:click_dragonBtn() end)
+    vars['bgBtn']:registerScriptTapHandler(function() self:click_bgBtn() end)
+end
+
+-------------------------------------
+-- function refresh
+-- 초기화 기능으로 사용함
+-------------------------------------
+function UI_VideoMaker:refresh()
+    self:setDragon(1)
+    self:changeBG()
+end
+
+-------------------------------------
+-- function update
+-------------------------------------
+function UI_VideoMaker:update(dt)
+	if (self.m_mapManager) then
+		self.m_mapManager:update(dt)
+	end
+end
+
+
+-------------------------------------
+-------------------------------------
+-- click listener method
+-------------------------------------
+-------------------------------------
+
+-------------------------------------
+-- function click_dragonBtn
+-------------------------------------
+function UI_VideoMaker:click_dragonBtn()
+end
+
+-------------------------------------
+-- function click_bgBtn
+-------------------------------------
+function UI_VideoMaker:click_bgBtn()
+end
+
+
+
+
+
+
+
+-------------------------------------
+-------------------------------------
+-- functional method
+-------------------------------------
+-------------------------------------
+
+-------------------------------------
+-- function setDragon
+-------------------------------------
+function UI_VideoMaker:setDragon(evolution)
+	self.m_dragonAnimator:setDragonAnimator(DRAGON_ID, evolution)
+    self.m_dragonAnimator.m_animator:changeAni('idle', true)
+
+    self.vars['dragonNode']:setPositionY(DRAGON_POS_Y_BY_EVOL[evolution])
+
+    ccdump(self.m_dragonAnimator.m_animator:getVisualList())
+end
+
+-------------------------------------
+-- function changeBG
+-------------------------------------
+function UI_VideoMaker:changeBG()
+	if self.m_mapManager then 
+		self.m_mapManager = nil
+	end
+	
+	self.m_mapManager = ScrollMap(self.vars['bgNode'])
+	self.m_mapManager:setBg(self.m_bgName)
+	self.m_mapManager:setSpeed(-100)
+end
+
+-------------------------------------
+-- function showSettingPanel
+-------------------------------------
+function UI_VideoMaker:showSettingPanel()
+    self.vars['settingPanel']:setVisible(not self.vars['settingPanel']:isVisible())
+end
+
+-------------------------------------
+-------------------------------------
+-- main method
+-------------------------------------
+-------------------------------------
+
+-------------------------------------
+-- function play
+-------------------------------------
+function UI_VideoMaker:play()
+    local evolve_visual = self.vars['evolveVisual']
+    evolve_visual:setTimeScale(1)
+
+    local function coroutine_function(dt)
+        local co = CoroutineHelper()
+        self.m_coroutineHelper = co
+
+		-- 코루틴 종료 콜백
+		local function close_cb()
+			self.m_coroutineHelper = nil
+			self:refresh()
+		end
+		co:setCloseCB(close_cb)
+
+        -- 해치 -> 해츨링 진화
+        cclog('-------------------------------- EVOLVE 1')
+        co:work()
+        
+        -- 이펙트
+        evolve_visual:setVisible(true)
+        evolve_visual:changeAni('top_appear', false, false)
+        evolve_visual:addAniHandler(function()
+            evolve_visual:setVisible(false)
+            self:setDragon(2)
+            co.NEXT()
+        end)
+
+        -- 꿀렁
+        local delay = cc.DelayTime:create(0.25)
+        local scaleUp = cc.EaseInOut:create(cc.ScaleTo:create(1.2, 0.3), 2)
+        local scaleDown = cc.EaseInOut:create(cc.ScaleTo:create(0, 0.3), 2)
+        local action = cc.Sequence:create(delay, scaleUp, scaleDown)
+        self.vars['dragonNode']:runAction(action)
+
+        if co:waitWork() then return end
+
+        -- 1초 대기
+        cclog('-------------------------------- WAIT : 1')
+        co:waitTime(1)
+
+        -- 해츨링 -> 성룡 진화
+        cclog('-------------------------------- EVOLVE 2')
+        co:work()
+
+        evolve_visual:setVisible(true)
+        evolve_visual:changeAni('top_appear', false, false)
+        evolve_visual:addAniHandler(function()
+            evolve_visual:setVisible(false)
+            self:setDragon(3)
+            co.NEXT()
+        end)
+        if co:waitWork() then return end
+        
+        -- 0.5초 대기
+        co:waitTime(0.5)
+
+        -- 드래곤 pose (화려한 동작) 연출
+        cclog('-------------------------------- POSE')
+        co:work()
+        self.m_dragonAnimator.m_animator:changeAni('pose_1', false)
+        self.m_dragonAnimator.m_animator:addAniHandler(function()
+            self.m_dragonAnimator.m_animator:changeAni('idle', true)
+            co.NEXT()
+        end)
+        if co:waitWork() then return end
+
+        -- 1초 대기
+        cclog('-------------------------------- WAIT 1')
+        co:waitTime(1)
+
+        cclog('-------------------------------- FINISH')
+
+        -- 끝
+        co:close()
+    end
+
+    Coroutine(coroutine_function, 'Play')
+end
+
+-------------------------------------
+-- function stop
+-------------------------------------
+function UI_VideoMaker:stop()
+    if (self.m_coroutineHelper == nil) then
+        return
+    end
+
+    self.m_coroutineHelper:close()
+end
+
+--@CHECK
+UI:checkCompileError(UI_VideoMaker)
