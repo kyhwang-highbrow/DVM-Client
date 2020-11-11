4cf03629842c90447a9b99fd90b9ceab6a6f1de4
kwkang <kwkang@highbrow.com>
2020-11-11 07:19:33 +0000

    [경웅] #드래곤 레벨업 시스템 개편 - 공지 팝업에서 보상 아이템 리스트로 날아올 경우 처리 가능하도록 변경
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@114571 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 8e3549c1b80d6f5a9b330b295201fb9be1cbf08c

--- dragonvillage/src/UI_LobbyNoticePopup.lua
+++ dragonvillage/src/UI_LobbyNoticePopup.lua
@@ -56,25 +56,56 @@ function UI_LobbyNoticePopup:initUI()
 
     local struct_lobby_notice = self.m_structLobbyNotice
     local l_item_list = struct_lobby_notice:getRewardList()
+    
+    if (#l_item_list == 1) then
+        local item = l_item_list[1]
+        local item_id = item['item_id']
+        local count = item['count']
 
-    -- 보상 리스트 아이콘 생성
-    for i,v in ipairs(l_item_list) do
-        local lua_name = string.format('itemNode%.2d', i)
-
-        -- rewardNode01과 같이 숫자가 붙어있는 형태가 아닌 ui 예외처리
-        if (i == 1) and (not vars[lua_name]) then
-            lua_name = 'itemNode'
-        end
-
-        if vars[lua_name] then
-            local item_id = v['item_id']
-            local count = v['count']
-
-            local ui = UI_ItemCard(item_id, count)
-            ui.root:setSwallowTouch(false)
-            vars[lua_name]:addChild(ui.root)
-        end
+        ui = UI_ItemCard(item_id, count)
+        ui.root:setSwallowTouch(false)
+        vars['itemNode']:addChild(ui.root)
+    
+    else
+        vars['itemNode']:setVisible(false)
+        vars['itemListNode']:setVisible(true)
+        self:initTableView()
     end
+end
+
+-------------------------------------
+-- function initTableView
+-------------------------------------
+function UI_LobbyNoticePopup:initTableView()
+    local vars = self.vars
+
+    local struct_lobby_notice = self.m_structLobbyNotice
+    local l_item_list = struct_lobby_notice:getRewardList()
+
+    local node = vars['itemListNode']
+
+    if (node == nil) then
+        return
+    end
+
+	-- 리스트 아이템 생성 콜백
+    local function make_func(object)
+        local ui = UI_ItemCard(object['item_id'], object['count'])
+        return ui
+    end
+
+    local function create_func(ui, data)
+        ui.root:setScale(0.95)
+    end
+
+    -- 테이블 뷰 인스턴스 생성
+    local table_view = UIC_TableView(node)
+    table_view.m_defaultCellSize = cc.size(150, 150)
+    table_view:setCellUIClass(make_func, create_func)
+    table_view:setDirection(cc.SCROLLVIEW_DIRECTION_HORIZONTAL)
+	table_view.m_bAlignCenterInInsufficient = true
+
+    table_view:setItemList(l_item_list)
 end
 
 -------------------------------------
