163a5f6ec69814231127c89cd1e9e11bc298f9e3
kwkang <kwkang@highbrow.com>
2020-12-04 08:46:21 +0000

    [경웅] # 미니 우편함 팝업에 룬 원석만 볼 수 있도록 추가
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@114944 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 24be6795bc7c9a2fc06f06a0a4c3c4b1265b83c4

--- dragonvillage/src/UI_MailSelectPopup.lua
+++ dragonvillage/src/UI_MailSelectPopup.lua
@@ -24,6 +24,7 @@ MAIL_SELECT_TYPE = {
     UPDATE_PACK = 8,    -- 승급 패키지 구매 시
     ITEM_GOOD = 9,      -- 패키지 상품 구성이 아이템+재화 일 때
     GOODS_WITH_CLOSE_CB = 10,
+    RUNE_STONE = 11,    -- 룬 원석
 }
 
 -------------------------------------
@@ -84,6 +85,11 @@ function UI_MailSelectPopup:initUI()
     elseif (type == MAIL_SELECT_TYPE.CAPSULE_COIN) then
         title = Str('우편함')
         local item_id = TableItem():getItemIDFromItemType('capsule_coin')
+        self:setItemByID(item_id)
+
+    elseif (type == MAIL_SELECT_TYPE.RUNE_STONE) then
+        title = Str('우편함')
+        local item_id = TableItem():getItemIDFromItemType('rune_stone')
         self:setItemByID(item_id)
 
 	elseif (type == MAIL_SELECT_TYPE.ITEM) then
