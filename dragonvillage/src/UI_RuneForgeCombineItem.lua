57ebec724c103428677b0a1b7fb98ff8757b7edb
kwkang <kwkang@highbrow.com>
2020-12-10 07:26:02 +0000

    [경웅] #룬 합성 결과물 아이콘에 7등급 이상일 때 나오던 이펙트 제거
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@115035 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 448925f6ea2ba0778282eaa6d7c2e841e06d0e5e

--- dragonvillage/src/UI_RuneForgeCombineItem.lua
+++ dragonvillage/src/UI_RuneForgeCombineItem.lua
@@ -9,7 +9,6 @@ UI_RuneForgeCombineItem = class(PARENT,{
         m_runeCombineData = 'StructRuneCombine',
         m_mRuneCardUI = 'map', -- 현재 생성되어있는 룬 카드 UI, map[index] = UI_RuneCard
         m_resultCard = 'UI_ItemCard',
-        m_resultEffect = 'visual',
     })
 
 -- 가능한 등급이 1~2라면 key : 12
@@ -110,17 +109,11 @@ function UI_RuneForgeCombineItem:refresh()
         if (self.m_resultCard ~= nil) then
             self.m_resultCard.vars['disableSprite']:setVisible(false)
         end
-        if (self.m_resultEffect ~= nil) then
-            self.m_resultEffect:setVisible(true)
-        end
     else
         vars['allSelectMenu']:setVisible(false)
         
         if (self.m_resultCard ~= nil) then
             self.m_resultCard.vars['disableSprite']:setVisible(true)
-        end
-        if (self.m_resultEffect ~= nil) then
-            self.m_resultEffect:setVisible(false)
         end
     end
 end
@@ -181,17 +174,6 @@ function UI_RuneForgeCombineItem:makeResultRuneCard()
     
     -- 얻을 수 있는 등급 중 좋은 등급의 룬 아이콘
     result_card_ui:makeSprite('runeNode', result_res, true) -- (lua_name, res, no_use_frames)
-    
-    -- 높은 등급을 획득할 수 있는 경우 효과 나타내기
-	if (success_grade >= 7) then
-		local rarity_effect = MakeAnimator('res/ui/a2d/card_summon/card_summon.vrp')
-		rarity_effect:changeAni('summon_hero', true)
-		rarity_effect:setScale(1.7)
-		result_card_ui.root:addChild(rarity_effect.m_node)
-        self.m_resultEffect = rarity_effect
-    else
-        self.m_resultEffect = nil
-	end
     
     -- 일단 검은 레이어 씌우고 모든 합성 재료 칸이 등록되면 벗겨주기
     local disable_res = 'card_cha_frame_disable.png'
