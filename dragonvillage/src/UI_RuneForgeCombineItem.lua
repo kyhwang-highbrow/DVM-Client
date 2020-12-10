d2c7770b85636358dd54f753fd6c059a7b59b4d0
kwkang <kwkang@highbrow.com>
2020-12-10 03:10:07 +0000

    [경웅] #룬 합성 UI/UX 변경 - 결과 룬 설명 툴팁 문구 변경
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@115023 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 32640a5a1eb2cb3f9136a4c36f0aec38d33b8e7b

--- dragonvillage/src/UI_RuneForgeCombineItem.lua
+++ dragonvillage/src/UI_RuneForgeCombineItem.lua
@@ -14,13 +14,13 @@ UI_RuneForgeCombineItem = class(PARENT,{
 
 -- 가능한 등급이 1~2라면 key : 12
 UI_RuneForgeCombineItem.result_info_text = {
-    ['12'] = Str('{@SKILL_NAME}1등급 룬 합성\n{@DEFAULT}1~2등급 룬을 얻을 수 있습니다.'),
-    ['23'] = Str('{@SKILL_NAME}2등급 룬 합성\n{@DEFAULT}2~3등급 룬을 얻을 수 있습니다.'),
-    ['34'] = Str('{@SKILL_NAME}3등급 룬 합성\n{@DEFAULT}3~4등급 룬을 얻을 수 있습니다.'),
-    ['45'] = Str('{@SKILL_NAME}4등급 룬 합성\n{@DEFAULT}4~5등급 룬을 얻을 수 있습니다.'),
-    ['56'] = Str('{@SKILL_NAME}5등급 룬 합성\n{@DEFAULT}5~6등급 룬을 얻을 수 있습니다.'),
-    ['67'] = Str('{@SKILL_NAME}6등급 룬 합성\n{@DEFAULT}6~7등급 룬을 얻을 수 있습니다.'),
-    ['77'] = Str('{@SKILL_NAME}7등급 룬 합성\n{@DEFAULT}7등급 룬을 얻을 수 있습니다.'),
+    ['12'] = Str('{@SKILL_NAME}1등급 룬 합성\n{@DEFAULT}랜덤한 1~2등급 룬을 얻을 수 있습니다.'),
+    ['23'] = Str('{@SKILL_NAME}2등급 룬 합성\n{@DEFAULT}랜덤한 2~3등급 룬을 얻을 수 있습니다.'),
+    ['34'] = Str('{@SKILL_NAME}3등급 룬 합성\n{@DEFAULT}랜덤한 3~4등급 룬을 얻을 수 있습니다.'),
+    ['45'] = Str('{@SKILL_NAME}4등급 룬 합성\n{@DEFAULT}랜덤한 4~5등급 룬을 얻을 수 있습니다.'),
+    ['56'] = Str('{@SKILL_NAME}5등급 룬 합성\n{@DEFAULT}랜덤한 5~6등급 룬을 얻을 수 있습니다.'),
+    ['67'] = Str('{@SKILL_NAME}6등급 룬 합성\n{@DEFAULT}랜덤한 6~7등급 룬을 얻을 수 있습니다.'),
+    ['77'] = Str('{@SKILL_NAME}7등급 룬 합성\n{@DEFAULT}랜덤한 7등급 룬을 얻을 수 있습니다.'),
 }
 
 -------------------------------------
