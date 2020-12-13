53fafde9b3f3be83bdd38a7ec7277aed3281723a
kwkang <kwkang@highbrow.com>
2020-12-13 11:32:07 +0000

    [경웅] #룬 연마 탭 기본 로직 구현
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@115063 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 75eccfa6ff910e48ec745f7a160500cda091e346

--- dragonvillage/src/UI_RuneForgeGrindTab.lua
+++ dragonvillage/src/UI_RuneForgeGrindTab.lua
@@ -22,7 +22,10 @@ function UI_RuneForgeGrindTab:onEnterTab(first)
 
     if (first == true) then
         self:initUI()
+        self:initButton()
     end
+
+    self:refresh()
 end
 
 -------------------------------------
@@ -38,4 +41,42 @@ end
 function UI_RuneForgeGrindTab:initUI()
     local vars = self.vars
 
+end
+
+-------------------------------------
+-- function initButton
+-------------------------------------
+function UI_RuneForgeGrindTab:initButton()
+    local vars = self.vars
+
+    vars['buyBtn']:registerScriptTapHandler(function() self:click_buyBtn() end)
+end
+
+-------------------------------------
+-- function refresh
+-------------------------------------
+function UI_RuneForgeGrindTab:refresh()
+    local vars = self.vars
+
+    -- 연마석 아이템 보유 수량
+    do
+        local cur_grind_stone_cnt = g_userData:get('grindstone')
+        vars['itemLabel']:setString(comma_value(cur_grind_stone_cnt))
+    end
+end
+
+-------------------------------------
+-- function click_buyBtn
+-- @brief 연마석 패키지 팝업을 띄움
+-------------------------------------
+function UI_RuneForgeGrindTab:click_buyBtn()
+    local vars = self.vars
+    
+    -- 연마석 패키지 팝업 띄우기
+    local pid = 110133
+    local is_popup = true
+    local package_name = TablePackageBundle:getPackageNameWithPid(pid)
+    local ui = PackageManager:getTargetUI(package_name, is_popup)
+
+    ui:setCloseCB(function() self:refresh() end) 
 end
