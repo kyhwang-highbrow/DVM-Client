ef2afe102c929af1bda19ad5416447086bc71d9c
kwkang <kwkang@highbrow.com>
2020-12-13 11:16:38 +0000

    [경웅] #룬 강화 탭 기본 로직 구현
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@115062 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 11400ae39bcec400d5f8d9656f4d538e4c0f5011

--- dragonvillage/src/UI_RuneForgeReinforceTab.lua
+++ dragonvillage/src/UI_RuneForgeReinforceTab.lua
@@ -11,7 +11,6 @@ UI_RuneForgeReinforceTab = class(PARENT,{
 -------------------------------------
 function UI_RuneForgeReinforceTab:init(owner_ui)
     local vars = self:load('rune_forge_reinforce.ui')
-    
 end
 
 -------------------------------------
@@ -22,7 +21,10 @@ function UI_RuneForgeReinforceTab:onEnterTab(first)
 
     if (first == true) then
         self:initUI()
+        self:initButton()
     end
+
+    self:refresh()
 end
 
 -------------------------------------
@@ -38,4 +40,51 @@ end
 function UI_RuneForgeReinforceTab:initUI()
     local vars = self.vars
 
+end
+
+-------------------------------------
+-- function initButton
+-------------------------------------
+function UI_RuneForgeReinforceTab:initButton()
+    local vars = self.vars
+
+    vars['buyBtn']:registerScriptTapHandler(function() self:click_buyBtn() end)
+
+end
+
+-------------------------------------
+-- function refresh
+-------------------------------------
+function UI_RuneForgeReinforceTab:refresh()
+    local vars = self.vars
+
+    -- 룬 춘복서 아이템 보유 수량
+    do
+        local cur_rune_bless_cnt = g_userData:get('rune_bless')
+        vars['itemLabel']:setString(comma_value(cur_rune_bless_cnt))
+    end
+
+    -- 룬 축복서 아이템 구매 제한 횟수
+    do
+        -- 룬 축복서 product ID : 220017
+        local product_struct = g_shopDataNew:getProduct('amethyst', 220017)
+        -- 구매제한 설명 문구 
+        local buy_count_desc = product_struct:getBuyCountDesc()
+        vars['buyLabel']:setString(buy_count_desc)
+    end
+end
+
+-------------------------------------
+-- function click_buyBtn
+-- @brief 룬 축복서 구매 팝업을 띄움
+-------------------------------------
+function UI_RuneForgeReinforceTab:click_buyBtn()
+    local vars = self.vars
+    
+    -- 룬 축복서 product ID : 220017
+    local product_struct = g_shopDataNew:getProduct('amethyst', 220017)
+    product_struct:buy(function(ret)
+        ItemObtainResult_Shop(ret) 
+        self:refresh()
+    end)
 end
