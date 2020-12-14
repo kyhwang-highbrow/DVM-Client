8fcf625563301e696be252bf331348e89869b810
kwkang <kwkang@highbrow.com>
2020-12-14 05:47:35 +0000

    [경웅] #'죄악의 화신 토벌작전' 풀팝업에서 시나리오 재생될 수 있도록 구현
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@115086 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 103e245a79e62124954402354d5462aa444ab29f

--- dragonvillage/src/UI_EventIncarnationOfSinsFullPopup.lua
+++ dragonvillage/src/UI_EventIncarnationOfSinsFullPopup.lua
@@ -6,6 +6,9 @@ local PARENT = UI
 UI_EventIncarnationOfSinsFullPopup = class(PARENT,{
     })
 
+
+UI_EventIncarnationOfSinsFullPopup.SCENARIO_NAME = 'scen_event_incarnation_of_sins'
+
 -------------------------------------
 -- function init
 -------------------------------------
@@ -15,6 +18,12 @@ function UI_EventIncarnationOfSinsFullPopup:init()
     self:initUI()
     self:initButton()
     self:refresh()
+
+    -- 이벤트 시나리오를 아직 보지 않은 경우 자동 재생
+    local b_is_view_scen = g_scenarioViewingHistory:isViewed(UI_EventIncarnationOfSinsFullPopup.SCENARIO_NAME)
+    if (not b_is_view_scen) then
+        self:playEventScenario()
+    end
 end
 
 -------------------------------------
@@ -29,6 +38,8 @@ end
 -------------------------------------
 function UI_EventIncarnationOfSinsFullPopup:initButton()
     local vars = self.vars
+
+    vars['scenarioBtn']:registerScriptTapHandler(function() self:playEventScenario() end)
 end
 
 -------------------------------------
@@ -36,4 +47,32 @@ end
 -------------------------------------
 function UI_EventIncarnationOfSinsFullPopup:refresh()
     local vars = self.vars
+
+    -- 이벤트 시나리오를 이미 본 경우 시나리오 버튼을 눌러 볼 수 있음
+    local b_is_view_scen = g_scenarioViewingHistory:isViewed(UI_EventIncarnationOfSinsFullPopup.SCENARIO_NAME)
+    if (b_is_view_scen) then
+        vars['scenarioBtn']:setVisible(true)
+    else
+        vars['scenarioBtn']:setVisible(false)
+    end
+end
+
+-------------------------------------
+-- function playEventScenario
+-- @brief 이벤트 시나리오를 재생시킨다
+-------------------------------------
+function UI_EventIncarnationOfSinsFullPopup:playEventScenario()
+    local vars = self.vars
+
+    local function finish_cb()
+        self:refresh()
+    end
+
+    local ui = UI_ScenarioPlayer(UI_EventIncarnationOfSinsFullPopup.SCENARIO_NAME)
+    if ui then
+        g_scenarioViewingHistory:addViewed(UI_EventIncarnationOfSinsFullPopup.SCENARIO_NAME)
+        ui:setCloseCB(finish_cb)
+        ui:next()
+        return 
+    end
 end
