0a4bb982f272f1c138c44268c245b66b614466ba
kwkang <kwkang@highbrow.com>
2020-12-15 06:25:21 +0000

    [경웅] #'죄악의 화신 토벌작전' 랭킹 요청 API 함수 구현
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@115133 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 2d111958292e774980ba8d6b7895b95327f0d6d6

--- dragonvillage/src/ServerData_EventIncarnationOfSins.lua
+++ dragonvillage/src/ServerData_EventIncarnationOfSins.lua
@@ -114,17 +114,17 @@ function ServerData_EventIncarnationOfSins:request_eventIncarnationOfSinsInfo(in
     end
 
     -- 네트워크 통신
-     local ui_network = UI_Network()
-     ui_network:setUrl('/shop/incarnation_of_sins/info')
-     ui_network:setParam('uid', uid)
-     ui_network:setParam('include_tables', include_tables) -- 정보 관련 테이블 내려받을지 여부 
-     ui_network:setParam('reward', include_reward) -- 랭킹 보상 지급 여부
-     ui_network:setSuccessCB(success_cb)
-	 ui_network:setFailCB(fail_cb)
-     ui_network:setRevocable(true)
-     ui_network:setReuse(false)
-	 ui_network:hideBGLayerColor()
-     ui_network:request()
+    local ui_network = UI_Network()
+    ui_network:setUrl('/shop/incarnation_of_sins/info')
+    ui_network:setParam('uid', uid)
+    ui_network:setParam('include_tables', include_tables) -- 정보 관련 테이블 내려받을지 여부 
+    ui_network:setParam('reward', include_reward) -- 랭킹 보상 지급 여부
+    ui_network:setSuccessCB(success_cb)
+	ui_network:setFailCB(fail_cb)
+    ui_network:setRevocable(true)
+    ui_network:setReuse(false)
+	ui_network:hideBGLayerColor()
+    ui_network:request()
 end
 
 -------------------------------------
@@ -138,4 +138,53 @@ function ServerData_EventIncarnationOfSins:response_eventIncarnationOfSinsInfo(r
     if (ret['reward']) then
         self.m_rewardStatus = ret['reward']
     end 
+end
+
+-------------------------------------
+-- function request_EventIncarnationOfSinsAttrRanking
+-- @brief 랭킹 정보를 요청하고, cb_func를 통해 랭킹 정보를 다룸
+-- @param attr_type : 속성 (earth, water, fire, light, dark, all(다섯가지 속성 전부 조회), total(합산 점수))
+-- @param search_type : 랭킹을 조회할 그룹 (world, clan, friend)
+-- @param offset : 랭킹 리스트의 offset 값 (-1 : 내 랭킹 기준, 0 : 상위 랭킹 기준, 20 : 랭킹의 20번째부터 조회..) 
+-- @param param_success_cb : 받은 데이터를 이용하여 처리할 콜백 함수
+-- @param param_fail_cb : 통신 실패 처리할 콜백 함수
+-------------------------------------
+function ServerData_EventIncarnationOfSins:request_EventIncarnationOfSinsAttrRanking(attr_type, search_type, offset, limit, param_success_cb, param_fail_cb)
+    local uid = g_userData:get('uid')
+    local attr = attr_type -- default : total
+    local type = search_type -- default : world
+    local offset = offset -- default : 0
+    local limit = limit -- default : 20
+
+    -- 콜백
+    local function success_cb(ret)
+        g_serverData:networkCommonRespone(ret)
+
+        self:response_eventIncarnationOfSinsInfo(ret)
+
+        if param_success_cb then
+            param_success_cb(ret)
+        end
+    end
+
+    local function fail_cb(ret)
+        if param_fail_cb then
+            param_fail_cb(ret)
+        end
+    end
+
+    -- 네트워크 통신
+    local ui_network = UI_Network()
+    ui_network:setUrl('/shop/incarnation_of_sins/ranking')
+    ui_network:setParam('uid', uid)
+    ui_network:setParam('attr', attr) 
+    ui_network:setParam('type', type)
+    ui_network:setParam('offset', offset)
+    ui_network:setParam('limit', limit)
+    ui_network:setSuccessCB(success_cb)
+	ui_network:setFailCB(fail_cb)
+    ui_network:setRevocable(true)
+    ui_network:setReuse(false)
+	ui_network:hideBGLayerColor()
+    ui_network:request()
 end
