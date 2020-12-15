0179e5e50b59f50c59fe0341a7a7e6891d31ca7e
ochoi <ochoi@highbrow.com>
2020-12-15 09:48:36 +0000

    [최외] #'죄악의 화신 토벌작전' 이벤트 종합랭킹 중간작업 (랭킹사이드 완료
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@115153 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 7aaef0d5ea5ba1036231386b862b08fac3bfcba4

--- dragonvillage/src/UI_EventIncarnationOfSinsRankingTotalTab.lua
+++ dragonvillage/src/UI_EventIncarnationOfSinsRankingTotalTab.lua
@@ -1,4 +1,5 @@
 local PARENT = class(UI_IndivisualTab, ITabUI:getCloneTable())
+local SCORE_OFFSET_GAP = 20
 
 -------------------------------------
 -- class UI_EventIncarnationOfSinsRankingTotalTab
@@ -6,6 +7,13 @@ local PARENT = class(UI_IndivisualTab, ITabUI:getCloneTable())
 UI_EventIncarnationOfSinsRankingTotalTab = class(PARENT,{
     m_rewardTableView = 'UIC_TableView',
     m_structRankReward = 'StructRankReward',
+
+    m_ownerUI = 'UI_EventIncarnationOfSinsRankingPopup', -- 현재 검색 타입에 대해 받아올 때 필요
+    m_searchType = 'string', -- 검색 타입 (world, clan, friend)
+    ------------------------------------------------
+    m_tRankData = 'table', -- 전체 랭크 정보
+    m_rankOffset = 'number', -- 오프셋
+    ------------------------------------------------
     })
 
 -------------------------------------
@@ -13,6 +21,12 @@ UI_EventIncarnationOfSinsRankingTotalTab = class(PARENT,{
 -------------------------------------
 function UI_EventIncarnationOfSinsRankingTotalTab:init(owner_ui)
     local vars = self:load('event_incarnation_of_sins_rank_popup_all.ui')
+
+    self.m_ownerUI = owner_ui
+    self.m_searchType = owner_ui.m_rankType
+    self.m_tRankData = {}
+    self.m_rankOffset = 1
+
 end
 
 -------------------------------------
@@ -22,6 +36,8 @@ function UI_EventIncarnationOfSinsRankingTotalTab:onEnterTab(first)
     if (first == true) then
         self:initUI()
         self:initButton()
+        self.m_searchType = self.m_ownerUI.m_rankType
+        self:request_EventIncarnationOfSinsAttrRanking('all')
     end
 
     self:refresh()
@@ -31,94 +47,48 @@ end
 -- function initUI
 -------------------------------------
 function UI_EventIncarnationOfSinsRankingTotalTab:initUI()
-    local vars = self.vars
 end
 
 -------------------------------------
 -- function initButton
 -------------------------------------
 function UI_EventIncarnationOfSinsRankingTotalTab:initButton()
-    local vars = self.vars
 end
 
 -------------------------------------
 -- function refresh
 -------------------------------------
 function UI_EventIncarnationOfSinsRankingTotalTab:refresh()
-    local vars = self.vars
 end
 
--------------------------------------
--- function makeRewardTableView
--------------------------------------
-function UI_EventIncarnationOfSinsRankingTotalTab:makeRewardTableView(my_info)
-    local vars = self.vars
-    local node = vars['rewardNode']
-    
-    -- 최조 한 번만 생성
-    if (self.m_rewardTableView) then
-        return
-    end
-
-    -- 콜로세움 랭킹 보상 테이블
-    -- TODO 랭킹 받아오기
-    local table_arena_rank = TABLE:get('table_arena_rank')
-    local struct_rank_reward = StructRankReward(table_arena_rank)
-    local l_arena_rank = struct_rank_reward:getRankRewardList()
-    self.m_structRankReward = struct_rank_reward
-
-    -- TODO 랭킹 받아오기
-    local table_arena = TABLE:get('table_arena')
-	local create_func = function(ui, data)
-        -- 티어 아이콘/ 티어 이름
-		local tier_id = data['tier_id']
-        if (tier_id) then
-            local tier = table_arena[tier_id]['tier']
-            local tier_icon = StructUserInfoArena:makeTierIcon(tier)
-            local tier_name = StructUserInfoArena:getTierName(tier) or ''
-            ui.vars['tierLabel']:setString(tier_name)
-            if (tier_icon) then
-                ui.vars['tierNode']:addChild(tier_icon)
-            end
-        end
-		self:createRewardFunc(ui, data, my_info)
-	end
-
-    -- 테이블 뷰 인스턴스 생성
-    local table_view = UIC_TableView(node)
-    table_view.m_defaultCellSize = cc.size(640, 55 + 5)
-    table_view:setCellUIClass(UI_ArenaRankingRewardListItem, create_func)
-    table_view:setDirection(cc.SCROLLVIEW_DIRECTION_VERTICAL)
-    table_view:setItemList(l_arena_rank)
-
-    self.m_rewardTableView = table_view
-end
 
 -------------------------------------
--- function makeArenaRankTableView
+-- function makeRankTableView
 -------------------------------------
-function UI_EventIncarnationOfSinsRankingTotalTab:makeArenaRankTableView(data)
+function UI_EventIncarnationOfSinsRankingTotalTab:makeRankTableView(data)
     local vars = self.vars
     local rank_node = vars['rankListNode']
     local rank_data = data
+    local my_rank_data = data['total_my_info'] -- g_eventIncarnationOfSinsData.m_tMyRankInfo['total']
+
 
     local make_my_rank_cb = function()
-        local my_data = rank_data['my_info'] or {}
-        local me_rank = UI_ArenaRankingListItem(my_data)
+        local my_data = my_rank_data or {}
+        local me_rank = UI_EventIncarnationOfSinsRankingTotalTabRankingListItem(my_data)
         vars['rankMeNode']:addChild(me_rank.root)
         me_rank.vars['meSprite']:setVisible(true)
     end
     
-    local l_rank_list = rank_data['list'] or {}
+    local l_rank_list = rank_data['total_list'] or {}
     
     -- 이전 랭킹 버튼 누른 후 콜백
     local function func_prev_cb(offset)
-        self:requestRank(offset)
+        self:refreshRank(offset)
     end
 
     -- 다음 랭킹 버튼 누른 후 콜백
     local function func_next_cb(offset)
-        self:requestRank(offset)
+        self:refreshRank(offset)
     end
 
     local uid = g_userData:get('uid')
@@ -129,13 +99,14 @@ function UI_EventIncarnationOfSinsRankingTotalTab:makeArenaRankTableView(data)
     end
     
     local rank_list = UIC_RankingList()
-    rank_list:setRankUIClass(UI_ArenaRankingListItem, create_cb)
+    rank_list:setRankUIClass(UI_EventIncarnationOfSinsRankingTotalTabRankingListItem, create_cb)
     rank_list:setRankList(l_rank_list)
     rank_list:setEmptyStr('랭킹 정보가 없습니다')
     rank_list:setMyRank(make_my_rank_cb)
     rank_list:setOffset(self.m_rankOffset)
-    rank_list:makeRankMoveBtn(func_prev_cb, func_next_cb, RANK_OFFSET_GAP)
+    rank_list:makeRankMoveBtn(func_prev_cb, func_next_cb, SCORE_OFFSET_GAP)
     rank_list:makeRankList(rank_node)
+
     
     local idx = 0
     for i,v in ipairs(l_rank_list) do
@@ -146,7 +117,7 @@ function UI_EventIncarnationOfSinsRankingTotalTab:makeArenaRankTableView(data)
      end
 
    -- 최상위 랭킹일 경우에는 포커싱을 1위에 함
-   if (self.m_rankType == 'world') and (self.m_rankOffset == 1) then
+   if (self.m_searchType == 'world') and (self.m_rankOffset == 1) then
         idx = 1
    end
 
@@ -154,31 +125,33 @@ function UI_EventIncarnationOfSinsRankingTotalTab:makeArenaRankTableView(data)
    rank_list.m_rankTableView:relocateContainerFromIndex(idx)
 end
 
+
 -------------------------------------
 -- function makeRewardTableView
 -------------------------------------
 function UI_EventIncarnationOfSinsRankingTotalTab:makeRewardTableView(my_info)
     local vars = self.vars
-    local node = vars['reawardNode']
+    local node = vars['userRewardNode']
     
     -- 최조 한 번만 생성
     if (self.m_rewardTableView) then
         return
     end
 
-    -- 콜로세움 랭킹 보상 테이블
-    local table_arena_rank = TABLE:get('table_arena_rank')
-    local struct_rank_reward = StructRankReward(table_arena_rank)
-    local l_arena_rank = struct_rank_reward:getRankRewardList()
+    -- 랭킹 보상 테이블
+    -- 서버로부터 랭킹 보상 테이블을 받아야 함
+    local table_rank = ''
+    local l_rank = struct_rank_reward:getRankRewardList()
+    local struct_rank_reward = StructRankReward(table_rank)
     self.m_structRankReward = struct_rank_reward
 
-
-    local table_arena = TABLE:get('table_arena')
+    -- 보상테이블 가져온다.
+    local table_rank = ''
 	local create_func = function(ui, data)
         -- 티어 아이콘/ 티어 이름
 		local tier_id = data['tier_id']
         if (tier_id) then
-            local tier = table_arena[tier_id]['tier']
+            local tier = table_rank[tier_id]['tier']
             local tier_icon = StructUserInfoArena:makeTierIcon(tier)
             local tier_name = StructUserInfoArena:getTierName(tier) or ''
             ui.vars['tierLabel']:setString(tier_name)
@@ -192,40 +165,202 @@ function UI_EventIncarnationOfSinsRankingTotalTab:makeRewardTableView(my_info)
     -- 테이블 뷰 인스턴스 생성
     local table_view = UIC_TableView(node)
     table_view.m_defaultCellSize = cc.size(640, 55 + 5)
-    table_view:setCellUIClass(UI_ArenaRankingRewardListItem, create_func)
+    table_view:setCellUIClass(UI_EventIncarnationOfSinsRankingTotalTabRewardListItem, create_func)
     table_view:setDirection(cc.SCROLLVIEW_DIRECTION_VERTICAL)
     table_view:setItemList(l_arena_rank)
 
     self.m_rewardTableView = table_view
 end
 
+
+
+
+
 -------------------------------------
--- function requestRank
+-- function request_EventIncarnationOfSinsAttrRanking
 -------------------------------------
-function UI_EventIncarnationOfSinsRankingTotalTab:requestRank(_offset) -- 다음/이전 버튼 눌렀을 경우 offset계산되어서 param으로 줌
-    local function finish_cb(ret)
-        cclog('oktt')
-        
+function UI_EventIncarnationOfSinsRankingTotalTab:request_EventIncarnationOfSinsAttrRanking(attr_type)
+    local attr_type = 'total'
+    
+    local function success_cb(ret)
         -- 랭킹 테이블 다시 만듬
-        self:makeArenaRankTableView(ret)
-		self:makeRewardTableView(ret['my_info'])
-        
-		if (ret['my_info']) then
-            -- 자신이 받을 보상에 포커싱
-            self:onFocusMyReward(ret['my_info'])
+        self:makeRankTableView(ret)
+        --self:makeRewardTableView(ret['my_info'])
+
+        -- TODO 리스트에 그려주기
+    end  
+    
+    local function fail_cb(ret)
+    end
+
+    local offset = self.m_rankOffset
+
+    local search_type = self.m_searchType
+    local limit = SCORE_OFFSET_GAP
+
+    g_eventIncarnationOfSinsData:request_EventIncarnationOfSinsAttrRanking(attr_type, search_type, offset, limit, success_cb, fail_cb)
+end
+
+-------------------------------------
+-- function refreshRank
+-------------------------------------
+function UI_EventIncarnationOfSinsRankingTotalTab:refreshRank(type) -- 다음/이전 버튼 눌렀을 경우 offset계산되어서 param으로 줌
+    local type = (type == 'top' or type == 'my') and 'world' or type
+    local offset = (type == 'top') and 1 or -1
+
+    self.m_searchType = type
+
+    self:request_EventIncarnationOfSinsAttrRanking('all')
+end
+
+
+
+
+
+
+
+
+-- tableview cell class
+local CELL_PARENT = class(UI, ITableViewCell:getCloneTable())
+-------------------------------------
+-- class UI_EventIncarnationOfSinsRankingAttributeTabListItem
+-------------------------------------
+UI_EventIncarnationOfSinsRankingTotalTabRankingListItem = class(CELL_PARENT,{
+        m_rankInfo = '',
+    })
+
+-------------------------------------
+-- function init
+-------------------------------------
+function UI_EventIncarnationOfSinsRankingTotalTabRankingListItem:init(m_rankInfo)
+    local vars = self:load('event_incarnation_of_sins_rank_popup_all_item_02.ui')
+    self.m_rankInfo = m_rankInfo
+
+    -- 닉네임 정보가 없다면, 다음/이전 버튼 데이터
+    if (not self.m_rankInfo['nick']) then
+        return    
+    end
+
+    self:initUI()
+    self:initButton()
+end
+
+-------------------------------------
+-- function initUI
+-------------------------------------
+function UI_EventIncarnationOfSinsRankingTotalTabRankingListItem:initUI()
+    local vars = self.vars
+    local t_rank_info = StructUserInfoArena:create_forRanking(self.m_rankInfo)
+
+    -- 점수 표시
+    vars['scoreLabel']:setString(self.m_rankInfo['rp'])
+
+    -- 유저 정보 표시 (레벨, 닉네임)
+    vars['userLabel']:setString(self.m_rankInfo['nick'])
+
+    -- 순위 표시
+    local rankStr = self.m_rankInfo['rank']
+    if (self.m_rankInfo['rank'] < 0) then
+        rankStr = '-'
+    end
+
+    vars['rankingLabel']:setString(rankStr)
+
+
+    do -- 리더 드래곤 아이콘
+        local ui = t_rank_info:getLeaderDragonCard()
+        if ui then
+            ui.root:setSwallowTouch(false)
+            vars['profileNode']:addChild(ui.root)
+            
+			ui.vars['clickBtn']:registerScriptTapHandler(function() 
+				local is_visit = true
+				UI_UserInfoDetailPopup:open(t_rank_info, is_visit, nil)
+			end)
         end
     end
 
-    -- 랭킹 데이터 요청
+    local struct_clan = t_rank_info:getStructClan()
+    if (struct_clan) then
+        -- 클랜 이름
+        local clan_name = struct_clan:getClanName()
+        vars['clanLabel']:setString(clan_name)
+        
+        -- 클랜 마크
+        local icon = struct_clan:makeClanMarkIcon()
+        if (icon) then
+            vars['markNode']:addChild(icon)
+        end
+    else
+        vars['clanLabel']:setVisible(false)
+    end
 
-    local offset = _offset
-    local limit = 20
-	local attr = 'all'
+    vars['itemMenu']:setSwallowTouch(false)
+end
 
-    -- attr = earth, water, fire, light, dark 
-    -- all : 모든 속성 리스트 
-    -- null : 전체 순위 20개씩 보여줌
-    -- offset : 1 , 21 , 41 ...
-    -- limit : 몇개를 보여줄지, 생략시 default 20개
-    -- g_eventIncarnationOfSinsData:request_eventIncarnationOfSinsRanking(offset, limit, attr, finish_cb, nil)
+
+
+-------------------------------------
+-- function initButton
+-------------------------------------
+function UI_ArenaRankingListItem:initButton()
+    local vars = self.vars
+    
+    local t_rank_info = self.m_rankInfo
+    local t_clan_info = t_rank_info['clan_info']
+    if (t_clan_info) then
+	    vars['clanBtn']:registerScriptTapHandler(function()
+            g_clanData:requestClanInfoDetailPopup(t_clan_info['id'])
+        end)
+    end
+end
+
+
+
+
+-- tableview cell class
+local CELL_PARENT = class(UI, ITableViewCell:getCloneTable())
+-------------------------------------
+-- class UI_EventIncarnationOfSinsRankingAttributeTabListItem
+-------------------------------------
+UI_EventIncarnationOfSinsRankingTotalTabRewardListItem = class(CELL_PARENT,{
+        m_rewardInfo = '',
+    })
+
+-------------------------------------
+-- function init
+-------------------------------------
+function UI_EventIncarnationOfSinsRankingTotalTabRewardListItem:init(t_data)
+    local vars = self:load('event_incarnation_of_sins_rank_popup_all_item_01.ui')
+    self.m_rewardInfo = t_data
+
+    self:initUI()
+end
+
+
+
+-------------------------------------
+-- function initUI
+-------------------------------------
+function UI_EventIncarnationOfSinsRankingTotalTabRewardListItem:initUI()
+    local vars = self.vars
+    local t_data = self.m_rewardInfo
+    local l_reward = TableClass:seperate(t_data['reward'], ',', true)
+
+    for i = 1, #l_reward do
+        local l_str = seperate(l_reward[i], ';')
+        local item_id = TableItem:getItemIDFromItemType(l_str[1]) -- 아이템 아이콘
+        local icon = IconHelper:getItemIcon(item_id) 
+
+        local cnt = l_str[2] -- 아이콘 수량
+        
+        if (icon and cnt) then
+			icon:setScale(0.4)
+		    vars['rewardLabel' .. i]:setString(comma_value(cnt))
+            vars['rewardNode' .. i]:addChild(icon)
+        end
+    end
+
+    local rank_str = StructRankReward.getRankName(t_data) 
+    vars['rankLabel']:setString(rank_str)
 end
