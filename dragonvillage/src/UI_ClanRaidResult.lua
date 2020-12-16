3808acd08fac883c8289f239e88cce3767faebda
kwkang <kwkang@highbrow.com>
2020-12-16 12:32:32 +0000

    [경웅] #'죄악의 화신 토벌작전' 서버로부터 받는 데이터값 rp -> score로 변경
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@115230 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 8cc72975188670f39706816869f368a2b8ffe20d

--- dragonvillage/src/UI_ClanRaidResult.lua
+++ dragonvillage/src/UI_ClanRaidResult.lua
@@ -484,12 +484,12 @@ function UI_ClanRaidResult:showLeaderBoard_IncarnationOfSins()
     local t_ex_me = g_eventIncarnationOfSinsData.m_tMyRankInfo['total']
     local t_ex_me = nil
     if (not t_ex_me) then -- 처음 때린 사람
-        t_ex_me = {['rp'] = 0, ['rank'] = t_me['rank'] + 1000, ['rate'] = 1}
+        t_ex_me = {['score'] = 0, ['rank'] = t_me['rank'] + 1000, ['rate'] = 1}
     end
 
     
     local ui_leader_board = UI_ResultLeaderBoard_IncarnationOfSins('incarnation_of_sins', true, true) -- type, is_move, is_popup
-    ui_leader_board:setScore(t_me['rp'] - t_ex_me['rp'], t_me['rp']) -- param : 더해진 점수, 더해진 점수가 반영된 최종 점수
+    ui_leader_board:setScore(t_me['score'] - t_ex_me['score'], t_me['score']) -- param : 더해진 점수, 더해진 점수가 반영된 최종 점수
     ui_leader_board:setRatio(t_ex_me['rate'], t_me['rate'])
     ui_leader_board:setRank(t_ex_me['rank'], t_me['rank'])
     ui_leader_board:setRanker(t_upper, t_me, t_lower)
