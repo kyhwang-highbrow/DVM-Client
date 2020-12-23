e143d3da069f321097eb941b8140177c948c5fac
ochoi <ochoi@highbrow.com>
2020-12-23 07:59:33 +0000

    [최외] #'게임 내 공지 시스템' 빠진 코드 추가 ;;
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@115405 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 24e88ecd316b3573663e22639f8126995a937289

--- dragonvillage/src/ServerData_Mail.lua
+++ dragonvillage/src/ServerData_Mail.lua
@@ -483,7 +483,7 @@ function ServerData_Mail:hasNewNotice()
     end
 
     --날짜값이 의미없는 값이면 공지 확인!
-    if tonumber(lastWatchedNoticeDate) < 0 then return true
+    if tonumber(lastWatchedNoticeDate) < 0 then return true end
 
     -- 더 작은 날짜로 저장되어 있으니 새 공지가 있음
     return tonumber(lastWatchedNoticeDate) < tonumber(t_notice.custom['start_date'])
