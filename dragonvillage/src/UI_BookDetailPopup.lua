817149ec1ae52e3c48ddaddff1032cf20f3372b4
kwkang <kwkang@highbrow.com>
2021-01-17 11:03:04 +0000

    [경웅] #드래곤 스킬 정보 팝업의 드래곤 아이콘에서의 등급과 드래곤 도감 정보 확인 팝업의 등급이 일치하지 않던 부분 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@115797 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: b501a5ff5d96bdc9bb5a7a54e482c208cf62e192

--- dragonvillage/src/UI_BookDetailPopup.lua
+++ dragonvillage/src/UI_BookDetailPopup.lua
@@ -482,7 +482,8 @@ function UI_BookDetailPopup:onChangeEvolution()
 
 				l_skill_icon[i].vars['clickBtn']:setActionType(UIC_Button.ACTION_TYPE_WITHOUT_SCAILING)
 				l_skill_icon[i].vars['clickBtn']:registerScriptTapHandler(function()
-					UI_SkillDetailPopup(t_dragon_data, i)
+					local new_dragon_data = self:makeDragonData()
+                    UI_SkillDetailPopup(new_dragon_data, i)
 				end)
 
 			-- 비어있는 스킬 아이콘 생성
