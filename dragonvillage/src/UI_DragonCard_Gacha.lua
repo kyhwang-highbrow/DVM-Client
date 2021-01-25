53df4b3b28edee3d2fadaa74e96ec054e28d1271
kwkang <kwkang@highbrow.com>
2021-01-25 06:22:23 +0000

    [경웅] #'드래곤 100개 뽑기' 드래곤 애니메이션 스케일링 관련 연출 변경
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@115938 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 33f574d17c2ee7cda1ffa7ceb7e43516ce37d7f3

--- dragonvillage/src/UI_DragonCard_Gacha.lua
+++ dragonvillage/src/UI_DragonCard_Gacha.lua
@@ -236,7 +236,7 @@ function UI_DragonCard_Gacha:openCard(b_do_open_cb)
             else
                 animator:changeAni('flip_3', false)
                 local sound_sequence = cc.Sequence:create(cc.CallFunc:create(play_gauging_sound), cc.DelayTime:create(0.6), cc.CallFunc:create(play_bomb_sound), 
-                cc.DelayTime:create(1.85), cc.CallFunc:create(card_open_finish_cb))
+                cc.DelayTime:create(1.9), cc.CallFunc:create(card_open_finish_cb))
                 self.root:runAction(sound_sequence)
             end
             
