f22b7d855ef1c31ba7658335b9abf62e006ffd3b
ochoi <ochoi@highbrow.com>
2021-02-19 05:40:32 +0000

    [최외] #콜로세움 개선 티어정보팝업 추가
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@116376 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 8444979ad20706f73cf3edd0aad38ae71522a1df

--- dragonvillage/src/UI_ArenaNewRankInfoPopup.lua
+++ dragonvillage/src/UI_ArenaNewRankInfoPopup.lua
@@ -10,20 +10,21 @@ UI_ArenaNewRankInfoPopup = class(PARENT,{
 -- function init
 -------------------------------------
 function UI_ArenaNewRankInfoPopup:init()
-    local vars = self:load('arena_new_scene_ranking_info_popup.ui')
+    self.m_uiName = 'UI_ArenaNewRankInfoPopup'
+    local vars = self:load('arena_new_popup_tier_reward.ui')
     UIManager:open(self, UIManager.POPUP)
 
     -- backkey 지정
     g_currScene:pushBackKeyListener(self, function() self:close() end, 'UI_ArenaNewRankInfoPopup')
 
     -- @UI_ACTION
-    --self:addAction(vars['rootNode'], UI_ACTION_TYPE_LEFT, 0, 0.2)
+    self:addAction(self.root, UI_ACTION_TYPE_LEFT, 0, 0.2)
     self:doActionReset()
     self:doAction(nil, false)
 
     self:initUI()
     self:initButton()
-    self:refresh()
+    --self:refresh()
 end
 
 -------------------------------------
@@ -39,6 +40,7 @@ end
 function UI_ArenaNewRankInfoPopup:initButton()
     local vars = self.vars
     vars['closeBtn']:registerScriptTapHandler(function() self:close() end)
+    vars['okBtn']:registerScriptTapHandler(function() self:close() end)
 end
 
 -------------------------------------
@@ -47,46 +49,6 @@ end
 function UI_ArenaNewRankInfoPopup:refresh()
     local vars = self.vars
 
-    do -- 최고 기록 데이터
-        local struct_user_info = g_arenaNewData:getPlayerArenaUserInfoHighRecord()
-
-        if (not struct_user_info) then return end
-
-        -- 티어 아이콘
-        vars['tierIconNode1']:removeAllChildren()
-        local icon = struct_user_info:makeTierIcon(nil, 'big')
-        vars['tierIconNode1']:addChild(icon)
-
-        -- 티어 이름
-        local tier_name = struct_user_info:getTierName()
-        vars['tierLabel1']:setString(tier_name)
-
-
-        -- 순위, 점수, 승률, 연승
-        local str = struct_user_info:getRankText() .. '\n'
-            .. struct_user_info:getRPText()  .. '\n'
-            .. struct_user_info:getWinRateText()  .. '\n'
-        vars['rankingLabel1']:setString(str)
-    end
-
-    do -- 현재 시즌 기록
-        local struct_user_info = g_arenaNewData:getPlayerArenaUserInfo()
-
-        -- 티어 아이콘
-        vars['tierIconNode2']:removeAllChildren()
-        local icon = struct_user_info:makeTierIcon(nil, 'big')
-        vars['tierIconNode2']:addChild(icon)
-
-        -- 티어 이름
-        local tier_name = struct_user_info:getTierName()
-        vars['tierLabel2']:setString(tier_name)
-
-        -- 순위, 점수, 승률
-        local str = struct_user_info:getRankText() .. '\n'
-            .. struct_user_info:getRPText()  .. '\n'
-            .. struct_user_info:getWinRateText()  .. '\n'
-        vars['rankingLabel2']:setString(str)
-    end
 end
 
 --@CHECK
