43cb1d22cc6a63c56b03ab146172c42029375db7
ochoi <ochoi@highbrow.com>
2021-02-22 13:11:07 +0000

    [최외] #콜로세움개선 다음티어 보상 아이콘 노출로직 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@116469 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 20a451a6762952e94590f4bc58759676d3bc4fb3

--- dragonvillage/src/ServerData_ArenaNew.lua
+++ dragonvillage/src/ServerData_ArenaNew.lua
@@ -942,9 +942,9 @@ end
 -- function setMatchUser
 -------------------------------------
 function ServerData_ArenaNew:hasArchiveReward(tier_id)    
-    if (not self.m_firstArchivedInfo or type(self.m_firstArchivedInfo) ~= 'table') then return false end
+    if (not self.m_firstArchivedInfo or type(self.m_firstArchivedInfo) ~= 'table') then return true end
 
-    if (self.m_firstArchivedInfo[tostring(tier_id)] and self.m_firstArchivedInfo[tostring(tier_id)] ~= 1) then return true end
+    if (self.m_firstArchivedInfo[tostring(tier_id)] and self.m_firstArchivedInfo[tostring(tier_id)] ~= 1) then return false end
 
-    return false
+    return true
 end
