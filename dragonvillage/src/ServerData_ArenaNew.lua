fa8de86b206aed5165461f6790bf866284060c49
ochoi <ochoi@highbrow.com>
2021-02-23 13:27:00 +0000

    [최외] #콜로세움개선 정신차려 친구!!!
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@116534 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 3ea658c334255554128d9f9e1fedf38e42c1fda8

--- dragonvillage/src/ServerData_ArenaNew.lua
+++ dragonvillage/src/ServerData_ArenaNew.lua
@@ -457,7 +457,7 @@ function ServerData_ArenaNew:request_setDeck(deckname, formation, leader, l_edoi
         local l_deck = ret['deck']
         self:refresh_playerUserInfo(t_data, l_deck, deckname)
 
-        if (deckname == 'arena_new_a' or deckname == 'arena_new_d') then
+        if (deckname == 'arena_new_a') then
             self.m_playerUserInfo:applyPvpDeckData(l_deck)
         elseif (deckname == 'arena_new_d') then
             self.m_playerUserInfo:applyPvpDefenseDeckData(l_deck)
