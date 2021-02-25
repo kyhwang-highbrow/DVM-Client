4c99666da3b77d0f3cec5d766048e229a7a28579
ochoi <ochoi@highbrow.com>
2021-02-25 09:19:50 +0000

    [최외] #인스타 링크 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@116618 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: f2ea55bd2bd459b826c9482fa3dca37930d6e366

--- dragonvillage/src/UI_EventPopupTab_Banner.lua
+++ dragonvillage/src/UI_EventPopupTab_Banner.lua
@@ -148,7 +148,7 @@ function UI_EventPopupTab_Banner:init_customUI()
         -- 인스타그램 페이지로 이동
         if vars['gameLinkBtn'] then
             vars['gameLinkBtn']:registerScriptTapHandler(function()
-                    SDKManager:goToWeb('https://www.instagram.com/p/CKiT6A8AsOB/')
+                    SDKManager:goToWeb('https://www.instagram.com/p/CLtY7W3HEmf/')
                 end)
         end
     end
