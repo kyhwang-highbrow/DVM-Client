2d1fed7103a3f0ceb64cfdfd4270809c9fee6247
ochoi <ochoi@highbrow.com>
2021-03-17 15:47:52 +0000

    [최외] #차원문 마누스 스테이지 정의 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@116977 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: bd3393ac66b7b85ded1a9468fd98e64c01a77b3d

--- dragonvillage/src/SummonedCreature.lua
+++ dragonvillage/src/SummonedCreature.lua
@@ -181,10 +181,150 @@ end
 function SummonedCreature:initState()
     PARENT.initState(self)
 
-    self:addState('charge', Monster.st_charge, 'idle', true)
-    self:addState('casting', Monster.st_casting, 'idle', true)
+    self:addState('charge', SummonedCreature.st_charge, 'idle', true)
+    self:addState('casting', SummonedCreature.st_casting, 'idle', true)
 
-    self:addState('wait', Monster.st_wait, 'idle', true)
+    -- 공격을 어떻게 할지 결정한다.
+    -- 물건이면 일정 시간 후 스킬 발동 후 사라짐
+    if (self.m_charTable and self.m_charTable['type'] and self.m_charTable['type'] == 'object') then 
+        self:addState('attackDelay', SummonedCreature.st_attackDelay, 'idle', false)
+    end
+
+
+    self:addState('wait', SummonedCreature.st_wait, 'idle', true)
+end
+
+
+
+-------------------------------------
+-------------------------------------
+--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+-- Animating States
+--@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+-------------------------------------
+-------------------------------------
+
+-------------------------------------
+-- function st_charge
+-------------------------------------
+function SummonedCreature.st_charge(owner, dt)
+    if (owner.m_stateTimer == 0) then
+        if (owner.m_chargeEffect) then
+            local attr = owner:getAttribute()
+
+            owner.m_chargeEffect:setVisible(true)
+            owner.m_chargeEffect:setFrame(0)
+        else
+            owner:changeState('attack')
+        end
+
+    elseif (owner.m_stateTimer >= owner.m_chargeDuration) then
+        owner:changeState('attack')
+
+    end
+end
+
+-------------------------------------
+-- function st_casting
+-------------------------------------
+function SummonedCreature.st_casting(owner, dt)
+    if (owner.m_stateTimer == 0) then
+		if (owner:isBoss()) then
+			SoundMgr:playEffect('EFFECT', 'monster_skill_cast') -- @ memo 출처 불분명하나 일단 남겨두기로함 (170928)
+		end
+
+        -- 스킬 사용 위치 정보가 있다면 해당 위치까지 이동시킴
+        if (owner.m_reservedSkillPos) then
+            owner:changeHomePosByTime(owner.m_reservedSkillPos.x, owner.m_reservedSkillPos.y, owner.m_reservedSkillCastTime)
+        end
+    end
+
+    PARENT.st_casting(owner, dt)
+end
+
+-------------------------------------
+-- function st_attackDelay
+-------------------------------------
+function Character.st_attackDelay(owner, dt)
+    if (owner.m_stateTimer == 0) then
+        owner.m_bEnableSpasticity = true
+
+        -- 어떤 스킬을 사용할 것인지 결정
+        local skill_id, is_add_skill
+
+        -- indie_time등의 우선순위가 높은 스킬로 인해 이전에 사용되지 못한 스킬이 있으면 사용하도록 함
+        if (owner.m_prevReservedSkillId) then
+            skill_id, is_add_skill = owner.m_prevReservedSkillId, owner.m_prevIsAddSkill
+            owner.m_stateTimer = owner.m_prevAttackDelayTimer
+
+            owner.m_prevReservedSkillId = nil
+            owner.m_prevIsAddSkill = nil
+            owner.m_prevAttackDelayTimer = 0
+        else
+            skill_id, is_add_skill = owner:getBasicAttackSkillID()
+        end
+
+		-- 스킬 캐스팅 불가 처리
+		if (owner.m_isSilence) then
+			is_add_skill = false
+			skill_id = owner:getSkillID('basic')
+		end
+
+        owner:reserveSkill(skill_id)
+        owner:calcAttackPeriod()
+		owner.m_isAddSkill = is_add_skill
+
+        -- 캐스팅 게이지
+        if (owner.m_castingNode) then
+            owner.m_castingNode:setVisible(false)
+        end
+
+        -- 부유중 연출
+        owner:runAction_Floating()
+
+    elseif (owner.m_stateTimer >= owner.m_attackPeriod) then
+        if (not owner.m_reservedSkillId) then
+            owner:changeState('attackDelay')
+        elseif (owner.m_reservedSkillCastTime > 0) then
+            owner:changeState('casting')
+        else
+            owner:changeState('charge')
+        end
+    end
+
+    if (not owner:hasStatusEffectToDisableSkill()) then
+        local tParam = {
+            time_out = owner.m_world.m_gameState:isTimeOut(),
+            hp_rate = owner.m_hpRatio
+        }
+
+        -- 일반 공격 관련 스킬보다 우선순위가 높은 스킬
+        local skill_id = owner:getInterceptableSkillID(tParam)
+        if (skill_id) then
+            local t_skill = owner:getSkillTable(skill_id)
+
+            if (owner:checkTarget(t_skill)) then
+                owner.m_prevReservedSkillId = owner.m_reservedSkillId
+                owner.m_prevIsAddSkill = owner.m_isAddSkill
+                owner.m_prevAttackDelayTimer = owner.m_stateTimer
+
+                owner:reserveSkill(skill_id)
+                owner.m_isAddSkill = false
+                
+                if (owner.m_reservedSkillCastTime > 0) then
+                    owner:changeState('casting')
+                else
+                    owner:changeState('attack')
+                end
+            else
+                -- 대상이 없을 경우
+                local skill_indivisual_info = owner:findSkillInfoByID(skill_id)
+                if (skill_indivisual_info) then
+                    skill_indivisual_info:startCoolTime()
+                end
+            end
+        end
+    end
 end
 
 
