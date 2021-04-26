640bb17eb30e4e883f96e9ebdb5880a220726994
ochoi <ochoi@highbrow.com>
2021-04-26 08:30:26 +0000

    [최외] #차원문 시즌효과 리더 필터링 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@117920 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: a86230c207936f2ac86a584c5442880666e87343

--- dragonvillage/src/FormationMgr_TargetRule.lua
+++ dragonvillage/src/FormationMgr_TargetRule.lua
@@ -126,7 +126,7 @@ function TargetRule_getTargetList(target_type, org_list, x, y, t_data)
     elseif (target_type == 'hero' or target_type == 'limited') then
         return TargetRule_getTargetList_category(target_type, org_list)
 
-    elseif (target_type == 'leader') then
+    elseif (pl.stringx.startswith(target_type, 'leader')) then
         return TargetRule_getTargetList_leader(org_list)
 
     else
