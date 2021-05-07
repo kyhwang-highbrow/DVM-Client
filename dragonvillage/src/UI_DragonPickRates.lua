fafbdbde69bed1412a213f5202a216755b991fab
ochoi <ochoi@highbrow.com>
2021-05-07 07:41:41 +0000

    [최외] 픽율 공동UI 선작업
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@118117 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 657400aaa28d9fa1607ee3608c3ec3548e206ff5

--- dragonvillage/src/UI_DragonPickRates.lua
+++ dragonvillage/src/UI_DragonPickRates.lua
@@ -16,6 +16,7 @@ function UI_DragonPickRates:init(parent)
     end
 
     self.root = parent
+
     self:initUI()
     self:initButton()
 end
@@ -26,13 +27,27 @@ end
 function UI_DragonPickRates:initUI()
     local node = self.root
 
+    local list = {
+        {did = 120454, rate = math.random(0, 50)},
+        {did = 120221, rate = math.random(0, 50)},
+        {did = 120402, rate = math.random(0, 50)},
+        {did = 120505, rate = math.random(0, 50)},
+        {did = 120101, rate = math.random(0, 30)},
+        {did = 120564, rate = math.random(0, 30)},
+        {did = 120011, rate = math.random(0, 30)},
+        {did = 120732, rate = math.random(0, 20)},
+        {did = 120702, rate = math.random(0, 10)},
+        {did = 120625, rate = math.random(0, 10)}
+    }
+
+    table.sort(list, function(a, b) return a['rate'] > b['rate'] end)
 
     -- 테이블 뷰 인스턴스 생성
     local table_view = UIC_TableView(node)
-    table_view.m_defaultCellSize = cc.size(720, 50)
-    table_view:setCellUIClass(UI_ArenaNewTierInfoListItem, create_func)
+    --table_view.m_defaultCellSize = cc.size(720, 50)
+    table_view:setCellUIClass(UI_DragonPickRateItem, create_func)
     table_view:setDirection(cc.SCROLLVIEW_DIRECTION_VERTICAL)
-    --table_view:setItemList(list)
+    table_view:setItemList(list)
 end
 
 -------------------------------------
@@ -45,4 +60,61 @@ end
 -- function refresh
 -------------------------------------
 function UI_DragonPickRates:refresh()
+end
+
+
+
+
+-------------------------------------
+-- class UI_DragonPickRateItem
+-------------------------------------
+UI_DragonPickRateItem = class(PARENT, IRankListItem:getCloneTable(), {
+        m_dragonInfo = 'table',
+    })
+
+
+-------------------------------------
+-- function init
+-------------------------------------
+function UI_DragonPickRateItem:init(t_dragon_info)
+    self.m_dragonInfo = t_dragon_info
+
+    local vars = self:load('dragon_pick_rate_item.ui')
+
+    self:initUI()
+end
+
+-------------------------------------
+-- function initUI
+-------------------------------------
+function UI_DragonPickRateItem:initUI()
+    local vars = self.vars
+
+    if (vars['dragonIconNode']) then
+        local icon = IconHelper:getDragonIconFromDid(self.m_dragonInfo['did'], 3, 1, 1)
+        icon:setScale(0.33)
+        vars['dragonIconNode']:addChild(icon)
+    end
+
+    if (vars['dragonNameLabel']) then
+        local name = TableDragon():getDragonName(self.m_dragonInfo['did'])
+
+        vars['dragonNameLabel']:setString( string.format('%s (%s%%)', name, tostring(self.m_dragonInfo['rate'])) )
+    end
+
+    if (vars['pickRateGauge']) then
+        vars['pickRateGauge']:setPercentage(self.m_dragonInfo['rate'])
+    end
+end
+
+-------------------------------------
+-- function initButton
+-------------------------------------
+function UI_DragonPickRateItem:initButton()
+end
+
+-------------------------------------
+-- function refresh
+-------------------------------------
+function UI_DragonPickRateItem:refresh()
 end
