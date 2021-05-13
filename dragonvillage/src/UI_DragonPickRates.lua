5c4ecea68e601a14aca84fd2906ab724bb4cb7a2
ochoi <ochoi@highbrow.com>
2021-05-13 09:39:04 +0000

    [최외] 드래곤 픽율 UI 임시커밋
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@118245 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: e91ffc6aad751547ca93fc996b074a2378e53cbe

--- dragonvillage/src/UI_DragonPickRates.lua
+++ dragonvillage/src/UI_DragonPickRates.lua
@@ -28,24 +28,26 @@ function UI_DragonPickRates:initUI()
     local node = self.root
 
     local list = {
-        {did = 120454, rate = math.random(0, 50)},
-        {did = 120221, rate = math.random(0, 50)},
-        {did = 120402, rate = math.random(0, 50)},
-        {did = 120505, rate = math.random(0, 50)},
-        {did = 120101, rate = math.random(0, 30)},
-        {did = 120564, rate = math.random(0, 30)},
-        {did = 120011, rate = math.random(0, 30)},
-        {did = 120732, rate = math.random(0, 20)},
-        {did = 120702, rate = math.random(0, 10)},
-        {did = 120625, rate = math.random(0, 10)}
+        {rank = 1, did = 120454, rate = 72},
+        {rank = 2, did = 120221, rate = 65},
+        {rank = 3, did = 120402, rate = 63},
+        {rank = 4, did = 120505, rate = 61},
+        {rank = 5, did = 120101, rate = 60},
+        {rank = 6, did = 120564, rate = 47},
+        {rank = 7, did = 120011, rate = 47},
+        {rank = 8, did = 120732, rate = 43},
+        {rank = 9, did = 120702, rate = 41},
+        {rank = 10, did = 120625, rate = 36}
     }
 
-    table.sort(list, function(a, b) return a['rate'] > b['rate'] end)
+    table.sort(list, function(a, b) return a['rank'] < b['rank'] end)
 
     -- 테이블 뷰 인스턴스 생성
     local table_view = UIC_TableView(node)
-    --table_view:setCellSizeToNodeSize(true)
-    table_view.m_defaultCellSize = cc.size(600, 60)
+    local root_width, root_height = node:getNormalSize()
+
+    table_view:setCellSizeToNodeSize(true)
+    table_view.m_defaultCellSize = cc.size(root_width, 78)
     table_view:setCellUIClass(UI_DragonPickRateItem, create_func)
     table_view:setDirection(cc.SCROLLVIEW_DIRECTION_VERTICAL)
     table_view:setItemList(list)
@@ -80,7 +82,7 @@ UI_DragonPickRateItem = class(PARENT, IRankListItem:getCloneTable(), {
 function UI_DragonPickRateItem:init(t_dragon_info)
     self.m_dragonInfo = t_dragon_info
 
-    local vars = self:load('dragon_pick_rate_item.ui')
+    local vars = self:load('dmgate_rank_popup_stage_dragon_item.ui')
 
     self:initUI()
 end
@@ -91,16 +93,24 @@ end
 function UI_DragonPickRateItem:initUI()
     local vars = self.vars
 
+    if (vars['rankLabel']) then
+        vars['rankLabel']:setString(self.m_dragonInfo['rank'])
+    end
+
     if (vars['dragonIconNode']) then
         local icon = IconHelper:getDragonIconFromDid(self.m_dragonInfo['did'], 3, 1, 1)
-        icon:setScale(0.33)
+        icon:setScale(0.8)
         vars['dragonIconNode']:addChild(icon)
     end
 
     if (vars['dragonNameLabel']) then
         local name = TableDragon():getDragonName(self.m_dragonInfo['did'])
 
-        vars['dragonNameLabel']:setString( string.format('%s (%s%%)', name, tostring(self.m_dragonInfo['rate'])) )
+        vars['dragonNameLabel']:setString(name)
+    end
+
+    if (vars['pickRateLabel']) then
+        vars['pickRateLabel']:setString(string.format('(%s%%)', tostring(self.m_dragonInfo['rate'])))
     end
 
     if (vars['pickRateGauge']) then
