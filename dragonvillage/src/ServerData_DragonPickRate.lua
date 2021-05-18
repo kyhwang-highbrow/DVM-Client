2bd7771878b9641e565517e7ebea6d73ed66314d
ochoi <ochoi@highbrow.com>
2021-05-18 02:08:20 +0000

    [최외] 차원문 픽율 리퀘스트가 비어서 가면 광신도로 노출하도록 한다...
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@118337 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 47966b45a3349589c62963b28cbceaa5f2184a7d

--- dragonvillage/src/ServerData_DragonPickRate.lua
+++ dragonvillage/src/ServerData_DragonPickRate.lua
@@ -24,6 +24,15 @@ function ServerData_DragonPickRate:request_getPickRate(data, cb_func)
 	local group = data['group']
 	local stage = data['stage']
 
+    -- 데이터가 빌 수도 있다고 함
+    if (not group) or (not tonumber(group)) or group <= 0 then
+        group = 3010000
+    end
+
+    if (not stage) or (not tonumber(stage)) or stage <= 0 then
+        stage = 3011001
+    end
+
     -- 콜백 함수
     local function success_cb(ret)
         -- @analytics
@@ -33,6 +42,14 @@ function ServerData_DragonPickRate:request_getPickRate(data, cb_func)
 			cb_func(ret)
 		end
     end
+    
+    -- 응답 상태 처리 함수
+    local t_error = {
+        [-1190] = Str('오류가 발생했습니다.'), -- 테이블이 없는 경우?
+        [-9999] = Str('오류가 발생했습니다.'), -- unknown
+    }
+
+    local response_status_cb = MakeResponseCB(t_error)
 
     -- 네트워크 통신 UI 생성
     local ui_network = UI_Network()
@@ -42,6 +59,7 @@ function ServerData_DragonPickRate:request_getPickRate(data, cb_func)
 	ui_network:setParam('group', group)
     ui_network:setParam('stage', stage)
     ui_network:setSuccessCB(success_cb)
+    ui_network:setResponseStatusCB(response_status_cb)
     ui_network:setRevocable(false)
     ui_network:setReuse(false)
     ui_network:request()
