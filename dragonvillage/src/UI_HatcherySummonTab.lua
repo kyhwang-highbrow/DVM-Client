c9053d09bdde8b91ac6274061c4013ce81778de5
ochoi <ochoi@highbrow.com>
2021-06-03 08:45:25 +0000

    [최외] 부화소 뽑기 알림 팝업 작업
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@118478 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 0c67b3521e2757bf28eaa198a227c3345c7577de

--- dragonvillage/src/UI_HatcherySummonTab.lua
+++ dragonvillage/src/UI_HatcherySummonTab.lua
@@ -379,7 +379,6 @@ function UI_HatcherySummonTab:setChanceUpDragons()
     for _, t_data in pairs(pickup_dragon_map) do
         local did = t_data['did']
         local attr = TableDragon:getDragonAttr(did)
-        cclog(attr)
 
         -- 빛어둠 3 / 땅물불 2
         idx = isExistValue(attr, 'light', 'dark') and 3 or 2
@@ -685,7 +684,7 @@ function UI_HatcherySummonTab:requestSummon(t_egg_data, old_ui, is_again)
 
     elseif (egg_id == 700001) then
         local msg = Str('"{1}" 진행하시겠습니까?', t_egg_data['name'])
-        UI_HacheryPickupBtnPopup(item_key, item_value, msg, ok_btn_cb, cancel_btn_cb)
+        UI_HacheryPickupBtnPopup(self, item_key, item_value, msg, ok_btn_cb, cancel_btn_cb)
 
     else
         local msg = Str('"{1}" 진행하시겠습니까?', t_egg_data['name'])
@@ -772,12 +771,14 @@ end
 -- class UI_HacheryPickupBtnPopup
 -- @brief 
 ----------------------------------------------------------------------
-UI_HacheryPickupBtnPopup = class(UI, {})
+UI_HacheryPickupBtnPopup = class(UI, {
+    m_parent = 'UI_HatcherySummonTab',
+})
  
 ----------------------------------------------------------------------
 -- function init
 ----------------------------------------------------------------------
-function UI_HacheryPickupBtnPopup:init(item_key, item_value, msg, ok_btn_cb, cancel_btn_cb)
+function UI_HacheryPickupBtnPopup:init(parent, item_key, item_value, msg, ok_btn_cb, cancel_btn_cb)
 	self.m_uiName = 'UI_HacheryPickupBtnPopup'
 
     local vars = self:load('hatchery_summon_confirm_popup.ui')
@@ -789,5 +790,73 @@ function UI_HacheryPickupBtnPopup:init(item_key, item_value, msg, ok_btn_cb, can
 
     -- backkey 지정
     g_currScene:pushBackKeyListener(self, function() self:close() end, 'UI_HacheryPickupBtnPopup')
+
+    
     vars['closeBtn']:registerScriptTapHandler(function() self:close() end)
+    vars['cancelBtn']:registerScriptTapHandler(function() self:close() end)
+
+    self.m_parent = parent
+
+    vars['selectLabel']:setString(msg)
+    vars['priceLabel']:setString(comma_value(item_value))
+
+    self:setChanceUpDragons()
+end
+
+
+-------------------------------------
+-- function setChanceUpDragons
+-- @brief 확률업 드래곤 
+-------------------------------------
+function UI_HacheryPickupBtnPopup:setChanceUpDragons()
+    local vars = self.vars
+
+    local idx = 0
+    local desc_idx = 0 -- dragonName1 :드래곤 1마리 일 때, dragonName2, dragonName3 : 드래곤 2마리 일 때
+
+    -- normal_did 물불땅 / unique_did 빛어둠
+    -- 바로 알아볼 수 있게 같은 로직 두번 돌림
+    local normal_did, unique_did = g_hatcheryData:getSelectedPickup()
+    local l_dragon = {}
+
+    if (normal_did) then table.insert(l_dragon, {did = normal_did}) end
+    if (unique_did) then table.insert(l_dragon, {did = unique_did}) end
+
+    local pickup_dragon_map = self.m_parent:makeDragonInfoMap(l_dragon)
+
+    for _, t_data in pairs(pickup_dragon_map) do
+        local did = t_data['did']
+        local attr = TableDragon:getDragonAttr(did)
+
+        -- 빛어둠 3 / 땅물불 2
+        idx = isExistValue(attr, 'light', 'dark') and 2 or 1
+        desc_idx = idx
+
+        -- 드래곤 이름
+        local name = TableDragon:getChanceUpDragonName2(did)
+
+        vars['dragonNameLabel'..desc_idx]:setString(name)
+
+        -- 드래곤 카드
+        do
+            local t_dragon_data = {}
+            t_dragon_data['did'] = did
+            t_dragon_data['evolution'] = 1
+            t_dragon_data['grade'] = 5
+            t_dragon_data['skill_0'] = 1
+            t_dragon_data['skill_1'] = 1
+            t_dragon_data['skill_2'] = 0
+            t_dragon_data['skill_3'] = 0
+
+            -- 드래곤 클릭 시, 도감 팝업
+            local func_tap = function()
+                UI_BookDetailPopup.openWithFrame(did, nil, 1, 0.8, true)    -- param : did, grade, evolution scale, ispopup
+            end
+
+            local dragon_card = UI_DragonCard(StructDragonObject(t_dragon_data))
+            dragon_card.root:setScale(0.66)
+            dragon_card.vars['clickBtn']:registerScriptTapHandler(function() func_tap() end)
+            vars['dragonCard'..desc_idx]:addChild(dragon_card.root)
+        end
+    end
 end
