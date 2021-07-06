4d907363230cddd31ff8861f89d7ff4a28c95eb2
ochoi <ochoi@highbrow.com>
2021-07-06 08:59:26 +0000

    [최외] #콜로세움개선 ui 구조 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@118973 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: a75c755e02e4679fbaf4262b80cac292e27c9903

--- dragonvillage/src/UI_EventArenaPlay.lua
+++ dragonvillage/src/UI_EventArenaPlay.lua
@@ -45,8 +45,8 @@ function UI_EventArenaPlay:refresh()
     local vars = self.vars
 
     if (vars['timeLabel']) then vars['timeLabel']:setString(g_eventArenaPlayData:getRemainEventTimeStr()) end
-    if (vars['playNumberLabel']) then  vars['playNumberLabel']:setString(Str('승리 횟수 보상: {1}회', g_eventArenaPlayData:getWinCount())) end
-    if (vars['winNumberLabel']) then  vars['winNumberLabel']:setString(Str('승리 횟수 보상: {1}회', g_eventArenaPlayData:getPlayCount())) end
+    if (vars['playNumberLabel']) then  vars['playNumberLabel']:setString(Str('참여 횟수 보상: {1}회', g_eventArenaPlayData:getPlayCount())) end
+    if (vars['winNumberLabel']) then  vars['winNumberLabel']:setString(Str('승리 횟수 보상: {1}회', g_eventArenaPlayData:getWinCount())) end
 
     
     local play_reward_info = g_eventArenaPlayData:getPlayRewardInfo()
@@ -55,21 +55,26 @@ function UI_EventArenaPlay:refresh()
     local play_reward_step = play_reward_info['product']['step']
     local win_reward_step = play_reward_info['product']['step']
 
-    local idx_play_item_start = 1
-    local idx_win_item_start = 6
-
     for idx = 1, play_reward_step do
         local is_received = play_reward_info['reward'][idx] == 1
-        vars['checkSprite' .. idx_play_item_start + (idx - idx_play_item_start)]:setVisible(is_received)
+        local can_receive = g_eventArenaPlayData:getPlayCount() >= play_reward_info['product']['price_' .. idx] and (not is_received)
+
+        vars['checkSprite' .. idx]:setVisible(is_received)
+        vars['playRewardSprite' .. idx]:setVisible(can_receive)
     end
 
     for idx = 1, win_reward_step do
         local is_received = win_reward_info['reward'][idx] == 1
-        vars['checkSprite' .. idx_win_item_start + (idx - idx_play_item_start)]:setVisible(is_received)
+        local can_receive = g_eventArenaPlayData:getWinCount() >= play_reward_info['product']['price_' .. idx] and (not is_received)
+
+        vars['checkSprite' .. idx]:setVisible(is_received)
+        vars['winRewardSprite' .. idx]:setVisible(can_receive)
     end
 
     if (vars['rewardPlayBtn']) then vars['rewardPlayBtn']:setEnabled(g_eventArenaPlayData:hasReward('play')) end
     if (vars['rewardWinBtn']) then vars['rewardWinBtn']:setEnabled(g_eventArenaPlayData:hasReward('win')) end
+    if (vars['receivePlaySprite']) then vars['receivePlaySprite']:setVisible(not g_eventArenaPlayData:hasReward('play')) end
+    if (vars['receiveWinSprite']) then vars['receiveWinSprite']:setVisible(not g_eventArenaPlayData:hasReward('win')) end
 end
 
 -------------------------------------
