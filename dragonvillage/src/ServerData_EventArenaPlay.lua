33987bfac238e12c753b4c0b45faa91655ef8791
ochoi <ochoi@highbrow.com>
2021-07-07 01:39:18 +0000

    [최외] #콜로세움개편 이벤트 기간 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@119005 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 7f9f4625fd304247ff3065e5a7e32a841a33661f

--- dragonvillage/src/ServerData_EventArenaPlay.lua
+++ dragonvillage/src/ServerData_EventArenaPlay.lua
@@ -97,7 +97,8 @@ function ServerData_EventArenaPlay:getRemainEventTimeStr()
         enable = false
         local show_second = true
         local first_only = false
-        msg = Str('{1} 남음', datetime.makeTimeDesc(time, show_second, first_only))
+        
+        msg = Str('이벤트 종료까지 {1} 남음', datetime.makeTimeDesc(time, show_second, first_only))
     end
 
     return msg
