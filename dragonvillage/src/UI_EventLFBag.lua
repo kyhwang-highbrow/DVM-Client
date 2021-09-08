9899a6af76d51d05bc134e45c98bf2a614bdbd03
yjkil <yjkil@highbrow.com>
2021-09-08 07:20:19 +0000

    [길영진] # 번역 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@120003 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 6a152d5ce222786019d1ea5d2cb1d472225eb90b

--- dragonvillage/src/UI_EventLFBag.lua
+++ dragonvillage/src/UI_EventLFBag.lua
@@ -625,7 +625,7 @@ function UI_EventLFBag:click_infoBtn()
     vars['descLabel04']:setString(Str('대박 점수는 5단계 이상에서 획득 가능합니다.'))
     vars['descLabel05']:setString(Str('중단을 선택하면 확보한 누적 보상을 우편으로 받을 수 있으며, 구슬의  단계가 초기화됩니다.'))
     vars['descLabel06']:setString(Str('{1}은(는) 주사위 이벤트와 {1} 패키지를 통해 획득할 수 있습니다.', self.m_eventItemName))
-    vars['descLabel07']:setString(Str('{1}을 통해 획득한 점수에 따라 일일 랭킹, 종합 랭킹 보상을 지급합니다.', self.m_eventItemName))
+    vars['descLabel07']:setString(Str('{1}을(를) 통해 획득한 점수에 따라 일일 랭킹, 종합 랭킹 보상을 지급합니다.', self.m_eventItemName))
 
 end
 
