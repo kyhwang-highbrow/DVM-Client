1b5b46b37e4ae74b0cfcdf612a4190fde7a86c36
yjkil <yjkil@highbrow.com>
2022-04-27 13:19:21 +0000

    [영진] 오타 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@122487 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 74037be8daca49c7466b7cf1cd9b3462f41843db

--- dragonvillage/src/UI_Package.lua
+++ dragonvillage/src/UI_Package.lua
@@ -360,18 +360,18 @@ function UI_Package:click_buyBtn(index)
         
         if struct_product:isOnlyContain('rune_box') then
             ItemObtainResult_ShowMailBox(ret, MAIL_SELECT_TYPE.RUNE_BOX, self.m_obtainResultCloseCb)
-        else    
-            
-        local is_basic_goods_shown = false
-        if self.m_package_name and (string.find(self.m_package_name, 'package_lucky_box')) then
-            is_basic_goods_shown = true
-        end
+        else        
+            local is_basic_goods_shown = false
+            if self.m_package_name and (string.find(self.m_package_name, 'package_lucky_box')) then
+                is_basic_goods_shown = true
+            end
 
-        if (self.m_mailSelectType ~= MAIL_SELECT_TYPE.NONE) then
-            ItemObtainResult_ShowMailBox(ret, self.m_mailSelectType, self.m_obtainResultCloseCb)
-        else
-            -- 아이템 획득 결과창
-            ItemObtainResult_Shop(ret, is_basic_goods_shown, self.m_obtainResultCloseCb)
+            if (self.m_mailSelectType ~= MAIL_SELECT_TYPE.NONE) then
+                ItemObtainResult_ShowMailBox(ret, self.m_mailSelectType, self.m_obtainResultCloseCb)
+            else
+                -- 아이템 획득 결과창
+                ItemObtainResult_Shop(ret, is_basic_goods_shown, self.m_obtainResultCloseCb)
+            end
         end
 
         -- 갱신이 필요한 상태일 경우
