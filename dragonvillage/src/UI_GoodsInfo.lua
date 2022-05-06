db7b0bd0a13aebeb701e6aac40d2f7059222aac3
jylee <jylee@highbrow.com>
2022-05-06 01:33:22 +0000

    [중엽] '자수정' 버튼 상점 이동 기능 추가
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@122541 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: f4a982563f1b19db1e70a249d94e6985fcd7963d

--- dragonvillage/src/UI_GoodsInfo.lua
+++ dragonvillage/src/UI_GoodsInfo.lua
@@ -167,6 +167,8 @@ function UI_GoodsInfo:click_chargeBtn()
         UINavigatorDefinition:goTo('shop', 'honor')
     elseif (goods_type == 'valor') then
         UINavigatorDefinition:goTo('shop', 'valor')
+    elseif (goods_type == 'amethyst') then
+        UINavigatorDefinition:goTo('shop', 'amethyst')
     elseif (goods_type == 'capsule_coin') then
         local capsule_coin_package_popup = PackageManager:getTargetUI('package_capsule_coin', true)
     elseif (goods_type == 'st') then
