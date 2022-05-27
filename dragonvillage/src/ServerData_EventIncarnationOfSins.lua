ed764575e9468158362d510115a9ef529734338e
jylee <jylee@highbrow.com>
2022-05-27 00:51:02 +0000

    [중엽] 테스트 커밋
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@122864 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: df34bf38379a80813e78c0a83d348b2476677753

--- dragonvillage/src/ServerData_EventIncarnationOfSins.lua
+++ dragonvillage/src/ServerData_EventIncarnationOfSins.lua
@@ -318,6 +318,8 @@ function ServerData_EventIncarnationOfSins:openRankingPopupForLobby()
 
         local last_info = self.m_tMyRankInfo['total']
         local reward_info = self.m_tReceiveReward
+        ccdump(last_info)
+        ccdump(reward_info)
 
         -- 보상을 받을 수 있는 상태라면
         if (last_info and reward_info) then
@@ -394,7 +396,7 @@ function ServerData_EventIncarnationOfSins:response_eventIncarnationOfSinsInfo(r
     if (ret['reward']) then
         self.m_rewardStatus = ret['reward']
     end 
-
+    
     if (ret['reward_info']) then
         self.m_tReceiveReward = ret['reward_info']
     else
