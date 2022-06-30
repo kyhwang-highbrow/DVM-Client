bc3965fb51cf67774c5035610e40aff84cea2ecb
dhki <dhki@highbrow.com>
2022-06-30 02:18:49 +0000

    [대현] 인스트리아 드래그 스킬 번역
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@123076 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 146138ed6d422dd2f3a413784e5e8a346bb9ae4c

--- dragonvillage/translate/lang_es_patch.lua
+++ dragonvillage/translate/lang_es_patch.lua
@@ -1,2 +1,1 @@
-return {['가만히 있게. 플루스! 테이머가 왔잖나']='Stay still, Plus! Our Tamer is here.',
-['무한의 주인, 인스트리아']='El amo infinito, la Instria'}
+return {['초거대 플루스']='súper gigante plus'}
--- dragonvillage/translate/lang_fa_patch.lua
+++ dragonvillage/translate/lang_fa_patch.lua
@@ -1,4 +1,1 @@
-return {['지금이다. 플루스 빔!']='Now! Plus Beam!',
-['플루스 중에 왕이라네~']='Here\'s the king of Fluffies!',
-['이 작은 친구들은 플루스라네. 내 소중한 조수들이지']='These little friends are called the Pluses. They\'re my precious assistants.',
-['무한의 주인, 인스트리아']='The Master of Infinity, Instria'}
+return {['초거대 플루스']='Super Giant Plus'}
