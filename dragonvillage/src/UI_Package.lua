011400fd4cf3883003415a5c460075f08ae9d333
yjkil <yjkil@highbrow.com>
2022-09-20 11:07:38 +0000

    [영진] #특별할인 패키지 오픈 시 오류가 발생하던 문제 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@123375 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 66a1ca38cdbb129b39e897326110d34c7616dd73

--- dragonvillage/src/UI_Package.lua
+++ dragonvillage/src/UI_Package.lua
@@ -270,8 +270,8 @@ function UI_Package:initEachProduct(index, struct_product)
     -- 5주년 기념 이벤트 
     local pid = struct_product:getProductID()
     local product_index = TablePackageBundle:getPidsIndex(pid)
-    node = vars['bgSprite' .. product_index] or vars['bgSprite']
     if (product_index ~= nil) and (node ~= nil) then
+        node = vars['bgSprite' .. product_index] or vars['bgSprite']
         node:setVisible(true)
     end
 end
