9fab29a24d1b893ad3c10ee1a1c7bc5a594dccec
kwkang <kwkang@highbrow.com>
2023-03-06 08:38:33 +0000

    [기영] #드래곤 스킨 번역 적용 시키기
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@124676 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: aecd16736823ececab50448952f98f80bdbb6ad5

--- dragonvillage/src/StructDragonSkin.lua
+++ dragonvillage/src/StructDragonSkin.lua
@@ -250,7 +250,7 @@ function StructDragonSkin:makeDefaultSkin(did)
     basic_data['skin_id'] = 0
     basic_data['did'] = did
     basic_data['ui_priority'] = 99
-    basic_data['t_name'] = '기본 스킨'
+    basic_data['t_name'] = Str('기본 스킨')
     basic_data['t_desc'] = ''
     basic_data['attribute'] = TableDragon:getValue(did, 'attr')
     basic_data['res'] = TableDragon:getValue(did, 'res')
