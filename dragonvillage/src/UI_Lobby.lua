fcd9d3c8fb85811fd0921f98097fb6ca04e5cdf7
kwkang <kwkang@highbrow.com>
2023-03-08 02:21:15 +0000

    [기영] #화이트 데이 이벤트 핫타임 노티 처리 살려놓음
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@124738 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: cee9f8f7a88c9c9a862120d50869047c26c237ae

--- dragonvillage/src/UI_Lobby.lua
+++ dragonvillage/src/UI_Lobby.lua
@@ -863,7 +863,7 @@ function UI_Lobby:initButton()
     vars['giftBoxBtn']:registerScriptTapHandler(function() self:click_giftBoxBtn() end) -- 랜덤박스(광고)
     vars['exchangeBtn']:registerScriptTapHandler(function() self:click_exchangeBtn() end) -- 교환이벤트
     vars['bingoBtn']:registerScriptTapHandler(function() self:click_bingoBtn() end) -- 빙고 이벤트
-    --vars['halloweenEventBtn']:registerScriptTapHandler(function() self:click_halloweenEventBtn() end) -- 할로윈 이벤트
+    vars['halloweenEventBtn']:registerScriptTapHandler(function() self:click_halloweenEventBtn() end) -- event_rune_festival/할로윈 이벤트
     vars['diceBtn']:registerScriptTapHandler(function() self:click_diceBtn() end) -- 주사위이벤트
     vars['luckyBagEventBtn']:registerScriptTapHandler(function() self:click_lfbagBtn() end) -- 소원 구슬 이벤트
     vars['alphabetBtn']:registerScriptTapHandler(function() self:click_alphabetBtn() end) -- 알파벳 이벤트
@@ -1258,20 +1258,18 @@ function UI_Lobby:update_highlight()
         end
     end
 
-    
+    do -- 할로윈 이벤트
+        vars['halloweenNotiSprite']:setVisible(false)
+        vars['halloweenNotiYellow']:setVisible(false)
 
-    -- do -- 할로윈 이벤트
-    --     vars['halloweenNotiSprite']:setVisible(false)
-    --     vars['halloweenNotiYellow']:setVisible(false)
+        if g_hotTimeData:isActiveEvent('event_rune_festival') then
+            if (g_eventRuneFestival:isDailyStLimit() == false) then
+                vars['halloweenNotiYellow']:setVisible(true)
+            end
 
-    --     if g_hotTimeData:isActiveEvent('event_rune_festival') then
-    --         if (g_eventRuneFestival:isDailyStLimit() == false) then
-    --             vars['halloweenNotiYellow']:setVisible(true)
-    --         end
-
-    --         vars['shopEventNoti']:setVisible(true)
-    --     end
-    -- end
+            vars['shopEventNoti']:setVisible(true)
+        end
+    end
 
     do -- 알파벳 이벤트
         vars['alphabetNotiRed']:setVisible(false)
@@ -2320,7 +2318,7 @@ function UI_Lobby:update_rightButtons()
     vars['bingoBtn']:setVisible(g_hotTimeData:isActiveEvent('event_bingo'))
 
     -- 할로윈 룬 축제(할로윈 이벤트)
-    --vars['halloweenEventBtn']:setVisible(g_hotTimeData:isActiveEvent('event_rune_festival'))
+    vars['halloweenEventBtn']:setVisible(g_hotTimeData:isActiveEvent('event_rune_festival'))
 
     -- 알파벳 이벤트
     vars['alphabetBtn']:setVisible(g_hotTimeData:isActiveEvent('event_alphabet'))
@@ -2470,7 +2468,7 @@ function UI_Lobby:update_rightButtons()
     table.insert(t_btn_name, 'matchCardBtn')
     table.insert(t_btn_name, 'mandragoraBtn')
     table.insert(t_btn_name, 'bingoBtn')
-    --table.insert(t_btn_name, 'halloweenEventBtn')
+    table.insert(t_btn_name, 'halloweenEventBtn')
     table.insert(t_btn_name, 'diceBtn')
     table.insert(t_btn_name, 'alphabetBtn')
     table.insert(t_btn_name, 'exchangeBtn')
