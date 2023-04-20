83581ba8a8c5790387db872769130b21e06552be
kyhwang <kyhwang@highbrow.com>
2023-04-20 07:41:55 +0000

    [기영] #태초의 빛 일일 퀘스트 보상 표기 2배 적용 표기 제거
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@125492 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 3132cb9d621fc8a56984387c00d4520cd311606c

--- dragonvillage/src/UI_StoryDungeonEventQuestListItem.lua
+++ dragonvillage/src/UI_StoryDungeonEventQuestListItem.lua
@@ -38,12 +38,47 @@ function UI_StoryDungeonEventQuestListItem:setQuestDescLabel()
     end
 end
 
+-------------------------------------
+-- function click_rewardBtn
+-------------------------------------
+function UI_StoryDungeonEventQuestListItem:click_rewardBtn(ui_quest_popup)
+    
+    --ui_quest_popup:setBlock(true)
+	local cb_function = function(t_quest_data)
+		
+        local is_mail = TableEventQuest:isRewardMailTypeQuest(self.m_questData['qid'])
+        local l_reward = self:makeRewardList()
+        if (not is_mail) then
+            UI_ObtainToastPopup(l_reward)
+        else
+            -- 우편함으로 전송
+		    local toast_msg = Str('보상이 우편함으로 전송되었습니다.')
+            UI_ToastPopup(toast_msg)
+		end
+
+        -- idx 교체 (테이블뷰의 아이템을 찾기위해 현재것으로 idx 지정)
+        if (t_quest_data) then
+            t_quest_data['idx'] = self.m_questData['idx']
+        end
+
+		-- 갱신
+		self:refresh(t_quest_data)
+		ui_quest_popup:refresh(t_quest_data)
+		--ui_quest_popup:setBlock(false)
+
+        -- 일일퀘스트 보상 2배 상품 판매촉진하는 팝업 조건 체크 후 팝업 출력
+        self:checkPromoteQuestDouble(ui_quest_popup)
+	end
+
+	g_eventDragonStoryDungeon:requestStoryDungeonQuestReward(self.m_questData, cb_function)
+end
+
 
 -------------------------------------
 -- function setRewardCard
 -- @brief 보상 아이콘 표시
 -------------------------------------
-function UI_StoryDungeonEventQuestListItem:setRewardCard()
+function UI_QuestListItem:setRewardCard()
     local vars = self.vars
 
     local l_reward_info = self.m_questData:getRewardInfoList()
@@ -107,7 +142,6 @@ function UI_StoryDungeonEventQuestListItem:setRewardCard()
             end
         end
     end
-    
 
 	-- 클랜 경험치
 	if (not g_clanData:isClanGuest()) then
@@ -144,39 +178,4 @@ function UI_StoryDungeonEventQuestListItem:setRewardCard()
             end
         end
     end
-end
-
--------------------------------------
--- function click_rewardBtn
--------------------------------------
-function UI_StoryDungeonEventQuestListItem:click_rewardBtn(ui_quest_popup)
-    
-    --ui_quest_popup:setBlock(true)
-	local cb_function = function(t_quest_data)
-		
-        local is_mail = TableEventQuest:isRewardMailTypeQuest(self.m_questData['qid'])
-        local l_reward = self:makeRewardList()
-        if (not is_mail) then
-            UI_ObtainToastPopup(l_reward)
-        else
-            -- 우편함으로 전송
-		    local toast_msg = Str('보상이 우편함으로 전송되었습니다.')
-            UI_ToastPopup(toast_msg)
-		end
-
-        -- idx 교체 (테이블뷰의 아이템을 찾기위해 현재것으로 idx 지정)
-        if (t_quest_data) then
-            t_quest_data['idx'] = self.m_questData['idx']
-        end
-
-		-- 갱신
-		self:refresh(t_quest_data)
-		ui_quest_popup:refresh(t_quest_data)
-		--ui_quest_popup:setBlock(false)
-
-        -- 일일퀘스트 보상 2배 상품 판매촉진하는 팝업 조건 체크 후 팝업 출력
-        self:checkPromoteQuestDouble(ui_quest_popup)
-	end
-
-	g_eventDragonStoryDungeon:requestStoryDungeonQuestReward(self.m_questData, cb_function)
 end
