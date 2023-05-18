3352398ef3a11518c58912424a149aae9585b6fc
kwkang <kwkang@highbrow.com>
2023-05-18 06:53:52 +0000

    [기영] #크로스 프로모션 코드 일부 롤백
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@125764 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 5a97400a7b0f86be9a38214d77d4ffd394430818

--- dragonvillage/src/UI_Lobby.lua
+++ dragonvillage/src/UI_Lobby.lua
@@ -185,12 +185,11 @@ function UI_Lobby:entryCoroutine()
             if co:waitWork() then return end
         end
 
-        -- hky
---[[         if (g_hotTimeData:isActiveEvent('event_vote')) then
+        if (g_hotTimeData:isActiveEvent('event_vote')) then
             co:work('# 신화 드래곤 투표권 이벤트 정보 받는 중')
             g_eventVote:requestEventVoteInfo(co.NEXT, required_fail_cb)
             if co:waitWork() then return end
-        end ]]
+        end
 
         if (g_eventLFBagData:canPlay() or g_eventLFBagData:canReward()) then
             co:work('# 복주머니 이벤트 정보 받는 중')
