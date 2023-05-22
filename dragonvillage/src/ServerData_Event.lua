28029d618f6cd15a9b544d38137d9291f2792d51
kwkang <kwkang@highbrow.com>
2023-05-22 01:17:24 +0000

    [기영] #이벤트 진입 시 투표 정보 요청 안하는 현상 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@125779 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: a517eecc240a837ccd37205c248bcf19ef4e5226

--- dragonvillage/src/ServerData_Event.lua
+++ dragonvillage/src/ServerData_Event.lua
@@ -982,7 +982,7 @@ function ServerData_Event:openEventPopup(tab, close_cb)
             if co:waitWork() then return end
         end
 
-        if (g_hotTimeData:isActiveEvent('event_vote_ticket')) then
+        if (g_hotTimeData:isActiveEvent('event_vote')) then
             co:work('# 신화 드래곤 투표 정보 받는 중')
             g_eventVote:requestEventVoteInfo(co.NEXT, co.ESCAPE)
             if co:waitWork() then return end
