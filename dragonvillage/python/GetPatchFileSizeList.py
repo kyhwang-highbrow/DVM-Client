f4be2970e730e89031d06085992bca7a9372777e
kwkang <kwkang@highbrow.com>
2023-06-21 03:04:45 +0000

    [기영] #패치 파일 사이즈 체크 툴
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@126041 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 32f3a729c765e879fb17f8ff99e971a91506f109

---
+++ dragonvillage/python/GetPatchFileSizeList.py
@@ -0,0 +1,67 @@
+# 특정 ROOT의 특정 확장자 파일의 이름과 사이즈 반환
+
+import os
+import re
+import csv
+import sys
+import imghdr
+ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../patch/', 'patch_')
+
+MAKE_CSV_NAME = 'result/FILE_SIZE_LIST.csv'
+
+root_simple = os.path.basename(ROOT)
+# brief : 특정 파일 이름 및 사이즈 리스트 반환
+
+
+def get_file_size_data(root):
+    data_list = []
+    file_name_list = os.listdir(root)
+    for file_name in file_name_list:
+        full_file_name = os.path.join(root, file_name)
+
+        if os.path.isdir(full_file_name):
+            sub_data_list = get_file_size_data(full_file_name)
+            data_list.extend(sub_data_list)
+
+        else:
+            ext = os.path.splitext(full_file_name)[-1]
+            if ext == '.zip' :
+                file_size = os.path.getsize(full_file_name)
+                name = root_simple + full_file_name[len(ROOT):]
+                data = [name, file_size, round(file_size / 1024, 2), round(file_size / 1024 / 1024, 2)]
+                data_list.append(data)
+
+    return data_list
+
+
+# brief : csv 생성에 쓰일 헤더 생성
+def make_header():
+    return ['name', 'byte', 'KB', 'MB']
+
+
+def make_csv(data_list):
+    sum_size = 0
+    for data in data_list:
+        sum_size = data[3] + sum_size    
+        print(' ', os.path.basename(data[0]), ':' , sum_size, 'MB')
+    print('Total Size :', sum_size, 'MB')
+        
+
+def main():
+    root = ROOT + sys.argv[1].replace('.', '_')
+    print("# Make Patch File Zip List And Size")
+    print("# Root :", root)
+
+    # 파일 이름 및 사이즈 데이터 리스트 얻음
+    data_list = get_file_size_data(root)
+
+    make_csv(data_list)
+
+    print("# FINISH :", MAKE_CSV_NAME)
+
+    os.system('pause')    
+
+
+if __name__ == '__main__':
+    main()
+         
