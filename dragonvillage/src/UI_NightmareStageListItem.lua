cfd2004e512ee3951697ffb272625a77ce4435fd
kyhwang <kyhwang@highbrow.com>
2023-07-17 06:44:08 +0000

    [기영] #악몽 던전 스테이지 추가, 11단계부터 10단계와 동일하게 보상 표기 처리
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@126290 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: df19bf2c414e803a54043428237a4115c43a3fef

--- dragonvillage/src/UI_NightmareStageListItem.lua
+++ dragonvillage/src/UI_NightmareStageListItem.lua
@@ -23,68 +23,66 @@ function UI_NightmareStageListItem:refresh_dropItem(t_data)
     local game_mode = g_stageData:getGameMode(t_data['stage'])
 
     local t_dungeon = g_nestDungeonData:parseNestDungeonID(stage_id)
-
     local tier = t_dungeon['tier']
 
-    local l_icon_res = {}
+    local drop_helper = DropHelper(stage_id)
+    local l_item_list = drop_helper:getDisplayItemList()
+
     if (tier == 1) then
-        table.insert(l_icon_res, 'star_23.png')
-        table.insert(l_icon_res, 'set_all_03.png')
+        table.insert(l_item_list, 'star_23.png')
 
     elseif (tier == 2) then
-        table.insert(l_icon_res, 'star_24.png')
-        table.insert(l_icon_res, '02_all_04.png')
-        table.insert(l_icon_res, '01_all_04.png')
+        table.insert(l_item_list, 'star_24.png')
 
     elseif (tier == 3) then
-        table.insert(l_icon_res, 'star_24.png')
-        table.insert(l_icon_res, '04_all_04.png')
-        table.insert(l_icon_res, '03_all_04.png')
+        table.insert(l_item_list, 'star_24.png')
     
     elseif (tier == 4) then
-        table.insert(l_icon_res, 'star_24.png')
-        table.insert(l_icon_res, '06_all_04.png')
-        table.insert(l_icon_res, '05_all_04.png')
+        table.insert(l_item_list, 'star_24.png')
     
     elseif (tier == 5) then
-        table.insert(l_icon_res, 'star_35.png')
-        table.insert(l_icon_res, 'set_all_05.png')
+        table.insert(l_item_list, 'star_35.png')
     
     elseif (tier == 6) then
-        table.insert(l_icon_res, 'star_36.png')
-        table.insert(l_icon_res, 'set_all_06.png')
+        table.insert(l_item_list, 'star_36.png')
     
     elseif (tier == 7) then
-        table.insert(l_icon_res, 'star_46.png')
-        table.insert(l_icon_res, '02_all_06.png')
-        table.insert(l_icon_res, '01_all_06.png')
+        table.insert(l_item_list, 'star_46.png')
     
     elseif (tier == 8) then
-        table.insert(l_icon_res, 'star_46.png')
-        table.insert(l_icon_res, '04_all_06.png')
-        table.insert(l_icon_res, '03_all_06.png')
+        table.insert(l_item_list, 'star_46.png')
     
     elseif (tier == 9) then
-        table.insert(l_icon_res, 'star_46.png')
-        table.insert(l_icon_res, '06_all_06.png')
-        table.insert(l_icon_res, '05_all_06.png')
-    
-    elseif (tier >= 10) then
-        table.insert(l_icon_res, 'star_46.png')
-        table.insert(l_icon_res, 'set_all_06.png')
+        table.insert(l_item_list, 'star_46.png')
+
+    elseif (tier == 10) then
+        table.insert(l_item_list, 'star_46.png')
+
+    elseif (tier >= 11) then
+        table.insert(l_item_list, 'star_46.png')
+
     end
 
-    for i,res in ipairs(l_icon_res) do
-        if (i == 1) then
-            local icon = IconHelper:getIcon('res/ui/icons/rune/' .. res)
-            vars['rewardNode' .. i]:addChild(icon)
-        else
-            local frame = IconHelper:getIcon('ui/a2d/card/card_item_frame.png')
-            local icon = IconHelper:getIcon('res/ui/icons/rune/' .. res)
-            frame:addChild(icon)
-            vars['rewardNode' .. i]:addChild(frame)
+    -- 별이 맨 앞으로 가도록 정렬
+    table.sort(l_item_list, function(a,b)
+        local a_value = string.find(tostring(a), 'star_') and 99 or 0
+        local b_value = string.find(tostring(b), 'star_') and 99 or 0
+        return a_value > b_value
+    end)
+
+    for i = 1, 4 do
+        local item_id = l_item_list[i]
+        if item_id then
+            if string.find(item_id, 'star_') then
+                local res = item_id
+                local icon = IconHelper:getIcon('res/ui/icons/rune/' .. res)
+                vars['rewardNode' .. i]:addChild(icon)
+            else
+                local ui = UI_ItemCard(item_id)
+                vars['rewardNode' .. i]:addChild(ui.root)
+                ui.root:setSwallowTouch(false)
+            end
         end
-        
     end
 
     -- 악몽 던전 소비 활동력 핫타임 관련
