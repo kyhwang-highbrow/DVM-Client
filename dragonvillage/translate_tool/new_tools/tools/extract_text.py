96933c11448a323abfc2e6432533b13c777722ff
kyhwang <kyhwang@highbrow.com>
2023-11-21 03:55:16 +0000

    [기영] #구글 지원 언어 확대 작업 중
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@127829 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 6db70c93b87f9edb2a07a1da48c91d11a113db22

--- dragonvillage/translate_tool/new_tools/tools/extract_text.py
+++ dragonvillage/translate_tool/new_tools/tools/extract_text.py
@@ -12,6 +12,7 @@ import json
 import re
 import G_sheet.spread_sheet as spread_sheet
 from tools.G_sheet.sheet_option import get_sheet_option
+import time
 
 from extract.extract import extract
 from sum_data.sum_data import sum_data
@@ -35,18 +36,28 @@ with open('config.json', 'r', encoding='utf-8') as f: # config.json으로부터 
 def start_upload(upload_method, patch_sheet_name, backup_sheet_name, all_data_list):
     ss_list_sheet = spread_sheet.get_spread_sheet(spreadsheet_id).get_work_sheet('ss_list')
     ss_info_list = quote_row_dics(spread_sheet.make_rows_to_dic(ss_list_sheet.get_all_values()))    
+    
+    # 1번째만 백업 시트와 비교 후 정석대로 업로드
+    row = ss_info_list[0]
+    ss_id = row['ss_id']
+    lang_code = row['lang_code']
+    lang_code_list = lang_code.split(',')
+    data_list = upload(upload_method, patch_sheet_name, backup_sheet_name, ss_id, all_data_list, lang_code_list)    
+    progress_str = 'Upload Complete({0}/{1}) : {2}'.format(1, len(ss_info_list), lang_code)
+    print(progress_str)   
 
-    idx = 1
-    for row in ss_info_list:
+    idx = 2
+    # 2번째부터는 1번째에 추출된 텍스트 리스트를 토대로 동기화(매번 비교하면 퍼포먼스가 느림)
+    for row in ss_info_list[1:]:
         ss_id = row['ss_id']
         lang_code = row['lang_code']
-        lang_code_list = lang_code.split(',')        
-        
-        progress_str = 'Upload start({0}/{1}) : {2}'.format(idx, len(ss_info_list), lang_code)
-        print(progress_str)
-
-        upload(upload_method, patch_sheet_name, backup_sheet_name, ss_id, all_data_list, lang_code_list)        
+        lang_code_list = lang_code.split(',')
+        direct_upload_method = 'direct_' + upload_method
+        upload(direct_upload_method, patch_sheet_name, backup_sheet_name, ss_id, data_list, lang_code_list)
+        progress_str = 'Sync Complete({0}/{1}) : {2}'.format(idx, len(ss_info_list), lang_code)
         idx = idx + 1
+        print(progress_str)
+        time.sleep(5)
 
 def extract_text(extract_config):
     date = datetime.datetime.now()
@@ -60,6 +71,9 @@ def extract_text(extract_config):
     extract_method_list = extract_config['extract_method_list']
     sum_data_method = extract_config['sum_data_method']
     upload_method = extract_config['upload_method']
+
+    if upload_method == 'upload_DVM_plain_text' :
+        return
 
     # 각 파일로부터 데이터를 추출하고 모읍니다.
     from_src_list = []
@@ -89,85 +103,8 @@ def extract_text(extract_config):
         print('\t', from_src['name'], '-', from_src['data_length'])
  
     # 하나로 모은 데이터를 구글 스프레드 시트에 작성합니다
-    # start_upload(upload_method, patch_sheet_name, backup_sheet_name, all_data_list) 
-    #make_plain_text_list(upload_method, all_data_list)
-    
+    start_upload(upload_method, patch_sheet_name, backup_sheet_name, all_data_list)    
     return all_data_list
-    
-def make_plain_text_list(upload_method, all_data_list):
-    if upload_method == "upload_DVM_plain_text":
-        temp_text_list = []
-        for data in all_data_list:
-            temp_data = []
-            temp_data.append(data[0])
-            temp_data.extend(['' for _ in range(len(locale_list))])
-            temp_data.append('plain_text')
-            temp_text_list.append(temp_data)
-        temp_text_list.sort(key=lambda line: line[0])
-        plain_text_list.extend(temp_text_list)
-
-    else:
-        # 데이터를 파일 이름과 페이지 값을 이용하여 정렬합니다.
-        all_data_list.sort(key=cmp_to_key(cmp_scenario)) 
-        for data in all_data_list:
-            temp_data1 = []
-            temp_data1.append(data[2])
-            temp_data1.extend(['' for _ in range(len(locale_list))])
-            temp_data1.append('scenario_speaker')
-            plain_text_list.append(temp_data1)
-
-        len_locale_list = len(locale_list)
-        for data in all_data_list:
-            temp_data2 = []            
-            temp_data2.append(data[2 + len_locale_list + 1])
-            temp_data2.extend(['' for _ in range(len(locale_list))])
-            temp_data1.append('scenario_text')
-            plain_text_list.append(temp_data2)
-
-def upload_integration_sheet():
-    if len(plain_text_list) == 0:
-        return
-    
-    # 헤더를 생성합니다.
-    header = ['kr']
-    for locale in locale_list:
-        header.append(locale)
-
-    # 시트를 만들 때 사용될 칼럼 사이즈입니다.
-    col_size = len(header)
-
-    # 스프레드시트의 아이디값을 이용하여 연결합니다.
-    sheet = spread_sheet.get_spread_sheet(integration_spreadsheet_id)
-
-    # 데이터 리스트 사이즈를 바탕으로 시트를 작성합니다.
-    delta_sheet = sheet.get_work_sheet('DIFF')
-    # if delta_sheet is not None:
-    #     sheet.del_work_sheet(delta_sheet)
-    #     delta_sheet = None
-
-    # 헤더 생성
-    if delta_sheet is None:
-        backup_option = {}
-        backup_option['rows'] = 1
-        backup_option['cols'] = col_size
-        delta_sheet = sheet.add_work_sheet('DIFF', backup_option)
-        delta_sheet.insert_row(header, 1, value_input_option='RAW')
-        #sheet_id = delta_sheet._properties['sheetId']
-        #sheet_option = get_sheet_option('DVM_plain_text', sheet_id, col_size)
-        #sheet.batch_update(sheet_option)
-
-    # 행들 추가
-    exist_datas = delta_sheet.get_all_values()
-    row_size = len(exist_datas) + 1
-    delta_sheet.resize(rows=row_size)
-
-    if len(plain_text_list) > 0:
-        delta_sheet.insert_rows(plain_text_list, row_size, value_input_option='RAW')
-
-    # 시트의 크기를 보기 좋게 조정합니다.
-    # sheet_id = delta_sheet._properties['sheetId']
-    # sheet_option = get_sheet_option('DVM_plain_text', sheet_id, col_size)
-    # sheet.batch_update(sheet_option)
 
 def find_all_color_codes():
     set_colors = set()
@@ -180,7 +117,6 @@ def find_all_color_codes():
                 text_color_list.append(color_code)
                 print(color_code)
     #print(text_color_list)
-
 
 def extract_text_from_config_lists():
     for extract_config in extract_config_list:
--- dragonvillage/translate_tool/new_tools/tools/move_divide_sheets.py
+++ dragonvillage/translate_tool/new_tools/tools/move_divide_sheets.py
@@ -2,16 +2,13 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-import datetime
 import json
-import re
 import G_sheet.spread_sheet as spread_sheet
 from tools.G_sheet.sheet_option import get_sheet_option
 from lang_codes.lang_codes import get_language_code_list
 from util.util_quote import quote_row_dics
 
 total_text_rows = []
-
 with open('config.json', 'r', encoding='utf-8') as f: # config.json으로부터 데이터 읽기
     config_json = json.load(f)
     spreadsheet_id = config_json['spreadsheet_id']
@@ -32,8 +29,7 @@ def read_input_sheets():
     sheet = spread_sheet.get_spread_sheet(integration_spreadsheet_id)
     work_sheet = sheet.get_work_sheet('DIFF')
     all_rows = work_sheet.get_all_values()   
-
-    for v in all_rows[1:]:        
+    for v in all_rows[1:]:
         data_list = v[1:]
         total_text_rows.append(data_list)
 
@@ -42,6 +38,7 @@ def copy_cells_to_sheets():
     ss_info_list = quote_row_dics(spread_sheet.make_rows_to_dic(ss_list_sheet.get_all_values()))
 
     slice_idx = 0
+    idx = 1
     key_word_list = ['plain_text', 'scenario_speaker', 'scenario_text']
     for row in ss_info_list:
         ss_id = row['ss_id']
@@ -54,9 +51,11 @@ def copy_cells_to_sheets():
 
         for key_word in key_word_list:
             sliced_text_list = get_slice_text_list(key_word, start_idx, last_idx)
-            update_sheets(ss_id, key_word, sliced_text_list, lang_count)
-        
-        break
+            update_sheets(ss_id, key_word, sliced_text_list, lang_count)       
+
+        progress_str = 'move complete !! ({0}/{1}) : {2}'.format(idx, len(ss_info_list), lang_code)
+        idx = idx + 1
+        print(progress_str)
         slice_idx = last_idx
 
 def update_sheets(ss_id, key_word, sliced_text_list, lang_count):
@@ -65,31 +64,24 @@ def update_sheets(ss_id, key_word, sliced_text_list, lang_count):
 
     if key_word == 'plain_text':
         work_sheet = sheet.get_work_sheet('only_ingame')
-
         ascii_code = chr(ord('B') + lang_count - 1)
-        cell_range = 'B2:{0}{1}'.format(ascii_code, len(sliced_text_list) + 1)
-
-        print('cell_range', cell_range)
-        print('sliced_text_list count', len(sliced_text_list) + 1)
-        print('sliced_text_list col count', len(sliced_text_list[0]))
-        
+        cell_range = 'B2:{0}{1}'.format(ascii_code, count + 1)       
         work_sheet.update(cell_range, sliced_text_list)        
-
     else:
         work_sheet = sheet.get_work_sheet('only_scenario')
         if key_word == 'scenario_speaker':
-            cell_range = 'C2:C{0}'.format(2 + count)
+            ascii_code = chr(ord('D') + lang_count - 1)
+            cell_range = 'D2:{0}{1}'.format(ascii_code, count + 1)
             work_sheet.update(cell_range, sliced_text_list)
-
         elif key_word == 'scenario_text':            
-            ascii_code = chr(ord('D') + lang_count)
-            cell_range = '{0}2:{1}{2}'.format(ascii_code, ascii_code, 2 + count)
+            start_ascii_code = chr(ord('D')  + lang_count + 1)  
+            end_ascii_code = chr(ord(start_ascii_code) + lang_count)
+            cell_range = '{0}2:{1}{2}'.format(start_ascii_code, end_ascii_code, count + 1)
             work_sheet.update(cell_range, sliced_text_list)
 
 if __name__ == '__main__':
-    print('\n*** 작업      : 프로젝트에서 텍스트를 추출합니다.')    
-
+    print('*** 작업      : 번역본 이동 시작')
     read_input_sheets()
     copy_cells_to_sheets()
-
+    print('*** 작업      : 번역본 이동 완료')
     os.system('pause')
