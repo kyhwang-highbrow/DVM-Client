a78887b0d1df9d954af208d773d1c35b3d12e3e5
kyhwang <kyhwang@highbrow.com>
2023-12-05 07:53:39 +0000

    [기영] #다국어 지원, 언어 패치 생성 스크립트 작업
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@128034 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 66a979ff45d36729d74fc199a383a4494ccb369e

--- dragonvillage/translate_tool/new_tools/tools/extract_text.py
+++ dragonvillage/translate_tool/new_tools/tools/extract_text.py
@@ -83,7 +83,11 @@ def extract_text(extract_config):
         only_include_files = extract_method['only_include_files']
         
         # 설정으로부터 추출된 데이터, 리스트 형태일수도 딕셔너리 형태일수도 있음
-        from_data = extract(extract_func, source_dir, ignore_files, ignore_folders, ignore_krs, only_include_files)
+        only_include_file_count = len(only_include_files)
+        if only_include_file_count == 0:
+            from_data = extract(extract_func, source_dir, ignore_files, ignore_folders, ignore_krs)
+        else:
+            from_data = extract(extract_func, source_dir, ignore_files, ignore_folders, ignore_krs, only_include_files)
 
         print('current working info')
         print('source_dir : ', source_dir)
