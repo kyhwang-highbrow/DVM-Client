7773a13358ce1aa64af535e8f9e3abd30b8cd091
kyhwang <kyhwang@highbrow.com>
2024-01-10 02:59:57 +0000

    [기영] #md5 로그 파일 커밋
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@128383 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 08d95c399ee1ae21e4f9857d2e47c2a2f0ef9c9f

--- dragonvillage/python/svn.py
+++ dragonvillage/python/svn.py
@@ -1,17 +1,13 @@
 import os
 import subprocess
 
-def svn_add_and_commit(folder_path, commit_message):
+def svn_add_and_commit(new_file_path, commit_message):
     try:
-        # 해당 폴더의 모든 파일에 대해 svn add 명령어 실행
-        for root, dirs, files in os.walk(folder_path):
-            for file_name in files:
-                file_path = os.path.join(root, file_name)
-                subprocess.run(['svn', 'add', file_path])
+        # SVN에 파일 추가
+        subprocess.run(['svn', 'add', new_file_path])
 
         # SVN 커밋 명령어 실행
-        subprocess.run(['svn', 'commit', '-m', commit_message, folder_path])
-        print(f'커밋이 성공적으로 완료되었습니다: {folder_path}')
-        
+        subprocess.run(['svn', 'commit', '-m', commit_message, new_file_path])
+        print(f'커밋이 성공적으로 완료되었습니다: {new_file_path}')
     except subprocess.CalledProcessError as e:
         print(f'커밋 도중 오류가 발생했습니다: {e}')
