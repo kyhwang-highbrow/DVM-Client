e2cef344344a025adb731b029aa3730d24905a37
kyhwang <kyhwang@highbrow.com>
2024-01-11 05:43:52 +0000

    [기영] #클랜 멤버 스크롤 사이즈 조절
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@128405 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 420f23e46cce7376208cae65b28599b8b55ea692

--- dragonvillage/src/UI_ClanTabMember.lua
+++ dragonvillage/src/UI_ClanTabMember.lua
@@ -71,7 +71,7 @@ function UI_ClanTabMember:init_TableView()
 
     -- 테이블 뷰 인스턴스 생성
     local table_view = UIC_TableView(node)
-    table_view.m_defaultCellSize = cc.size(1170, 85 + 6)
+    table_view.m_defaultCellSize = cc.size(1170, UIHelper:getProfileScrollItemHeight(85 + 6, 15))
     table_view:setCellUIClass(UI_ClanMemberListItem, create_func)
     table_view:setDirection(cc.SCROLLVIEW_DIRECTION_VERTICAL)
     table_view:setItemList(l_item_list)
