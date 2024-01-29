0e9d54b83517c1b40c212ea395b7c6eb882075e0
kyhwang <kyhwang@highbrow.com>
2024-01-29 06:08:18 +0000

    [기영] #광고 무료 소환 프로세스 변경(광고 -> /shop/adv_show API -> /shop/summon/cash API)
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@128738 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 57a17c01625d2a53a9ac3c409cf668e8ac96f7b7

--- dragonvillage/src/UI_HatcherySummonTab.lua
+++ dragonvillage/src/UI_HatcherySummonTab.lua
@@ -1261,18 +1261,27 @@ function UI_HatcherySummonTab:requestSummon(t_egg_data, old_ui, is_again)
             return
         end
 
-        local function ad_success_cb(ret)
-            AdManager.getInstance():showRewardAd_Common(function(ret)
-                if (ret == 'success') then
-                        self:click_cashSummonBtn(is_bundle, true--[[is_ad]], false--[[is_sale]], 
-                            t_egg_data, old_ui, draw_cnt, pickup_id)
-                else
-                    -- 실패의 경우 함수 내부에서 알아서 처리됨
-                end
-            end)
-        end
+        -- local function ad_success_cb(ret)
+        --     AdManager.getInstance():showRewardAd_Common(function(ret)
+        --         if (ret == 'success') then
+        --                 self:click_cashSummonBtn(is_bundle, true--[[is_ad]], false--[[is_sale]], 
+        --                     t_egg_data, old_ui, draw_cnt, pickup_id)
+        --         else
+        --             -- 실패의 경우 함수 내부에서 알아서 처리됨
+        --         end
+        --     end)
+        -- end
 
-        g_advertisingData:request_dailyAdShow(AD_TYPE.ADVANCED_SUMMON, ad_success_cb)
+        AdManager.getInstance():showRewardAd_Common(function(ret)
+            if (ret == 'success') then
+                g_advertisingData:request_dailyAdShow(AD_TYPE.ADVANCED_SUMMON, function()
+                    self:click_cashSummonBtn(is_bundle, true--[[is_ad]], false--[[is_sale]], 
+                    t_egg_data, old_ui, draw_cnt, pickup_id)
+                end)
+            else
+                -- 실패의 경우 함수 내부에서 알아서 처리됨
+            end
+        end)
     end
 
     local item_key = t_egg_data['price_type']
