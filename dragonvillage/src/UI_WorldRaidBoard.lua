575ced25bc73fb0c574ea2413f86e33012648a97
kyhwang <kyhwang@highbrow.com>
2024-02-06 10:28:38 +0000

    [기영] #월드 레이드, 게시판 보상 이슈 수정
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@128993 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: 5da147e775edfc892f260b8b419ae68dcf7f001e

--- dragonvillage/src/UI_WorldRaidBoard.lua
+++ dragonvillage/src/UI_WorldRaidBoard.lua
@@ -106,22 +106,17 @@ function UI_WorldRaidBoard:checkEnterEvent()
                 return
             end
 
-            local profile_frames = ret['profile_frames']
-            if profile_frames == nil then
+            local profile_frame_id = ret['reward_frame']
+            if profile_frame_id == nil then
                 return
             end
 
             -- 랭킹 정산 보상은 프레임만 있다고 함
             local t_reward = {}
-            local profile_frame_id = 0
-            for item_id, _ in pairs(profile_frames) do               
-                local t_item = {item_id = tonumber(item_id), count = 1}
-                table.insert(t_reward, t_item)
-                profile_frame_id = tonumber(item_id)
-                break                
-            end
+            local t_item = {item_id = profile_frame_id, count = 1}
+            table.insert(t_reward, t_item)
 
-            local t_item = {item_id = 700330, count = 30}
+            t_item = {item_id = 700330, count = 30}
             table.insert(t_reward, t_item)
 
             self.m_tRankingRewardInfo['reward_info'] = t_reward
