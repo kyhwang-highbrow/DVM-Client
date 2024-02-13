29ac4b1b1d0e68180c89c1221ab641a3e0b1e8fe
kyhwang <kyhwang@highbrow.com>
2024-02-13 07:19:10 +0000

    [기영] #번역 프로세스 개선
    git-svn-id: svn://35.216.44.119/DVM/dragonvillage/src/frameworks@129040 5003bd00-c432-460b-831f-0659da9acde2
    
    Former-commit-id: bbcb505fee0b2dcde7e9fcc756cbf79e0e189c79

---
+++ dragonvillage/translate_tool/py_tools/tools/check_sync/check_sync.py
@@ -0,0 +1,51 @@
+#############################################################################
+## 14개 번역 시트 간 데이터 일치여부 체크
+#############################################################################
+import sys
+import os
+import json
+import util.util_file as util_file
+from util.util_quote import quote_row_dics
+
+import G_sheet.spread_sheet as spread_sheet
+
+# search_root = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+with open('config.json', 'r', encoding='utf-8') as f: # config.json으로부터 데이터 읽기
+    config_json = json.load(f)
+    spreadsheet_id = config_json['spreadsheet_id']
+    extract_config_list = config_json['extract_config_list']
+
+def get_sheet_col_vals(compare_sheet, target_column_name):
+    # 시트에서 첫 번째 행의 컬럼명 가져오기
+    header_row = compare_sheet.row_values(1)
+    column_index = header_row.index(target_column_name)
+    values = compare_sheet.col_values(column_index)
+    return values
+
+def check_sync(sheet_name_list):
+    ss_list_sheet = spread_sheet.get_spread_sheet(spreadsheet_id).get_work_sheet('ss_list')
+    ss_info_list = quote_row_dics(spread_sheet.make_rows_to_dic(ss_list_sheet.get_all_values()))
+    prev_compare_sheet = None
+
+    # 함수 호출
+    try:
+        for sheet_name in sheet_name_list:
+            for row in ss_info_list:
+                ss_id = row['ss_id']
+                lang_code = row['lang_code']
+                # 스프레드시트의 아이디값을 이용하여 연결합니다.
+                sheet = spread_sheet.get_spread_sheet(ss_id)
+                # 데이터 리스트 사이즈를 바탕으로 시트를 작성합니다.
+                compare_sheet = sheet.get_work_sheet(sheet_name)
+                if compare_sheet is not None:                  
+                    if prev_compare_sheet is not None:
+                        compare_values = get_sheet_col_vals(compare_sheet, 'kr')
+                        prev_values = get_sheet_col_vals(prev_compare_sheet, 'kr')
+                        if prev_values != compare_values:
+                            raise ValueError('분할 시트 간에 싱크가 맞지 않습니다. [{0}] : {1}'.format(sheet_name, lang_code))
+                    prev_compare_sheet = compare_sheet
+    
+    except ValueError as e:
+        print(f"예외 발생: {e}")
+
+    return 0
